064956a76a3d8389f20e9fc27007c6dc9a841545
Sarah Brown <sarah@buoyant.io>
2016-01-26 14:00:09 -0800

    Adds router label dropdown for dtab ui.
    My hope is the dropdown can be used across all pages eventually to filter metrics based on router label. For now the dropdown is hidden on all pages except the dtab ui.

--- linkerd/admin/src/main/resources/io/buoyant/linkerd/admin/js/admin.js
+++ linkerd/admin/src/main/resources/io/buoyant/linkerd/admin/js/admin.js
@@ -4,4 +4,36 @@ $(function() {
   // highlight current page in navbar
   var path = window.location.pathname;
   $('a[href="' + path + '"]').parent().addClass("current");
+
+  var matches = window.location.search.match(/router=(.+)(?:&|$)/)
+  if (matches && matches.length > 1) {
+    var label = decodeURIComponent(matches[1]);
+    $(".dropdown-toggle .router-label").text(label)
+  }
 });
+
+$.when(
+  $.get("/files/template/router_option.template"),
+  $.get("/routers.json")
+).done(function(templateRsp, routers) {
+  var template = Handlebars.compile(templateRsp[0])
+  var routerHtml = routers[0].map(template);
+
+  $(".dropdown-menu").html(routerHtml.join(""));
+
+  $(".router-menu-option").click(function() {
+    selectRouter($(this).text());
+  });
+});
+
+function selectRouter(label) {
+  var uriComponent = "router=" + encodeURIComponent(label);
+  var re = /router=(.+)(?:&|$)/
+  if (window.location.search == "") {
+    window.location.search = uriComponent;
+  } else if (window.location.search.match(re)) {
+    window.location.search = window.location.search.replace(re, uriComponent);
+  } else {
+    window.location.search = window.location.search + "&" + uriComponent;
+  }
+}
--- linkerd/admin/src/main/resources/io/buoyant/linkerd/admin/js/delegate.js
+++ linkerd/admin/src/main/resources/io/buoyant/linkerd/admin/js/delegate.js
@@ -8,15 +8,33 @@ $.when(
 ).done(function(dentryRsp, nodeRsp){
   templates.dentry = Handlebars.compile(dentryRsp[0]);
   templates.node = Handlebars.compile(nodeRsp[0]);
-  var dtab = JSON.parse($("#data").html());
-  var dtabViewer = new DtabViewer(dtab, templates.dentry);
-  $(function(){
-    $('.go').click(function(e){
-      e.preventDefault();
-      var path = $('.path input').val();
-      $.get("delegator.json?" + $.param({ n: path, d: dtabViewer.dtabStr() }), renderAll);
+  var dtabMap = JSON.parse($("#data").html());
+
+  var selectedRouter = $(".dropdown-toggle .router-label").text();
+  var dtab = dtabMap[selectedRouter];
+
+  if (!dtab) {
+    var defaultRouter = $(".router-menu-option:first").text();
+    if (dtabMap[defaultRouter]) {
+      selectRouter(defaultRouter);
+    } else {
+      console.warn("undefined router:", selectedRouter);
+    }
+  } else {
+    //TODO: remove this once dropdown is site-wide
+    $(".nav .dropdown").removeClass("hide");
+
+    $(".router-label-title").text("Router \"" + selectedRouter + "\"");
+
+    var dtabViewer = new DtabViewer(dtab, templates.dentry);
+    $(function(){
+      $('.go').click(function(e){
+        e.preventDefault();
+        var path = $('.path input').val();
+        $.get("delegator.json?" + $.param({ n: path, d: dtabViewer.dtabStr() }), renderAll);
+      });
     });
-  });
+  }
 });
 
 $(function() {
