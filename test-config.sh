bf00bd1642f75f3fd2b0b43f9ce3bcb6434467d0
Oliver Gould <ver@buoyant.io>
2016-03-19 19:52:10 +0000

    Support building against -SNAPSHOT twitter dependencies
    Add an sbt setting, developTwitterDeps, that causes all twitter dependencies to
    be set to their SNAPSHOT versions.  The project/install-twitter-develop-deps.sh
    script fetches and installs SNAPSHOT version of all relevant projects.
    
    Once SNAPSHOT versions are published, linkerd may be tested against these
    dependencies with the following:
    
        ./sbt 'set developTwitterDeps in Global := true' test
    
    circle.yml has been updated to support develop builds (via environment) and to
    publish test reports.

--- test-config.sh
+++ test-config.sh
@@ -30,7 +30,11 @@ function cleanup {
 function run_tests {
   export LOG_LEVEL=DEBUG
 
-  ./sbt examples/acceptance-test:run &
+  if [ -n "${TWITTER_DEVELOP}" ]; then
+    ./sbt 'set developTwitterDeps in Global := true' examples/acceptance-test:run &
+  else
+    ./sbt examples/acceptance-test:run &
+  fi
 
   # Wait for linkerd to initialize
   sleep 60
