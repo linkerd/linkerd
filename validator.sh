f3c54f560855e716f2209665f8dfc8ee8281fe29
Greg Campbell <greg@buoyant.io>
2016-03-30 14:39:38 -0700

    Update headVersion to 0.3.0.
    Also update validator script.

--- validator.sh
+++ validator.sh
@@ -1,2 +1,2 @@
-./sbt "project validator" "run -linkerd.exec=linkerd/target/scala-2.11/linkerd-0.2.2-SNAPSHOT-exec -namerd.exec=namerd/target/scala-2.11/namerd-0.2.2-SNAPSHOT-exec"
+./sbt "project validator" "run -linkerd.exec=linkerd/target/scala-2.11/linkerd-0.3.0-SNAPSHOT-exec -namerd.exec=namerd/target/scala-2.11/namerd-0.3.0-SNAPSHOT-exec"
 
