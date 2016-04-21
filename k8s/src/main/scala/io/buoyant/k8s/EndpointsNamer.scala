6a92418a0e730a38e0ca8c8eaea592c445e3bc7c
Alex Leong <alex@buoyant.io>
2016-04-21 09:22:43 -0700

    Simplify the NsCache interface (#316)
    Simplify the NsCache interface by making the state Var private and exposing
    an Activity instead.

--- k8s/src/main/scala/io/buoyant/k8s/EndpointsNamer.scala
+++ k8s/src/main/scala/io/buoyant/k8s/EndpointsNamer.scala
@@ -23,7 +23,7 @@ class EndpointsNamer(idPrefix: Path, mkApi: String => NsApi) extends Namer {
     case id@Path.Utf8(nsName, portName, serviceName) =>
       val residual = path.drop(PrefixLen)
       log.debug("k8s lookup: %s %s", id.show, path.show)
-      Activity(Ns.get(nsName).services).flatMap { services =>
+      Ns.get(nsName).services.flatMap { services =>
         log.debug("k8s ns %s initial state: %s", nsName, services.keys.mkString(", "))
         services.get(serviceName) match {
           case None =>
@@ -64,7 +64,7 @@ class EndpointsNamer(idPrefix: Path, mkApi: String => NsApi) extends Namer {
     }
 
     private[this] def mkNs(name: String): NsCache = {
-      val nsCache = new NsCache(name, Var(Activity.Pending))
+      val nsCache = new NsCache(name)
       _watches = _watches + (name -> watch(name, nsCache))
       nsCache
     }
@@ -231,19 +231,21 @@ private object EndpointsNamer {
       }
   }
 
-  class NsCache(name: String, activity: ActUp[Map[String, SvcCache]]) {
+  class NsCache(name: String) {
 
-    def services: Var[Activity.State[Map[String, SvcCache]]] = activity
+    private[this] val state = Var[Activity.State[Map[String, SvcCache]]](Activity.Pending)
+
+    val services: Activity[Map[String, SvcCache]] = Activity(state)
 
     def clear(): Unit = synchronized {
-      activity.sample() match {
+      state.sample() match {
         case Activity.Ok(snap) =>
           for (svc <- snap.values) {
             svc.clear()
           }
         case _ =>
       }
-      activity() = Activity.Pending
+      state() = Activity.Pending
     }
 
     /**
@@ -257,7 +259,7 @@ private object EndpointsNamer {
       }
 
       synchronized {
-        activity() = Activity.Ok(initSvcs.toMap)
+        state() = Activity.Ok(initSvcs.toMap)
       }
     }
 
@@ -280,17 +282,17 @@ private object EndpointsNamer {
     private[this] def add(endpoints: v1.Endpoints): Unit =
       for (svc <- mkSvc(endpoints)) synchronized {
         log.debug("k8s added: %s", svc.name)
-        val svcs = services.sample() match {
+        val svcs = state.sample() match {
           case Activity.Ok(svcs) => svcs
           case _ => Map.empty[String, SvcCache]
         }
-        activity() = Activity.Ok(svcs + (svc.name -> svc))
+        state() = Activity.Ok(svcs + (svc.name -> svc))
       }
 
     private[this] def modify(endpoints: v1.Endpoints): Unit =
       for (name <- getName(endpoints)) synchronized {
         log.debug("k8s modified: %s", name)
-        services.sample() match {
+        state.sample() match {
           case Activity.Ok(snap) =>
             snap.get(name) match {
               case None =>
@@ -305,17 +307,15 @@ private object EndpointsNamer {
     private[this] def delete(endpoints: v1.Endpoints): Unit =
       for (name <- getName(endpoints)) synchronized {
         log.debug("k8s deleted: %s", name)
-        services.sample() match {
+        state.sample() match {
           case Activity.Ok(snap) =>
             for (svc <- snap.get(name)) {
               svc.clear()
-              activity() = Activity.Ok(snap - name)
+              state() = Activity.Ok(snap - name)
             }
 
           case _ =>
         }
       }
-
   }
-
 }
