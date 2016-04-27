4a2b12a928233730e81289d3622be7c793dd3adb
Alex Leong <alex@buoyant.io>
2016-04-27 16:30:30 -0700

    Checkpoint.

--- namerd/iface/interpreter-thrift/src/main/scala/io/buoyant/namerd/iface/ObserverCache.scala
+++ namerd/iface/interpreter-thrift/src/main/scala/io/buoyant/namerd/iface/ObserverCache.scala
@@ -1,5 +1,6 @@
 package io.buoyant.namerd.iface
 
+import com.google.common.cache.{CacheBuilder, RemovalListener, RemovalNotification}
 import com.twitter.util.{Return, Throw, Try}
 import io.buoyant.namerd.iface.ThriftNamerInterface.Observer
 import java.util.concurrent.ConcurrentHashMap
@@ -7,36 +8,46 @@ import java.util.concurrent.ConcurrentHashMap
 class MaximumObservationsReached(maxObservations: Int)
   extends Exception(s"The maximum number of concurrent observations has been reached ($maxObservations)")
 
-class ObserverCache[K, T](maxObservations: Int)(mkObserver: K => Observer[T]) {
+class ObserverCache[K, T](activeCapacity: Int, inactiveCapacity: Int)(mkObserver: K => Observer[T]) {
 
   def get(key: K): Try[Observer[T]] = {
     Option(Return(activeCache.get(key))).getOrElse {
       synchronized {
-        makeActive(key)
+        Option(activeCache.get(key))
+          .map(Return(_))
+          .getOrElse(makeActive(key))
       }
     }
   }
 
   private[this] val activeCache = new ConcurrentHashMap[K, Observer[T]]
-  private[this] val inactiveCache = new ConcurrentHashMap[K, Observer[T]]
-
-  private[this] def currentObservations = activeCache.size + inactiveCache.size
+  private[this] val inactiveCache = CacheBuilder.newBuilder()
+    .maximumSize(inactiveCapacity)
+    .removalListener(new RemovalListener[K, Observer[T]] {
+      override def onRemoval(notification: RemovalNotification[K, Observer[T]]): Unit =
+        notification.getValue.close()
+    })
+    .build[K, Observer[T]]()
 
   private[this] def makeActive(key: K): Try[Observer[T]] =
-    Option(Return(inactiveCache.get(key))).getOrElse {
-      if (currentObservations < maxObservations)
-        Return(mkObserver(key))
-      else
-        Throw(new MaximumObservationsReached(maxObservations))
-    }.onSuccess { obs =>
+    if (activeCache.size < activeCapacity) {
+      val obs = Option(inactiveCache.getIfPresent(key)).getOrElse {
+        mkObserver(key)
+      }
+      inactiveCache.invalidate(key)
       activeCache.put(key, obs)
       obs.nextValue().ensure {
         makeInactive(key, obs)
       }
+      Return(obs)
+    } else {
+      Throw(new MaximumObservationsReached(activeCapacity))
     }
 
   private[this] def makeInactive(key: K, obs: Observer[T]): Unit = {
-    activeCache.remove(key)
-    inactiveCache.put(key, obs)
+    synchronized {
+      activeCache.remove(key)
+      inactiveCache.put(key, obs)
+    }
   }
 }
