e06665f5233e5e82d2c94f2c0ac8ba80d3611d2b
Alex Leong <alex@buoyant.io>
2016-04-27 17:30:13 -0700

    Checkpoint.

---
+++ namerd/iface/interpreter-thrift/src/test/scala/io/buoyant/namerd/iface/ObserverCacheTest.scala
@@ -0,0 +1,27 @@
+package io.buoyant.namerd.iface
+
+import org.scalatest.FunSuite
+
+class ObserverCacheTest extends FunSuite {
+
+  test("observer cache limits active observations") {
+
+    val 
+
+    val cache = new ObserverCache[]()
+
+  }
+
+  test("observer cache deactivates observations") {
+
+  }
+
+  test("observer cache reactivates observations") {
+
+  }
+
+  test("observer cache tears down inactive observations") {
+
+  }
+
+}
