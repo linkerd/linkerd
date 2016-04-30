5a757e0fa943d19cb365d6bab57e170c1d59a9a3
Oliver Gould <ver@buoyant.io>
2016-04-30 00:09:00 +0000

    add methods so it compiles

--- namerd/storage/etcd/src/main/scala/io/buoyant/namerd/storage/etcd/EtcdDtabStore.scala
+++ namerd/storage/etcd/src/main/scala/io/buoyant/namerd/storage/etcd/EtcdDtabStore.scala
@@ -1,8 +1,23 @@
-package io.buoyant.namerd.storage.etcd
+package io.buoyant.namerd
+package storage.etcd
 
-import io.buoyant.namerd.DtabStore
+import com.twitter.finagle.Dtab
+import com.twitter.io.Buf
+import com.twitter.util.{Activity, Future}
 import io.buoyant.etcd.Key
 
 class EtcdDtabStore(key: Key) extends DtabStore {
+  import DtabStore.Version
 
+  def list(): Activity[Set[Ns]] = ???
+
+  def create(ns: Ns, dtab: Dtab): Future[Unit] = ???
+
+  def delete(ns: Ns): Future[Unit] = ???
+
+  def update(ns: Ns, dtab: Dtab, version: Version): Future[Unit] = ???
+
+  def put(ns: Ns, dtab: Dtab): Future[Unit] = ???
+
+  def observe(ns: Ns): Activity[Option[VersionedDtab]] = ???
 }
