067a60069bd82c3690713e9d03adbdccaef72422
Oliver Gould <ver@buoyant.io>
2016-04-30 00:28:30 +0000

    Implement put and update for EtcdDtabStore

--- namerd/storage/etcd/src/main/scala/io/buoyant/namerd/storage/etcd/EtcdDtabStore.scala
+++ namerd/storage/etcd/src/main/scala/io/buoyant/namerd/storage/etcd/EtcdDtabStore.scala
@@ -1,12 +1,12 @@
 package io.buoyant.namerd
 package storage.etcd
 
-import com.twitter.finagle.Dtab
+import com.twitter.finagle.{Dtab, Path}
 import com.twitter.io.Buf
 import com.twitter.util.{Activity, Future}
 import io.buoyant.etcd.Key
 
-class EtcdDtabStore(key: Key) extends DtabStore {
+class EtcdDtabStore(root: Key) extends DtabStore {
   import DtabStore.Version
 
   def list(): Activity[Set[Ns]] = ???
@@ -15,9 +15,14 @@ class EtcdDtabStore(key: Key) extends DtabStore {
 
   def delete(ns: Ns): Future[Unit] = ???
 
-  def update(ns: Ns, dtab: Dtab, version: Version): Future[Unit] = ???
+  def update(ns: Ns, dtab: Dtab, version: Version): Future[Unit] = {
+    val Buf.Utf8(vstr) = version
+    val index = vstr.toLong
+    root.key(Path.Utf8(ns)).compareAndSwap(Buf.Utf8(dtab.show), prevIndex = Some(index)).unit
+  }
 
-  def put(ns: Ns, dtab: Dtab): Future[Unit] = ???
+  def put(ns: Ns, dtab: Dtab): Future[Unit] =
+    root.key(Path.Utf8(ns)).set(Some(Buf.Utf8(dtab.show))).unit
 
   def observe(ns: Ns): Activity[Option[VersionedDtab]] = ???
 }
