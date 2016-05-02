f67ab5261b7bb96dabfa92e82a253a617f373d11
Alex Leong <alex@buoyant.io>
2016-05-02 13:26:10 -0700

    Comment.

--- test-util/src/main/scala/io/buoyant/test/Exceptions.scala
+++ test-util/src/main/scala/io/buoyant/test/Exceptions.scala
@@ -4,6 +4,7 @@ import org.scalatest.Assertions
 
 trait Exceptions extends Assertions {
 
+  // assertThrows is like intercept, but discards the intercepted exception.
   def assertThrows[T <: AnyRef: Manifest](f: => Any): Unit = {
     val _ = intercept[T](f)
   }
