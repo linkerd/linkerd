24582cf20598fdabcee31580321f8d253ae51e55
Alex Leong <alex@buoyant.io>
2016-05-06 12:10:58 -0700

    Rename serverset announcer kind.

--- linkerd/announcer/serversets/src/main/scala/io/l5d/announcer/Serversets.scala
+++ linkerd/announcer/serversets/src/main/scala/io/l5d/announcer/Serversets.scala
@@ -1,14 +1,13 @@
-package io.l5d.zk
+package io.l5d.announcer
 
 import com.twitter.finagle.Announcer
 import com.twitter.finagle.zookeeper.buoyant.ZkAnnouncer
 import io.buoyant.linkerd.{AnnouncerInitializer, AnnouncerConfig}
 
 class ServersetsAnnouncerInitializer extends AnnouncerInitializer {
-  override def configClass = classOf[ServersetsAnnouncerConfig]
-  override def configId = "serversets"
+  override def configClass = classOf[Serversets]
 }
 
-case class ServersetsAnnouncerConfig(hosts: Seq[String], pathPrefix: Option[String]) extends AnnouncerConfig {
+case class Serversets(hosts: Seq[String], pathPrefix: Option[String]) extends AnnouncerConfig {
   override def mk(): Announcer = new ZkAnnouncer(hosts.mkString(","), pathPrefix.getOrElse("/discovery"))
 }
