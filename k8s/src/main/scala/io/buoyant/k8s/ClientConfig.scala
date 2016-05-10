f8fadab9dfac00c5566119a237095b60e731cc6c
Alex Leong <alex@buoyant.io>
2016-05-10 10:57:33 -0700

    Remove TLS support from k8s namer. (#385)
    Remove TLS support from k8s namer.  Starting a local kubectl proxy is the recommended way to securely talk to the k8s cluster API. See BuoyantIO/linkerd-examples#12

--- k8s/src/main/scala/io/buoyant/k8s/ClientConfig.scala
+++ k8s/src/main/scala/io/buoyant/k8s/ClientConfig.scala
@@ -14,9 +14,6 @@ trait ClientConfig {
 
   def host: Option[String]
   def portNum: Option[Int]
-  def tls: Option[Boolean]
-  def tlsWithoutValidation: Option[Boolean]
-  def authTokenFile: Option[String]
 
   protected def getHost = host.getOrElse(DefaultHost)
 
@@ -24,36 +21,13 @@ trait ClientConfig {
 
   protected def dst = s"/$$/inet/$getHost/$getPort"
 
-  private def authFilter = authTokenFile match {
-    case Some(path) =>
-      val token = Source.fromFile(path).mkString
-      if (token.nonEmpty) new AuthFilter(token)
-      else Filter.identity[Request, Response]
-    case None => Filter.identity[Request, Response]
-  }
-
   protected def mkClient(
     params: Stack.Params = Stack.Params.empty
-  ) = {
-    val setHost = new SetHostFilter(getHost, getPort)
-    val client = (tls, tlsWithoutValidation) match {
-      case (Some(false), Some(_)) =>
-        log.warning("tlsWithoutValidation is specified, but has no effect as tls is disabled")
-        Http.client
-      case (Some(false), _) => Http.client
-      case (_, Some(true)) => Http.client.withTlsWithoutValidation
-      case _ => Http.client.withTls(setHost.host)
-    }
-    client.withParams(client.params ++ params)
-      .withStreaming(true)
-      .filtered(setHost)
-      .filtered(authFilter)
-  }
-
+  ) = Http.client.withParams(Http.client.params ++ params).withStreaming(true)
 }
 
 object ClientConfig {
-  val DefaultHost = "kubernetes.default.svc.cluster.local"
+  val DefaultHost = "localhost"
   val DefaultNamespace = "default"
-  val DefaultPort = 443
+  val DefaultPort = 8001
 }
