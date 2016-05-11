f2ec62b70020914964b565d136d986af76762e8d
Kevin Lingerfelt <kl@buoyant.io>
2016-05-10 17:41:09 -0700

    Add configurable http path identifier
    HTTP requests are routed based on a configurable number of "/" separated
    segments from the start of their HTTP path.
    
    For instance, here's a router configured with an http path identifier:
    
    ```yaml
    routers:
    - protocol: http
      dstPrefix: /custom/prefix
      identifier:
        kind: path
        segments: 2
      servers:
        port: 5000
    ```
    
    A request to `:5000/true/love/waits.php` will be identified as
    `/custom/prefix/true/love`.

--- linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/HttpIdentifierConfig.scala
+++ linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/HttpIdentifierConfig.scala
@@ -1,19 +1,17 @@
-package io.l5d
+package io.buoyant.linkerd.protocol
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility
 import com.fasterxml.jackson.annotation.{JsonAutoDetect, JsonIgnore, JsonTypeInfo}
-import com.twitter.finagle.{Dtab, Path, http}
+import com.twitter.finagle.{Dtab, Path}
+import com.twitter.finagle.http.Request
 import io.buoyant.router.RoutingFactory.Identifier
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "kind")
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 trait HttpIdentifierConfig {
-  var httpUriInDst: Option[Boolean] = None
-
   @JsonIgnore
   def newIdentifier(
     prefix: Path,
     baseDtab: () => Dtab = () => Dtab.base
-  ): Identifier[http.Request]
+  ): Identifier[Request]
 }
-
