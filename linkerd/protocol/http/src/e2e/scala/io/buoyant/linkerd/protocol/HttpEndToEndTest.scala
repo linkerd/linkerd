3787fa164faf1012e64991387064b44686fb1204
Alex Leong <alex@buoyant.io>
2016-05-17 15:15:45 -0700

    Fix e2e test.

--- linkerd/protocol/http/src/e2e/scala/io/buoyant/linkerd/protocol/HttpEndToEndTest.scala
+++ linkerd/protocol/http/src/e2e/scala/io/buoyant/linkerd/protocol/HttpEndToEndTest.scala
@@ -11,7 +11,7 @@ import com.twitter.finagle.tracing.{Annotation, BufferingTracer, NullTracer}
 import com.twitter.finagle.util.LoadService
 import com.twitter.util._
 import io.buoyant.router.Http
-import io.buoyant.router.http.MethodAndHostIdentifier$
+import io.buoyant.router.http.MethodAndHostIdentifier
 import io.buoyant.test.Awaits
 import java.net.InetSocketAddress
 import org.scalatest.FunSuite
