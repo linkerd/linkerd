cb05e6b7dc687ebe00c1dcc5627d43d6bd10c080
Alex Leong <alex@buoyant.io>
2016-06-06 14:05:02 -0700

    Use :: as the zk host delimiter in the zk leader namer

--- namer/zk-leader/src/main/scala/io/buoyant/namer/zk/leader.scala
+++ namer/zk-leader/src/main/scala/io/buoyant/namer/zk/leader.scala
@@ -6,17 +6,25 @@ import com.twitter.util.Activity
 import io.buoyant.config.types.{Port, HostAndPort}
 
 /**
- * This namer accepts paths of the form /<zkHosts>/<zkPath>.  The zkPath is the location
- * in ZooKeeper of a leader group.  This namer resolves to the addresses stored in the data of
- * the leader of the group.
+ * This namer accepts paths of the form /<zkHosts>/<zkPath>.  zkHosts is a ::
+ * delimited list of zookeeper hostname:port pairs.  The zkPath is the location
+ * in ZooKeeper of a leader group.  This namer resolves to the addresses stored
+ * in the data of the leader of the group.
+ *
+ * e.g. `/1.2.3.4:8001::5.6.7.8:8001::9.10.11.12:8001/path/to/leader/group`
  */
 class leader extends Namer {
-  override def lookup(path: Path): Activity[NameTree[Name]] = {
+
+  private[zk] def zkLeaderNamer(path: Path): ZkLeaderNamer = {
     val Path.Utf8(hosts) = path.take(1)
-    val zkAddrs = InetSocketAddressUtil.parseHostPorts(hosts).map {
-      case (host, port) => HostAndPort(Some(host), Some(Port(port)))
-    }
-    val namer = new ZkLeaderNamer(Path.empty, zkAddrs)
-    namer.lookup(path.drop(1))
+    val zkAddrs = hosts.split("::")
+      .flatMap(InetSocketAddressUtil.parseHostPorts).map {
+        case (host, port) => HostAndPort(Some(host), Some(Port(port)))
+      }
+    new ZkLeaderNamer(Path.empty, zkAddrs)
+  }
+
+  override def lookup(path: Path): Activity[NameTree[Name]] = {
+    zkLeaderNamer(path).lookup(path.drop(1))
   }
 }
