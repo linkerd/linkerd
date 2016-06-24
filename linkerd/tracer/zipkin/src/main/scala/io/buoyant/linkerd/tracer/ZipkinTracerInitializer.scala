c93c3cf54e95e89593db64fcaf3c3946a945e3cf
Greg Campbell <greg@buoyant.io>
2016-06-24 14:18:25 -0700

    customizable zipkin tracer client.
    Access to the ClientBuilder allows us to do things like disable
    failfast.
    
    A previous version of this branch made some tweaks already; the current
    version copies the ClientBuilder wholesale as a baseline.

--- linkerd/tracer/zipkin/src/main/scala/io/buoyant/linkerd/tracer/ZipkinTracerInitializer.scala
+++ linkerd/tracer/zipkin/src/main/scala/io/buoyant/linkerd/tracer/ZipkinTracerInitializer.scala
@@ -1,9 +1,17 @@
 package io.buoyant.linkerd.tracer
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import com.twitter.finagle.tracing.{Record, TraceId, Tracer}
-import com.twitter.finagle.zipkin.thrift.{Sampler, ZipkinTracer}
+import com.twitter.conversions.time._
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.stats.{ClientStatsReceiver, NullStatsReceiver}
+import com.twitter.finagle.thrift.{Protocols, ThriftClientFramedCodec}
+import com.twitter.finagle.tracing.{Record, Trace, TraceId, Tracer}
+import com.twitter.finagle.util.DefaultTimer
+import com.twitter.finagle.zipkin.thrift.{RawZipkinTracer, Sampler, ZipkinTracer}
+import com.twitter.finagle.zipkin.thriftscala.Scribe
 import io.buoyant.linkerd.{TracerConfig, TracerInitializer}
+import java.net.InetSocketAddress
 
 class ZipkinTracerInitializer extends TracerInitializer {
   val configClass = classOf[ZipkinConfig]
@@ -20,15 +28,50 @@ case class ZipkinConfig(
 
   @JsonIgnore
   override def newTracer(): Tracer = new Tracer {
-    private[this] val underlying: Tracer =
-      ZipkinTracer.mk(
-        host = host.getOrElse("localhost"),
-        port = port.getOrElse(9410),
-        sampleRate = sampleRate.map(_.toFloat).getOrElse(Sampler.DefaultSampleRate)
+    private[this] val underlying: Tracer = {
+      val transport = ClientBuilder()
+        .name("zipkin-tracer")
+        .hosts(new InetSocketAddress(host.getOrElse("localhost"), port.getOrElse(9410)))
+        .codec(ThriftClientFramedCodec())
+        .reportTo(ClientStatsReceiver)
+        .hostConnectionLimit(5)
+        // using an arbitrary, but bounded number of waiters to avoid memory leaks
+        .hostConnectionMaxWaiters(250)
+        // somewhat arbitrary, but bounded timeouts
+        .timeout(1.second)
+        .daemon(true)
+        .build()
+
+      val client = new Scribe.FinagledClient(
+        new TracelessFilter andThen transport,
+        Protocols.binaryFactory()
       )
+
+      val rawTracer = RawZipkinTracer(
+        client,
+        NullStatsReceiver,
+        DefaultTimer.twitter
+      )
+      new ZipkinTracer(
+        rawTracer,
+        sampleRate.map(_.toFloat).getOrElse(Sampler.DefaultSampleRate)
+      )
+    }
 
     def sampleTrace(t: TraceId) = underlying.sampleTrace(t)
     def record(r: Record) = underlying.record(r)
   }
 }
 
+/**
+ * _Copied from Finagle's RawZipkinTracer.scala_
+ *
+ * Makes sure we don't trace the Scribe logging.
+ */
+private class TracelessFilter[Req, Rep] extends SimpleFilter[Req, Rep] {
+  def apply(request: Req, service: Service[Req, Rep]) = {
+    Trace.letClear {
+      service(request)
+    }
+  }
+}
