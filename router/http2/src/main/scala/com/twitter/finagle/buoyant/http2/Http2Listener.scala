430fae0705e314788ff7d64eae01b3127aa1cc8a
Oliver Gould <ver@buoyant.io>
2016-07-05 06:58:22 +0000

    seems to transcode messages

--- router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/Http2Listener.scala
+++ router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/Http2Listener.scala
@@ -5,7 +5,6 @@ import com.twitter.finagle.netty4.{Netty4Listener, Netty4Transporter}
 import com.twitter.finagle.server.Listener
 import com.twitter.logging.Logger
 import io.netty.channel._
-import io.netty.handler.logging.LoggingHandler
 import io.netty.handler.codec.http2._
 import io.netty.channel.socket.SocketChannel
 import scala.language.implicitConversions
@@ -16,37 +15,37 @@ import scala.language.implicitConversions
 object Http2Listener {
   private[this] val log = Logger.get(getClass.getName)
 
-  private trait ToUnit { def unit: Unit }
-  private object ToUnit extends ToUnit { val unit = () }
-  private implicit def toUnit(a: Any): ToUnit = ToUnit
+  private trait ToIgnoreMe { def ignoreme: Unit }
+  private object ToIgnoreMe extends ToIgnoreMe { val ignoreme = () }
+  private implicit def toIgnoreMe(a: Any): ToIgnoreMe = ToIgnoreMe
 
   private[this] val mkHttp2: ChannelInitializer[Channel] => ChannelHandler =
     stream => new ChannelInitializer[SocketChannel] {
       def initChannel(ch: SocketChannel): Unit = {
-        ch.pipeline.addLast("debug.raw", new DebugHandler("srv.raw")).unit
+        ch.pipeline.addLast("debug.raw", new DebugHandler("srv.raw")).ignoreme
 
-        ch.pipeline.addLast("framer", new Http2FrameCodec(true)).unit
-        ch.pipeline.addLast("debug.frame", new DebugHandler("srv.frame")).unit
+        ch.pipeline.addLast("framer", new Http2FrameCodec(true)).ignoreme
+        ch.pipeline.addLast("debug.frame", new DebugHandler("srv.frame")).ignoreme
 
         // If we want to intercept things like Settings messages, this
         // might be a good place to do that.  Eventually, we'll want
         // to surface window settings on streams across clients/servers.
 
-        ch.pipeline.addLast("muxer", new Http2MultiplexCodec(true, null, stream)).unit
-        ch.pipeline.addLast("debug.mux", new DebugHandler("srv.mux")).unit
+        ch.pipeline.addLast("muxer", new Http2MultiplexCodec(true, null, stream)).ignoreme
+        ch.pipeline.addLast("debug.mux", new DebugHandler("srv.mux")).ignoreme
 
         log.info(s"srv: ${ch} ${ch.pipeline}")
       }
     }
 
-  private[this] val pipelineInit: ChannelPipeline => Unit = { pipeline =>
+  private[this] val prepareStream: ChannelPipeline => Unit = { pipeline =>
     pipeline.addLast("debug.stream", new DebugHandler("srv.stream"))
     log.info(s"srv.stream.pipeline: $pipeline")
   }
 
-  def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2Frame] =
+  def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2StreamFrame] =
     Netty4Listener(
-      pipelineInit = pipelineInit,
+      pipelineInit = prepareStream,
       // XXX Netty4's Http2 Codec doesn't support backpressure yet.
       // See https://github.com/netty/netty/issues/3667#issue-69640214
       params = params + Netty4Listener.BackPressure(false),
--- router/http2/src/main/scala/io/buoyant/router/http2/Main.scala
+++ router/http2/src/main/scala/io/buoyant/router/http2/Main.scala
@@ -1,43 +1,31 @@
 package io.buoyant.router.http2
 
-import com.twitter.finagle.Stack
-import com.twitter.finagle.buoyant.http2.Http2Listener
+import com.twitter.finagle.{Service, Stack}
+import com.twitter.finagle.buoyant.http2._
 import com.twitter.finagle.transport.Transport
 import com.twitter.server.TwitterServer
-import com.twitter.util.{Await, Return, Throw}
+import com.twitter.util.{Await, Future}
 import io.netty.handler.codec.http2.Http2StreamFrame
 import java.net.InetSocketAddress
+import java.util.concurrent.ConcurrentHashMap
 
 object Main extends TwitterServer {
 
   def main(): Unit = {
-    val params = Stack.Params.empty +
-      Transport.Verbose(true)
+    val addr = new InetSocketAddress("127.1", 4142)
 
-    val listener = Http2Listener.mk(params)
+    val listener = Http2Listener.mk(Stack.Params.empty)
 
-    log.info("Main: listening on 127.1:4142")
-    val listening = listener.listen(new InetSocketAddress("127.1", 4142))(transporter)
-    closeOnExit(listening)
-    Await.result(listening)
-  }
-
-  def transporter[I, O] = { transport: Transport[I, O] =>
-    def readLoop(): Unit = {
-      log.info(s"Main: ${transport.remoteAddress} reading")
-
-      val _ = transport.read().respond {
-        case Throw(e) =>
-          log.error(e, s"Main: ${transport.remoteAddress} error")
-          val _ = transport.close()
-
-        case Return(frame) =>
-          log.info(s"Main: ${transport.remoteAddress} read ${frame}")
-          readLoop()
-      }
+    val service = Service.mk[Request, Response] { req =>
+      Future.exception(new Exception("ughit"))
     }
 
-    readLoop()
+    log.info(s"Main: listening on $addr")
+    val listening = listener.listen(addr) { transport: Transport[Http2StreamFrame, Http2StreamFrame] =>
+      val _ = ServerDispatcher.dispatch(transport, service)
+    }
+    closeOnExit(listening)
+    Await.result(listening)
   }
 
 }
