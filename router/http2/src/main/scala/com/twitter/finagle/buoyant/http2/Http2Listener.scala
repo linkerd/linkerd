023e76ead34529f2ed9593b9b302229c3ceda35b
Oliver Gould <ver@buoyant.io>
2016-07-25 22:44:53 +0000

    listener touchup

--- router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/Http2Listener.scala
+++ router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/Http2Listener.scala
@@ -1,6 +1,5 @@
 package com.twitter.finagle.buoyant.http2
 
-import com.twitter.finagle.{Stack, param}
 import com.twitter.finagle.netty4.Netty4Listener
 import com.twitter.finagle.server.Listener
 import io.netty.channel.{Channel, ChannelInitializer, ChannelPipeline}
@@ -12,39 +11,37 @@ import io.netty.handler.codec.http2.{Http2FrameCodec, Http2MultiplexCodec, Http2
 object Http2Listener {
 
   def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2StreamFrame] = {
-    val param.Stats(statsReceiver) = params[param.Stats]
-    val connStats = statsReceiver.scope("conn")
-    val streamStats = statsReceiver.scope("stream")
-
     /*
      * XXX The stream is configured with Netty4ServerChannelinitializer,
      * which expects that the inbound side of the pipeline transmits
      * bytes. However, the HTTP/2 listener uses frames and
      * de-multiplexes the connection earlier in the pipeline so that
      * each child stream transmits Http2StreamFrame objects.
+     *
+     * ChannelStatsHandler logs a tremendous amount of errors when
+     * it processes non-ByteBuf messages, and so for now we just
+     * remove it from each stream pipeline.
      */
 
     def initHttp2Connection(stream: ChannelInitializer[Channel]) =
       new ChannelInitializer[Channel] {
         def initChannel(ch: Channel): Unit = {
-          val _ = ch.pipeline.addLast(
-            // new TimingHandler(connStats.scope("outer")),
-            new Http2FrameCodec(true /*server*/ ),
-            // new DebugHandler("srv.conn"),
-            // new Http2FrameStatsHandler(statsReceiver.scope("conn")),
-            // new TimingHandler(connStats.scope("inner")),
-            new Http2MultiplexCodec(true /*server*/ , null, prepChildStream(stream))
+          // new TimingHandler(connStats.scope("outer")),
+
+          ch.pipeline.addLast(new Http2FrameCodec(true /*server*/ ))
+
+          // new DebugHandler("srv.conn"),
+          // new Http2FrameStatsHandler(statsReceiver.scope("conn")),
+          // new TimingHandler(connStats.scope("inner")),
+
+          val child = prepChildStream(stream)
+          val _ = ch.pipeline.addLast(new Http2MultiplexCodec(true /*server*/ , null, child))
+
           // No events happen on the pipeline after the muxer, since
           // it dispatches events onto the stream pipeline.
-          )
         }
       }
 
-    /*
-     * XXX ChannelStatsHandler logs a tremendous amount of errors when
-     * it processes non-ByteBuf messages, and so for now we just
-     * remove it from each stream pipeline.
-     */
     def prepChildStream(stream: ChannelInitializer[Channel]) =
       new ChannelInitializer[Channel] {
         def initChannel(ch: Channel): Unit = {
@@ -58,9 +55,9 @@ object Http2Listener {
         }
       }
 
-    // There's no need to configure anything on the stream channel, but
-    // if we wanted to do install anything on each stream, this would be
-    // where it happens.
+    // There's no need to configure anything on the stream channel,
+    // but if we wanted to do install anything on each stream, this
+    // would be where it happens.
     def initHttp2Stream(pipeline: ChannelPipeline): Unit = {}
 
     Netty4Listener(
