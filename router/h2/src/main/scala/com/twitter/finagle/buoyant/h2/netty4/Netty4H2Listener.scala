95632dae4a2ca49746f78a94d1bd85939ea26d14
Oliver Gould <ver@buoyant.io>
2016-07-27 07:29:22 +0000

    Support H2C and HTTP upgrade in servers

--- router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4H2Listener.scala
+++ router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4H2Listener.scala
@@ -4,8 +4,9 @@ package netty4
 import com.twitter.finagle.Stack
 import com.twitter.finagle.netty4.Netty4Listener
 import com.twitter.finagle.server.Listener
-import io.netty.channel.{Channel, ChannelInitializer, ChannelPipeline}
-import io.netty.handler.codec.http.HttpServerUpgradeHandler
+import io.netty.buffer.ByteBuf
+import io.netty.channel._
+import io.netty.handler.codec.http.{HttpServerCodec, HttpServerUpgradeHandler}
 import io.netty.handler.codec.http2._
 import io.netty.util.AsciiString
 
@@ -13,6 +14,11 @@ import io.netty.util.AsciiString
  * Please note that the listener cannot be used for TLS yet.
  */
 object Netty4Http2Listener {
+
+  // private val log = com.twitter.logging.Logger.get(getClass.getName)
+
+  val H2UpgraderKey = "h2 upgrader"
+  val H2FramerKey = "h2 framer"
 
   def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2StreamFrame] = {
     /*
@@ -27,37 +33,23 @@ object Netty4Http2Listener {
      * remove it from each stream pipeline.
      */
 
-    def initHttp2Connection(stream: ChannelInitializer[Channel]) =
-      new ChannelInitializer[Channel] {
-        def initChannel(ch: Channel): Unit = {
-          // new TimingHandler(connStats.scope("outer")),
-
-          ch.pipeline.addLast(new Http2FrameCodec(true /*server*/ ))
-
-          // new DebugHandler("srv.conn"),
-          // new Http2FrameStatsHandler(statsReceiver.scope("conn")),
-          // new TimingHandler(connStats.scope("inner")),
-
-          val child = prepChildStream(stream)
-          val _ = ch.pipeline.addLast(new Http2MultiplexCodec(true /*server*/ , null, child))
-
-          // No events happen on the pipeline after the muxer, since
-          // it dispatches events onto the stream pipeline.
-        }
+    def initHttp2Connection(stream: ChannelInitializer[Channel]) = new ChannelInitializer[Channel] {
+      def initChannel(ch: Channel): Unit = {
+        val _ = ch.pipeline.addLast(H2UpgraderKey, new H2Upgrader(prepChildStream(stream)))
       }
+    }
 
-    def prepChildStream(stream: ChannelInitializer[Channel]) =
-      new ChannelInitializer[Channel] {
-        def initChannel(ch: Channel): Unit = {
-          // ch.pipeline.addLast(new Http2FrameStatsHandler(statsReceiver.scope("stream")))
-          ch.pipeline.addLast(stream)
-          val _ = ch.pipeline.addLast(new ChannelInitializer[Channel] {
-            def initChannel(ch: Channel): Unit = {
-              val _ = ch.pipeline.remove("channel stats")
-            }
-          })
-        }
+    def prepChildStream(stream: ChannelInitializer[Channel]) = new ChannelInitializer[Channel] {
+      def initChannel(ch: Channel): Unit = {
+        // ch.pipeline.addLast(new Http2FrameStatsHandler(statsReceiver.scope("stream")))
+        ch.pipeline.addLast(stream)
+        val _ = ch.pipeline.addLast(new ChannelInitializer[Channel] {
+          def initChannel(ch: Channel): Unit = {
+            val _ = ch.pipeline.remove("channel stats")
+          }
+        })
       }
+    }
 
     // There's no need to configure anything on the stream channel,
     // but if we wanted to do install anything on each stream, this
@@ -73,20 +65,52 @@ object Netty4Http2Listener {
     )
   }
 
-  class DeteectH2Preface(handlerH1: ChannelHandler, handlerH2: ChannelHandler)
-      extends ChannelDuplexHandler {
+  /**
+   * Accept either H2C requests (beginning with a Connection preface)
+   * or HTTP requests with h2c protocol upgrading.
+   */
+  private class H2Upgrader(stream: ChannelHandler) extends ChannelDuplexHandler {
+    private[this] val prefaceBuf = Http2CodecUtil.connectionPrefaceBuf
+    private[this] val prefaceLen = prefaceBuf.readableBytes
 
-    def channelRead(ctx: ChannelHandlerContext, obj: Any): Unit = obj match {
-      case bb: ByteBuf =>
-        val preface = Http2CodecUtil.connectionPrefaceBuf
-        if (bb.readableBytes >= preface.readableBytes) {
-        } else {
-          // TODO buffer at least preface.readableBytes from bb
-          val _ = ctx.fireChannelRead(bb)
-        }
+    private[this] def isPreface(bb: ByteBuf) =
+      (bb.readableBytes >= prefaceLen &&
+        bb.slice(0, prefaceLen).equals(prefaceBuf))
 
-      case obj =>
-        val _ = ctx.fireChannelRead(obj)
+    private[this] val rawH2 = new Http2Codec(true /*server*/ , stream)
+
+    private[this] val upgradedH2 = new HttpServerUpgradeHandler.UpgradeCodecFactory {
+      override def newUpgradeCodec(proto: CharSequence): HttpServerUpgradeHandler.UpgradeCodec =
+        if (isH2C(proto)) new Http2ServerUpgradeCodec(rawH2) else null
+    }
+
+    /**
+     * Detect the HTTP2 connection preface to support Prior Knowledge
+     * HTTP2 (i.e. gRPC). If that doesn't exist
+     */
+    override def channelRead(ctx: ChannelHandlerContext, obj: Any): Unit = {
+      obj match {
+        case bb: ByteBuf if isPreface(bb) =>
+          // TODO buffer at least preface.readableBytes from bb --
+          // there's got to be a utility handler for this, right?
+          ctx.pipeline.addLast("h2", rawH2)
+
+        case bb: ByteBuf => // Try to upgrade from HTTP1
+          val h1 = new HttpServerCodec
+          ctx.pipeline.addLast("h1", h1)
+          ctx.pipeline.addLast("h1 to h2", new HttpServerUpgradeHandler(h1, upgradedH2))
+
+        case _ => // iunno, just pass it on.
+      }
+
+      // Stop trying to upgrade the protocol.
+      ctx.pipeline.remove(this)
+
+      // Pass it on.
+      val _ = ctx.fireChannelRead(obj)
     }
   }
+
+  private def isH2C(proto: CharSequence): Boolean =
+    AsciiString.contentEquals(Http2CodecUtil.HTTP_UPGRADE_PROTOCOL_NAME, proto)
 }
