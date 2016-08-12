64ad916c10e8f138fdf6937be684f8c171659241
Risha Mars <mars@buoyant.io>
2016-08-12 14:14:45 -0700

    Dashboard: add toggling to the router clients to better handle large numbers of clients
    * All clients under a router now collapse if there are more than 6 clients.
    * Collapsed clients remove themselves from the list of metricsListeners on metricsCollector,
    which should result in much less work being done for a large number of clients.

--- admin/src/main/resources/io/buoyant/admin/js/router_clients.js
+++ admin/src/main/resources/io/buoyant/admin/js/router_clients.js
@@ -1,6 +1,8 @@
 /* globals CombinedClientGraph, RouterClient */
 /* exported RouterClients */
 var RouterClients = (function() {
+  var EXPAND_CLIENT_THRESHOLD = 6;
+
   function assignColorsToClients(colors, clients) {
     var colorIdx = 0;
 
@@ -10,19 +12,29 @@ var RouterClients = (function() {
     }, {});
   }
 
+  function shouldExpandClients(numClients) {
+    // if there are many clients, collapse them by default to improve page perfomance
+    return numClients < EXPAND_CLIENT_THRESHOLD;
+  }
+
   return function (metricsCollector, routers, $clientEl, routerName, clientTemplate, metricPartial, clientContainerTemplate, colors) {
     var clientToColor = assignColorsToClients(colors, routers.clients(routerName));
     var combinedClientGraph = CombinedClientGraph(metricsCollector, routerName, $clientEl.find(".router-graph"), clientToColor);
     var clients = routers.clients(routerName);
 
-    var routerClients = _.map(clients, initializeClient);
+    var expandClients = shouldExpandClients(clients.length);
+
+    var routerClients = _.map(clients, function(client) {
+      return initializeClient(client, expandClients);
+    });
+
     if (routerClients.length == 0) {
       $clientEl.hide();
     }
 
     routers.onAddedClients(addClients);
 
-    function initializeClient(client) {
+    function initializeClient(client, shouldExpand) {
       $clientEl.show();
       var colorsForClient = clientToColor[client.label];
       var $container = $(clientContainerTemplate({
@@ -31,8 +43,9 @@ var RouterClients = (function() {
       })).appendTo($clientEl);
       var $metrics = $container.find(".metrics-container");
       var $chart = $container.find(".chart-container");
+      var $toggle = $container.find(".client-toggle");
 
-      return RouterClient(metricsCollector, routers, client, $metrics, routerName, clientTemplate, metricPartial, $chart, colorsForClient);
+      return RouterClient(metricsCollector, routers, client, $metrics, routerName, clientTemplate, metricPartial, $chart, colorsForClient, $toggle, shouldExpand);
     }
 
     function addClients(addedClients) {
@@ -46,11 +59,13 @@ var RouterClients = (function() {
         routerClient.updateColors(clientToColor);
       });
 
+      var expandClients = shouldExpandClients(addedClients.length + routerClients.length);
+
       // add new clients
       _.chain(addedClients)
         .filter(function(client) { return client.router === routerName })
         .each(function(clientForRouter) {
-          routerClients.push(initializeClient(clientForRouter));
+          routerClients.push(initializeClient(clientForRouter, expandClients));
         })
         .value();
     }
