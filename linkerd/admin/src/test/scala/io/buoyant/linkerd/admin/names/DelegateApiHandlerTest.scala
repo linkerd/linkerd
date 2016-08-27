472a862dcecdc14191a688c58efffd8309fdf0c2
Alex Leong <alex@buoyant.io>
2016-08-26 17:52:54 -0700

    Add test

--- linkerd/admin/src/test/scala/io/buoyant/linkerd/admin/names/DelegateApiHandlerTest.scala
+++ linkerd/admin/src/test/scala/io/buoyant/linkerd/admin/names/DelegateApiHandlerTest.scala
@@ -81,6 +81,7 @@ class DelegateApiHandlerTest extends FunSuite with Awaits {
     req.uri = s"/delegate?path=invalid-param&namespace=label"
     val rsp = await(web(req))
     assert(rsp.status == Status.BadRequest)
+    assert(rsp.contentString == "Invalid path: '/' expected but 'i' found at '[i]nvalid-param'")
   }
 
   test("invalid dtab results in 400") {
@@ -89,5 +90,6 @@ class DelegateApiHandlerTest extends FunSuite with Awaits {
     req.uri = s"/delegate?path=/boo/humbug&dtab=invalid-param"
     val rsp = await(web(req))
     assert(rsp.status == Status.BadRequest)
+    assert(rsp.contentString == "Invalid dtab: '/' expected but 'i' found at '[i]nvalid-param'")
   }
 }
