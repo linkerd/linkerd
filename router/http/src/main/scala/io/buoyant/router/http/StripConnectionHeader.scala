66c84d94256d0aeace20a3e6c184e2d74f724bbb
fantayeneh <fantayeneh@gmail.com>
2016-09-17 17:38:40 +0100

    Prefer Nil over Seq()

--- router/http/src/main/scala/io/buoyant/router/http/StripConnectionHeader.scala
+++ router/http/src/main/scala/io/buoyant/router/http/StripConnectionHeader.scala
@@ -7,13 +7,13 @@ import com.twitter.finagle.{Service, ServiceFactory, SimpleFilter, Stack}
 object StripConnectionHeader {
 
   /**
-   * Removes the 'Connection' header from (i.e. downstream) requests.
+   * Removes the 'Connection' and header and any header it list from (i.e. downstream) requests.
    */
   object filter extends SimpleFilter[Request, Response] {
     def apply(req: Request, svc: Service[Request, Response]) = {
       val headersListedInConnection: Seq[String] = req.headerMap.remove(Connection) match {
         case Some(s) => s.split(",").map(_.trim).filter(_.nonEmpty)
-        case None => Seq()
+        case None => Nil
       }
       headersListedInConnection.foreach(req.headerMap.remove(_))
       svc(req)
@@ -22,7 +22,7 @@ object StripConnectionHeader {
 
   object module extends Stack.Module0[ServiceFactory[Request, Response]] {
     val role = Stack.Role("StripConnectionHeader")
-    val description = "Removes the 'Connection' header from requests"
+    val description = "Removes the 'Connection' header and any header it list from requests"
 
     def make(next: ServiceFactory[Request, Response]) =
       filter andThen next
