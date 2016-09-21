468b7dce1edfdfb80bf7cfda9834f3a167b53b96
ncohensm <ncohensm@cisco.com>
2016-09-21 15:44:01 -0700

    final working canary routing

--- namer/consul/src/main/scala/io/buoyant/namer/consul/ConsulCanaryNamer.scala
+++ namer/consul/src/main/scala/io/buoyant/namer/consul/ConsulCanaryNamer.scala
@@ -22,26 +22,12 @@ object ConsulCanaryNamer {
   private[this] class TaggedNamer(lookup: LookupCache, prefix: Path) extends Namer {
 
     def lookup(path: Path): Activity[NameTree[Name]] = {
-      System.err.format("ConsulCanaryNamer: got path: %s\n\n", path.toString())
-      path.take(4) match {
-        case id@Path.Utf8(dc, tag, canary, service) =>
-          val canarytag: Option[String] =
-            canary match {
-              case "enabled" if tag == "canary" =>
-                Option("canary")
-              case "disabled" =>
-                None
-              case "always" =>
-                Option("canary")
-              case _ =>
-                None
-            }
-          val k = SvcKey(service, canarytag)
-          val result = lookup(dc, k, prefix ++ id, path.drop(4))
-          System.err.format("ConsulCanaryNamer: lookup result: %s\n\n", result.toString)
+      path.take(3) match {
+        case id@Path.Utf8(dc, tag, service) =>
+          val k = SvcKey(service, Option(tag))
+          val result = lookup(dc, k, prefix ++ id, path.drop(3))
           result
         case _ => {
-          System.err.format("ConsulCanaryNamer: Negative lookup result!\n\n")
           Activity.value(NameTree.Neg)
         }
       }
--- router/http/src/main/scala/io/buoyant/router/http/CanaryAndServiceIdentifier.scala
+++ router/http/src/main/scala/io/buoyant/router/http/CanaryAndServiceIdentifier.scala
@@ -7,14 +7,14 @@ import com.twitter.util.Future
 import io.buoyant.router.RoutingFactory
 import io.buoyant.router.RoutingFactory.{IdentifiedRequest, RequestIdentification, UnidentifiedRequest}
 
-object MethodAndServiceIdentifier {
+object CanaryAndServiceIdentifier {
   def mk(
     prefix: Path,
     baseDtab: () => Dtab = () => Dtab.base
-  ): RoutingFactory.Identifier[Request] = MethodAndServiceIdentifier(prefix, false, baseDtab)
+  ): RoutingFactory.Identifier[Request] = CanaryAndServiceIdentifier(prefix, false, baseDtab)
 }
 
-case class MethodAndServiceIdentifier(
+case class CanaryAndServiceIdentifier(
   prefix: Path,
   uris: Boolean = false,
   baseDtab: () => Dtab = () => Dtab.base
@@ -35,8 +35,8 @@ case class MethodAndServiceIdentifier(
       req.host match {
         case Some(host) if host.nonEmpty =>
           val canary = req.headerMap.get("x-cisco-spark-canary-opts").getOrElse("disabled")
-          val dst = mkPath(Path.Utf8("1.1", req.method.toString, canary, host.split('.')(0)) ++ suffix(req))
-          System.err.format("MethodAndServiceIdentifier: returning path: %s\n\n", dst.toString)
+          val dst = mkPath(Path.Utf8("1.1", canary, host.split('.')(0)) ++ suffix(req))
+          System.err.format("CanaryAndServiceIdentifier: returning path: %s\n\n", dst.toString)
           Future.value(new IdentifiedRequest(dst, req))
         case _ =>
           Future.value(
