0caa6c07b444f0acaab6fa549dc7c05a8e9a36c8
Oliver Gould <ver@buoyant.io>
2016-09-30 23:13:57 +0000

    Begin to rework h2 server support using the newer finagle-netty4 APIs.
    Doesn't compile.

--- router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4H2Listener.scala
+++ router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4H2Listener.scala
@@ -6,60 +6,47 @@ import com.twitter.finagle.netty4.Netty4Listener
 import com.twitter.finagle.server.Listener
 import io.netty.buffer.ByteBuf
 import io.netty.channel._
-import io.netty.handler.codec.http.{HttpServerCodec, HttpServerUpgradeHandler}
-import io.netty.handler.codec.http2._
-import io.netty.util.AsciiString
+import io.netty.handler.codec.http2.{Http2Codec, Http2StreamFrame}
 
+/**
+ * Based on com.twitter.finagle.http2.Http2Listener
+ */
 object Netty4H2Listener {
 
-  // private val log = com.twitter.logging.Logger.get(getClass.getName)
-
-  val H2UpgraderKey = "h2 upgrader"
-  val H2FramerKey = "h2 framer"
-
   def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2StreamFrame] = {
-    /*
-     * XXX The stream is configured with Netty4ServerChannelinitializer,
-     * which expects that the inbound side of the pipeline transmits
-     * bytes. However, the HTTP/2 listener uses frames and
-     * de-multiplexes the connection earlier in the pipeline so that
-     * each child stream transmits Http2StreamFrame objects.
-     *
-     * ChannelStatsHandler logs a tremendous amount of errors when
-     * it processes non-ByteBuf messages, and so for now we just
-     * remove it from each stream pipeline.
-     */
-
-    def initHttp2Connection(stream: ChannelInitializer[Channel]) = new ChannelInitializer[Channel] {
-      def initChannel(ch: Channel): Unit = {
-        val _ = ch.pipeline.addLast(H2UpgraderKey, new H2Upgrader(prepChildStream(stream)))
-      }
-    }
-
-    def prepChildStream(stream: ChannelInitializer[Channel]) = new ChannelInitializer[Channel] {
-      def initChannel(ch: Channel): Unit = {
-        // ch.pipeline.addLast(new Http2FrameStatsHandler(statsReceiver.scope("stream")))
-        ch.pipeline.addLast(stream)
-        val _ = ch.pipeline.addLast(new ChannelInitializer[Channel] {
-          def initChannel(ch: Channel): Unit = {
-            // XXX this was necessary but maybe isn't now..
-            // val _ = ch.pipeline.remove("channel stats")
-          }
-        })
-      }
-    }
-
-    // There's no need to configure anything on the stream channel,
-    // but if we wanted to do install anything on each stream, this
-    // would be where it happens.
-    def initHttp2Stream(pipeline: ChannelPipeline): Unit = {}
-
-    Netty4Listener(
-      pipelineInit = initHttp2Stream,
-      handlerDecorator = initHttp2Connection,
-      // Netty4's Http2 Codec doesn't allow finagle-style
-      // backpressure; we instead rely on Http2's flow control.
-      params = params + Netty4Listener.BackPressure(false)
-    )
+    PriorKnowledgeListener.mk(params)
   }
+
+  private[this] object PriorKnowledgeListener {
+
+    def mk(params: Stack.Params): Listener[Http2StreamFrame, Http2StreamFrame] =
+      Netty4Listener(
+        pipelineInit = pipelineInit,
+        params = params + Netty4Listener.BackPressure(false),
+        setupMarshalling = setupMarshalling
+      )
+
+    val PlaceholderKey = "prior knowledge placeholder"
+    private[this] object PlaceholderHandler extends ChannelDuplexHandler
+
+    // we inject a dummy handler so we can replace it with the real stuff
+    // after we get `init` in the setupMarshalling phase.
+    private[this] val pipelineInit: ChannelPipeline => Unit = { pipeline =>
+      val _ = pipeline.addLast(PlaceholderKey, PlaceholderHandler)
+    }
+
+    private[this] val setupMarshalling: ChannelInitializer[Channel] => ChannelHandler = { init =>
+      val initializer = new ChannelInitializer[Channel] {
+        def initChannel(ch: Channel): Unit = {
+          val _ = ch.pipeline.addLast(init)
+        }
+      }
+      new ChannelInitializer[Channel] {
+        def initChannel(ch: Channel): Unit = {
+          val _ = ch.pipeline.replace(PlaceholderKey, "h2Codec", new Http2Codec(true, initializer))
+        }
+      }
+    }
+  }
+
 }
---
+++ router/h2/src/main/scala/io/buoyant/router/h2/PathIdentifier.scala
@@ -0,0 +1,40 @@
+package io.buoyant.router.h2
+
+import com.twitter.finagle.{Dtab, Path, Stack}
+import com.twitter.finagle.buoyant.Dst
+import com.twitter.finagle.buoyant.h2.Request
+import com.twitter.util.Future
+import io.buoyant.router.H2
+import io.buoyant.router.RoutingFactory._
+
+object PathIdentifier {
+  def mk(params: Stack.Params) = {
+    val DstPrefix(pfx) = params[DstPrefix]
+    val BaseDtab(baseDtab) = params[BaseDtab]
+    new PathIdentifier(pfx, baseDtab)
+  }
+
+  val param = H2.Identifier(mk)
+}
+
+class PathIdentifier(pfx: Path, baseDtab: () => Dtab)
+  extends Identifier[Request] {
+
+  override def apply(req: Request): Future[RequestIdentification[Request]] = {
+    val dst = Dst.Path(pfx ++ reqPath(req), baseDtab(), Dtab.empty)
+    Future.value(new IdentifiedRequest(dst, req))
+  }
+
+  private def reqPath(req: Request): Path = req.path match {
+    case "" | "/" => Path.empty
+    case UriPath(path) => Path.read(path)
+  }
+
+  private object UriPath {
+    def unapply(uri: String): Option[String] =
+      uri.indexOf('?') match {
+        case -1 => Some(uri.stripSuffix("/"))
+        case idx => Some(uri.substring(idx + 1).stripSuffix("/"))
+      }
+  }
+}
