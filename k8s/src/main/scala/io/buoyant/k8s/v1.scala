5f0888bc694ab8ef362071b41e734fdca521fc7d
Alex Leong <alex@buoyant.io>
2016-10-14 14:39:11 -0700

    Add k8s-external namer (#729)
    * Add ServiceNamer
    
    * Add k8s external namer

--- k8s/src/main/scala/io/buoyant/k8s/v1.scala
+++ k8s/src/main/scala/io/buoyant/k8s/v1.scala
@@ -1,12 +1,12 @@
 package io.buoyant.k8s
 
 import com.fasterxml.jackson.annotation.{JsonProperty, JsonSubTypes, JsonTypeInfo}
-import com.twitter.finagle.{Service, http}
+import com.twitter.finagle.{Service => FService, http}
 import io.buoyant.k8s.{KubeObject => BaseObject}
 
 package object v1 {
 
-  type Client = Service[http.Request, http.Response]
+  type Client = FService[http.Request, http.Response]
 
   val group = "api"
   val version = "v1"
@@ -15,31 +15,60 @@ package object v1 {
 
   @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
   @JsonSubTypes(Array(
-    new JsonSubTypes.Type(value = classOf[Added], name = "ADDED"),
-    new JsonSubTypes.Type(value = classOf[Modified], name = "MODIFIED"),
-    new JsonSubTypes.Type(value = classOf[Deleted], name = "DELETED"),
-    new JsonSubTypes.Type(value = classOf[Error], name = "ERROR")
+    new JsonSubTypes.Type(value = classOf[EndpointsAdded], name = "ADDED"),
+    new JsonSubTypes.Type(value = classOf[EndpointsModified], name = "MODIFIED"),
+    new JsonSubTypes.Type(value = classOf[EndpointsDeleted], name = "DELETED"),
+    new JsonSubTypes.Type(value = classOf[EndpointsError], name = "ERROR")
   ))
   sealed trait EndpointsWatch extends Watch[Endpoints]
-  case class Added(
+  case class EndpointsAdded(
     `object`: Endpoints
   ) extends EndpointsWatch with Watch.Added[Endpoints]
 
-  case class Modified(
+  case class EndpointsModified(
     `object`: Endpoints
   ) extends EndpointsWatch with Watch.Modified[Endpoints]
 
-  case class Deleted(
+  case class EndpointsDeleted(
     `object`: Endpoints
   ) extends EndpointsWatch with Watch.Deleted[Endpoints]
 
-  case class Error(
+  case class EndpointsError(
     @JsonProperty(value = "object") status: Status
   ) extends EndpointsWatch with Watch.Error[Endpoints]
 
   implicit object EndpointsDescriptor extends ObjectDescriptor[Endpoints, EndpointsWatch] {
     def listName = "endpoints"
-    def toWatch(e: Endpoints) = Modified(e)
+    def toWatch(e: Endpoints) = EndpointsModified(e)
+  }
+
+  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+  @JsonSubTypes(Array(
+    new JsonSubTypes.Type(value = classOf[ServiceAdded], name = "ADDED"),
+    new JsonSubTypes.Type(value = classOf[ServiceModified], name = "MODIFIED"),
+    new JsonSubTypes.Type(value = classOf[ServiceDeleted], name = "DELETED"),
+    new JsonSubTypes.Type(value = classOf[ServiceError], name = "ERROR")
+  ))
+  sealed trait ServiceWatch extends Watch[Service]
+  case class ServiceAdded(
+    `object`: Service
+  ) extends ServiceWatch with Watch.Added[Service]
+
+  case class ServiceModified(
+    `object`: Service
+  ) extends ServiceWatch with Watch.Modified[Service]
+
+  case class ServiceDeleted(
+    `object`: Service
+  ) extends ServiceWatch with Watch.Deleted[Service]
+
+  case class ServiceError(
+    @JsonProperty(value = "object") status: Status
+  ) extends ServiceWatch with Watch.Error[Service]
+
+  implicit object ServiceDescriptor extends ObjectDescriptor[Service, ServiceWatch] {
+    def listName = "services"
+    def toWatch(e: Service) = ServiceModified(e)
   }
 
   case class Api(client: Client) extends Version[Object] {
@@ -47,11 +76,13 @@ package object v1 {
     def version = v1.version
     override def withNamespace(ns: String) = new NsApi(client, ns)
     def endpoints = listResource[Endpoints, EndpointsWatch, EndpointsList]()
+    def services = listResource[Service, ServiceWatch, ServiceList]()
   }
 
   class NsApi(client: Client, ns: String)
     extends NsVersion[Object](client, v1.group, v1.version, ns) {
     def endpoints = listResource[Endpoints, EndpointsWatch, EndpointsList]()
+    def services = listResource[Service, ServiceWatch, ServiceList]()
   }
 
   case class EndpointsList(
@@ -83,5 +114,41 @@ package object v1 {
     port: Int,
     name: Option[String] = None,
     protocol: Option[String] = None
+  )
+
+  case class ServiceList(
+    items: Seq[Service],
+    kind: Option[String] = None,
+    metadata: Option[ObjectMeta] = None,
+    apiVersion: Option[String] = None
+  ) extends KubeList[Service]
+
+  case class Service(
+    status: Option[ServiceStatus] = None,
+    spec: Option[ServiceSpec] = None,
+    kind: Option[String] = None,
+    metadata: Option[ObjectMeta] = None,
+    apiVersion: Option[String] = None
+  ) extends Object
+
+  case class ServiceStatus(
+    loadBalancer: Option[LoadBalancerStatus] = None
+  )
+
+  case class LoadBalancerStatus(
+    ingress: Option[Seq[LoadBalancerIngress]]
+  )
+
+  case class LoadBalancerIngress(
+    ip: String
+  )
+
+  case class ServiceSpec(
+    ports: Seq[ServicePort]
+  )
+
+  case class ServicePort(
+    port: Int,
+    name: String
   )
 }
