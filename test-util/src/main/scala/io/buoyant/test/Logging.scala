967831b3b7c6ba4d0d8a7a1a384725c29abdef18
Oliver Gould <ver@buoyant.io>
2016-10-26 22:48:25 +0000

    Minor updates to test-util Logging and Awaits
    Whiel working on HTTP/2 tests, I found some minor test-util changes to be
    helpful.

--- test-util/src/main/scala/io/buoyant/test/Logging.scala
+++ test-util/src/main/scala/io/buoyant/test/Logging.scala
@@ -2,21 +2,17 @@ package io.buoyant.test
 
 import com.twitter.logging._
 import com.twitter.util.{Await, Duration, Future, Time, TimeoutException}
-import org.scalatest.BeforeAndAfter
 
-trait Logging extends BeforeAndAfter { _: org.scalatest.FunSuite =>
+trait Logging { _: org.scalatest.FunSuite =>
 
-  var logLevel: Level = Level.OFF
+  val log = Logger.get(getClass.getName)
 
-  before {
+  def setLogLevel(level: Level): Unit =
     Logger.configure(List(LoggerFactory(
       node = "",
-      level = Some(logLevel),
+      level = Some(level),
       handlers = List(ConsoleHandler())
     )))
-  }
 
-  after {
-    Logger.reset()
-  }
+  setLogLevel(Level.OFF)
 }
