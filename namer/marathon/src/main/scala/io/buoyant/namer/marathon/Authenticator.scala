cdfa795812e61eab1270f8ed93a2117f4e24aa18
Andrew Seigner <andrew@sig.gy>
2016-10-28 16:31:07 -0700

    add auth support to marathon namer (#759)
    modify marathon namer to read credentials from an environment variable,
    and, if found, authenticate against a login endpoint
    
    fixes #743

---
+++ namer/marathon/src/main/scala/io/buoyant/namer/marathon/Authenticator.scala
@@ -0,0 +1,109 @@
+package io.buoyant.namer.marathon
+
+import com.twitter.finagle.{Failure, http}
+import com.twitter.util.{Future, Promise, Return, Throw, Time}
+import io.buoyant.marathon.v2.Api
+import java.net.URL
+import java.util.concurrent.atomic.AtomicReference
+import pdi.jwt.{Jwt, JwtAlgorithm}
+import scala.annotation.tailrec
+
+object Authenticator {
+
+  case class AuthRequest(
+    loginEndpoint: String,
+    uid: String,
+    privateKey: String,
+    algorithm: JwtAlgorithm = JwtAlgorithm.RS256
+  ) {
+    val path: String = new URL(loginEndpoint).getPath
+    val jwt: String = {
+      val token = Jwt.encode(s"""{"uid":"$uid"}""", privateKey, algorithm)
+      s"""{"uid":"$uid","token":"$token"}"""
+    }
+  }
+  private[marathon] case class UnauthorizedResponse(rsp: http.Response) extends Throwable
+  private[this] case class AuthToken(token: Option[String])
+  private[this] val closedException = Failure("closed").flagged(Failure.Interrupted)
+  private[this] val closedExceptionF = Future.exception(closedException)
+  private[this] val missingTokenException = Failure("missing token")
+  private[this] val missingTokenExceptionF = Future.exception(missingTokenException)
+
+  class Authenticated(client: Api.Client, authRequest: AuthRequest) extends Api.Client {
+    private[this] sealed trait State
+    private[this] object Init extends State
+    private[this] case class Authenticating(token: Future[String]) extends State
+    private[this] object Closed extends State
+    private[this] val state = new AtomicReference[State](Init)
+
+    final def apply(req: http.Request): Future[http.Response] = state.get match {
+      case Init =>
+        val tokenP = new Promise[String]
+        if (state.compareAndSet(Init, Authenticating(tokenP))) {
+          tokenP.become(getToken())
+          tokenP.flatMap(issueAuthed(req, _))
+        } else apply(req) // try again on race
+
+      case s0@Authenticating(tokenF) =>
+        tokenF.flatMap(issueAuthed(req, _)).rescue {
+          case UnauthorizedResponse(rsp) =>
+            // If a request is unauthorized even though we have a
+            // token, it may have expired. If another request hasn't
+            // already started authenticating, get a new token and
+            // reissue the request at most once.
+            val tokenP = new Promise[String]
+            if (state.compareAndSet(s0, Authenticating(tokenP))) {
+              tokenP.become(getToken())
+              tokenP.flatMap(issueAuthed(req, _))
+            } else apply(req) // try again on race
+          case e =>
+            Future.exception(e)
+        }
+
+      case Closed => closedExceptionF
+    }
+
+    private[this] def issueAuthed(req: http.Request, token: String): Future[http.Response] = {
+      req.headerMap.set("Authorization", s"token=$token")
+      client(req).flatMap {
+        case rsp if rsp.status == http.Status.Unauthorized =>
+          Future.exception(UnauthorizedResponse(rsp))
+        case rsp => Future.value(rsp)
+      }
+    }
+
+    private[this] def getToken(): Future[String] = {
+      val tokReq = http.Request(http.Method.Post, authRequest.path)
+      tokReq.setContentTypeJson()
+      tokReq.setContentString(authRequest.jwt)
+      client(tokReq).flatMap {
+        case rsp if rsp.status == http.Status.Ok =>
+          Api.readJson[AuthToken](rsp.content) match {
+            case Return(AuthToken(Some(token))) => Future.value(token)
+            case Return(AuthToken(None)) => missingTokenExceptionF
+            case Throw(e) => Future.exception(e)
+          }
+        case rsp if rsp.status == http.Status.Unauthorized =>
+          Future.exception(UnauthorizedResponse(rsp))
+        case rsp =>
+          Future.exception(Api.UnexpectedResponse(rsp))
+      }
+    }
+
+    @tailrec final override def close(d: Time): Future[Unit] = {
+      state.get match {
+        case Init =>
+          if (state.compareAndSet(Init, Closed)) {
+            client.close(d)
+          } else close(d)
+        case s0@Authenticating(tokenF) =>
+          if (state.compareAndSet(s0, Closed)) {
+            tokenF.raise(closedException)
+            client.close(d)
+          } else close(d)
+        case Closed => Future.Unit
+      }
+    }
+  }
+
+}
--- namer/marathon/src/main/scala/io/buoyant/namer/marathon/MarathonInitializer.scala
+++ namer/marathon/src/main/scala/io/buoyant/namer/marathon/MarathonInitializer.scala
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import com.twitter.conversions.time._
 import com.twitter.finagle.param.Label
 import com.twitter.finagle.tracing.NullTracer
-import com.twitter.finagle.{Stack, Http, Path}
+import com.twitter.finagle._
+import com.twitter.io.Buf
+import com.twitter.logging.Logger
+import com.twitter.util.{Return, Throw}
 import io.buoyant.config.types.Port
 import io.buoyant.namer.{NamerConfig, NamerInitializer}
 import io.buoyant.marathon.v2.{Api, AppIdNamer}
@@ -30,6 +33,13 @@ class MarathonInitializer extends NamerInitializer {
 
 object MarathonInitializer extends MarathonInitializer
 
+case class MarathonSecret(
+  login_endpoint: Option[String],
+  private_key: Option[String],
+  scheme: Option[String],
+  uid: Option[String]
+)
+
 case class MarathonConfig(
   host: Option[String],
   port: Option[Port],
@@ -37,6 +47,9 @@ case class MarathonConfig(
   uriPrefix: Option[String],
   ttlMs: Option[Int]
 ) extends NamerConfig {
+
+  private[this] val log = Logger.get(getClass.getName)
+
   @JsonIgnore
   override val experimentalRequired = true
 
@@ -53,16 +66,58 @@ case class MarathonConfig(
 
   private[this] def getDst = dst.getOrElse(s"/$$/inet/$getHost/$getPort")
 
+  // canonical environment variable holding marathon auth info
+  // example secret json object:
+  // {
+  //   "login_endpoint": "https://leader.mesos/acs/api/v1/auth/login",
+  //   "private_key": "<private-key-value>",
+  //   "scheme": "RS256",
+  //   "uid": "service-acct"
+  // }
+  // more info at:
+  // https://docs.mesosphere.com/1.8/administration/id-and-access-mgt/service-auth/custom-service-auth/
+  // https://github.com/mesosphere/universe/search?utf8=%E2%9C%93&q=DCOS_SERVICE_ACCOUNT_CREDENTIAL
+  private[this] val SecretKey = "DCOS_SERVICE_ACCOUNT_CREDENTIAL"
+  private[this] def getAuth =
+    sys.env.get(SecretKey).flatMap { secret =>
+      Api.readJson[MarathonSecret](Buf.Utf8(secret)) match {
+        case Return(MarathonSecret(Some(loginEndpoint), Some(privateKey), Some("RS256"), Some(uid))) =>
+          Some(Authenticator.AuthRequest(loginEndpoint, uid, privateKey))
+        case Throw(e) =>
+          log.error(e, "readJson error")
+          throw e
+        case _ =>
+          val e = new Exception(s"unexpected format for $SecretKey")
+          log.error(e, s"unexpected format for $SecretKey")
+          throw e
+      }
+    }
+
+  private[this] case class SetHost(host: String)
+    extends SimpleFilter[http.Request, http.Response] {
+
+    def apply(req: http.Request, service: Service[http.Request, http.Response]) = {
+      req.host = host
+      service(req)
+    }
+  }
+
   /**
    * Construct a namer.
    */
   def newNamer(params: Stack.Params) = {
-    val service = Http.client
-      .withParams(params)
-      .configured(Label("namer" + prefix.show))
-      .withTracer(NullTracer)
-      .newService(getDst)
+    val client = SetHost(getHost).andThen(
+      Http.client
+        .withParams(params)
+        .configured(Label("namer" + prefix.show))
+        .withTracer(NullTracer)
+        .newService(getDst)
+    )
+    val service = getAuth match {
+      case Some(authRequest) => new Authenticator.Authenticated(client, authRequest)
+      case None => client
+    }
 
-    new AppIdNamer(Api(service, getHost, getUriPrefix), prefix, getTtl)
+    new AppIdNamer(Api(service, getUriPrefix), prefix, getTtl)
   }
 }
