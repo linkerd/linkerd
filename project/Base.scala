ccd4423bc497fc22ff2beced81f8b9492a47b521
Alex Leong <alex@buoyant.io>
2016-11-07 09:44:41 -0800

    0.8.3

--- project/Base.scala
+++ project/Base.scala
@@ -17,7 +17,7 @@ import scoverage.ScoverageSbtPlugin
  * Base project configuration.
  */
 class Base extends Build {
-  val headVersion = "0.8.2"
+  val headVersion = "0.8.3"
 
   object Git {
     def git(arg: String, args: String*) = Process("git" +: arg +: args)
