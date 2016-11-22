5007e84bd4c70dd29fde00ef03ac63911fcc30df
Daniel Schobel <dschobel@twitter.com>
2016-11-22 16:54:36 -0700

    bump twitter-server, util and finagle. fixup Netty4H2Listener buffer management.

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,15 +8,15 @@ object Deps {
 
   // process lifecycle
   val twitterServer =
-    ("com.twitter" %% "twitter-server" % "1.24.0")
+    ("com.twitter" %% "twitter-server" % "1.25.0")
       .exclude("com.twitter", "finagle-zipkin_2.11")
 
   def twitterUtil(mod: String) =
-    "com.twitter" %% s"util-$mod" % "6.38.0"
+    "com.twitter" %% s"util-$mod" % "6.39.0"
 
   // networking
   def finagle(mod: String) =
-    "com.twitter" %% s"finagle-$mod" % "6.39.0"
+    "com.twitter" %% s"finagle-$mod" % "6.40.0"
 
   def netty4(mod: String) =
     "io.netty" % s"netty-$mod" % "4.1.4.Final"
