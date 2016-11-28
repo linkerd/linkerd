a72c6c7bf6af4cadb40488a5563b044a8888892d
Oliver Gould <ver@buoyant.io>
2016-11-28 23:06:42 +0000

    Nevermind, my golf game isn't that good

--- router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4ServerDispatcher.scala
+++ router/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4ServerDispatcher.scala
@@ -76,7 +76,7 @@ class Netty4ServerDispatcher(
   private[this] def serveStream(st: Netty4StreamTransport[Response, Request]) = {
     // Note: `remoteMsg` should be satisfied immediately, since the
     // headers frame will have just been admitted to the stream.
-    val serveF = st.onRemoteMessage.flatMap(serve).flatMap(st.writeAll)
+    val serveF = st.onRemoteMessage.flatMap(serve).flatMap(st.write(_).flatten)
 
     // When the stream is reset, ensure that the cancelation is
     // propagated downstream.
