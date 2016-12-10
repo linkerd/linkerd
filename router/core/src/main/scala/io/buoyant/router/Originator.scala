da4ba684e93495fdc8a12614b277e9e24595fe92
Kevin Lingerfelt <kl@buoyant.io>
2016-12-09 16:35:25 -0800

    Reformat scaladoc comment (#851)

--- router/core/src/main/scala/io/buoyant/router/Originator.scala
+++ router/core/src/main/scala/io/buoyant/router/Originator.scala
@@ -3,13 +3,13 @@ package io.buoyant.router
 import com.twitter.finagle.Stack
 
 /**
-  * Originator.Param is a boolean stack param that is used to configure a
-  * [[io.buoyant.router.Router Router]]. If the param is set to true, it
-  * indicates that the router is the first hop in a linker-to-linker request,
-  * and the router's stats are updated to reflect that.
-  *
-  * @see com.twitter.finagle.Stack.Param
-  */
+ * Originator.Param is a boolean stack param that is used to configure a
+ * [[io.buoyant.router.Router Router]]. If the param is set to true, it
+ * indicates that the router is the first hop in a linker-to-linker request,
+ * and the router's stats are updated to reflect that.
+ *
+ * @see com.twitter.finagle.Stack.Param
+ */
 object Originator {
   case class Param(originator: Boolean) {
     def mk(): (Param, Stack.Param[Param]) =
