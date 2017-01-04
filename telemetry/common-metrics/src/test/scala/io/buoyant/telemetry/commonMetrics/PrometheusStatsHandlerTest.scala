f9aa899f066698485e9fbbe2f113b5f1fa444a6f
Kevin Lingerfelt <kl@buoyant.io>
2017-01-04 13:41:08 -0800

    Add test for histogram label conversions

--- telemetry/common-metrics/src/test/scala/io/buoyant/telemetry/commonMetrics/PrometheusStatsHandlerTest.scala
+++ telemetry/common-metrics/src/test/scala/io/buoyant/telemetry/commonMetrics/PrometheusStatsHandlerTest.scala
@@ -9,9 +9,17 @@ class PrometheusStatsHandlerTest extends FunSuite {
     assert(key == "foo:bar_baz_abc123:go_zoo_default:size")
   }
 
-  test("Extract labels from the key") {
-    val key = PrometheusStatsHandler.formatKey("rt/http/srv/0.0.0.0/12345/handletime_us.max")
-    assert(key == "rt:http:srv:0_0_0_0:12345:handletime_us{stat=\"max\"}")
+  test("Extract histogram labels from the key") {
+    val labels = Seq("count", "sum", "avg", "min", "max", "stddev", "p50", "p90", "p95", "p99", "p9990", "p9999")
+    for (label <- labels) {
+      val key = PrometheusStatsHandler.formatKey(s"rt/http/srv/0.0.0.0/12345/handletime_us.${label}")
+      assert(key == s"""rt:http:srv:0_0_0_0:12345:handletime_us{stat=\"${label}\"}""")
+    }
+  }
+
+  test("Does not extract non-histogram labels from the key") {
+    val key = PrometheusStatsHandler.formatKey(s"rt/http/srv/0.0.0.0/12345/handletime_us.custom")
+    assert(key == s"rt:http:srv:0_0_0_0:12345:handletime_us_custom")
   }
 
   test("Preserve non-histogram stats") {
