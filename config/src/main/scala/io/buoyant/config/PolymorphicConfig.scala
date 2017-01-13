158122ffefad79cd514df9ba08e3bf0dd9398219
Sarah Brown <sarah@buoyant.io>
2017-01-13 10:09:20 -0800

    comment auto formatting

--- config/src/main/scala/io/buoyant/config/PolymorphicConfig.scala
+++ config/src/main/scala/io/buoyant/config/PolymorphicConfig.scala
@@ -2,14 +2,13 @@ package io.buoyant.config
 
 import com.fasterxml.jackson.annotation.{JsonProperty, JsonTypeInfo}
 
-
 /**
-  * A trait that defines the property "kind" as both a var on the object
-  *   and as type information for JSON de/serialization.
-  *
-  * Config objects that expect to be subclassed by multiple other configs
-  *   with different "kind" values should extend this trait.
-  */
+ * A trait that defines the property "kind" as both a var on the object
+ *   and as type information for JSON de/serialization.
+ *
+ * Config objects that expect to be subclassed by multiple other configs
+ *   with different "kind" values should extend this trait.
+ */
 @JsonTypeInfo(
   use = JsonTypeInfo.Id.NAME,
   include = JsonTypeInfo.As.EXISTING_PROPERTY,
