cc55a26f8c2e5082d990e454de8f6a411b516fe8
Oliver Gould <ver@buoyant.io>
2017-01-26 16:13:26 -0800

    Fix and implement additional tests

--- grpc/interop/README.md
+++ grpc/interop/README.md
@@ -13,11 +13,11 @@ Some of the basic interop tests work, but many need improvements.
 These test names are standard and defined in the above document.
 
 - [x] `empty_unary` empty (zero bytes) request and response.
-- [ ] `large_unary` single request and (large) response. #929
-- [ ] `client_streaming` request streaming with single response. #982
-- [ ] `server_streaming` single request with response streaming. #983
-- [ ] `ping_pong` full-duplex streaming. #980
-- [ ] `empty_stream` full-duplex streaming with zero message. #981
+- [ ] `large_unary` single request and (large) response. (flow control deadlock) #929
+- [ ] `client_streaming` request streaming with single response. (flow control deadlock) #982
+- [x] `server_streaming` single request with response streaming.
+- [ ] `ping_pong` full-duplex streaming. (flow control deadlock) #980
+- [x] `empty_stream` full-duplex streaming with zero message. #981
 - [x] `timeout_on_sleeping_server` fullduplex streaming on a sleeping server.
 - [ ] `compute_engine_creds` large_unary with compute engine auth. (needs TLS) #895
 - [ ] `service_account_creds` large_unary with service account auth. (needs TLS) #894
@@ -25,11 +25,11 @@ These test names are standard and defined in the above document.
 - [ ] `per_rpc_creds` large_unary with per rpc token. (needs TLS) #891
 - [ ] `oauth2_auth_token` large_unary with oauth2 token auth. (needs TLS)
 - [x] `cancel_after_begin` cancellation after metadata has been sent but before payloads are sent.
-- [ ] `cancel_after_first_response` cancellation after receiving 1st message from the server. #984
-- [ ] `status_code_and_message` status code propagated back to client. #985
-- [ ] `custom_metadata` server will echo custom metadata. #986
-- [ ] `unimplemented_method` client attempts to call unimplemented method. #928
-- [ ] `unimplemented_service` client attempts to call unimplemented service. (default "large_unary") #979
+- [x] `cancel_after_first_response` cancellation after receiving 1st message from the server.
+- [x] `status_code_and_message` status code propagated back to client.
+- [ ] `custom_metadata` server will echo custom metadata. (needs metadata support) #986
+- [x] `unimplemented_method` client attempts to call unimplemented method.
+- [x] `unimplemented_service` client attempts to call unimplemented service.
 
 ## How to test a grpc-finagle server against the official grpc-go interop client.
 # build and run the interop server
---
+++ grpc/interop/src/main/scala/io/buoyant/grpc/interop/Client.scala
@@ -0,0 +1,241 @@
+package io.buoyant.grpc.interop
+
+import com.twitter.app.App
+import com.twitter.finagle.{Failure, Path}
+import com.twitter.finagle.buoyant.H2
+import com.twitter.io.Buf
+import com.twitter.logging.Logging
+import com.twitter.util.{Await, Future, Return, Throw, Try}
+import grpc.{testing => pb}
+import io.buoyant.grpc.runtime.{GrpcStatus, Stream}
+import java.net.InetSocketAddress
+
+object Client extends App with Logging {
+
+  val srvDst = flag("srv-dst", Path.read("/$/inet/127.1/60001"), "server location")
+
+  val DefaultReqSizes = Seq(27182, 8, 1828, 45904)
+  val reqSizes = flag("req-sizes", DefaultReqSizes, "request sizes")
+
+  val DefaultRspSizes = Seq(31415, 9, 2653, 58979)
+  val rspSizes = flag("rsp-sizes", DefaultRspSizes, "response sizes")
+
+  val DefaultLargeReqSize = 271828
+  val largeReqSize = flag("large-req", DefaultLargeReqSize, "large request size")
+
+  val DefaultLargeRspSize = 314159
+  val largeRspSize = flag("large-rsp", DefaultLargeRspSize, "large response size")
+
+  val testCase = flag("test-case", "large_unary", "test case to be run")
+
+  def main() {
+    val h2 = H2.newService(srvDst().show)
+    closeOnExit(h2)
+    val client = new Client(new pb.TestService.Client(h2))
+
+    val res = testCase() match {
+      case "empty_unary" => client.emptyUnary()
+      case "large_unary" => client.largeUnary(largeReqSize(), largeRspSize())
+      case "client_streaming" => client.clientStreaming(reqSizes())
+      case "server_streaming" => client.serverStreaming(rspSizes())
+      case "ping_pong" => client.pingPong(reqSizes().zip(rspSizes()))
+      case "empty_stream" => client.emptyStream()
+      case "timeout_on_sleeping_server" => client.timeoutOnSleepingServer()
+      case "cancel_after_begin" => client.cancelAfterBegin()
+      case "cancel_after_first_response" => client.cancelAfterFirstResponse()
+      case "status_code_and_message" => client.statusCodeAndMessage()
+      case name => unimplementedTest(name)
+    }
+
+    Await.result(res.liftToTry) match {
+      case Return(_) => log.info("success")
+      case Throw(e) => log.error(e, "failed")
+    }
+  }
+
+  private def mkPayload(sz: Int): pb.Payload = {
+    val body = Buf.ByteArray.Owned(Array.fill(sz) { 0.toByte })
+    pb.Payload(body = Some(body))
+  }
+
+  private def unimplementedTest(name: String) =
+    Future.exception(new UnimplementedException(name))
+
+  class UnimplementedException(name: String) extends Exception(s"test not implemented: '${name}'")
+}
+
+class Client(
+  svc: pb.TestService
+) {
+  import Client.{mkPayload, unimplementedTest}
+
+  def emptyUnary(): Future[Unit] =
+    svc.emptyCall(pb.Empty()).flatMap {
+      case pb.Empty() => Future.Unit
+      case rsp => Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+    }
+
+  def largeUnary(reqSize: Int, rspSize: Int): Future[Unit] = {
+    val req = pb.SimpleRequest(
+      responseType = Some(pb.PayloadType.COMPRESSABLE), // cargocult
+      responseSize = Some(rspSize),
+      payload = Some(mkPayload(reqSize))
+    )
+    svc.unaryCall(req).flatMap {
+      case pb.SimpleResponse(Some(pb.Payload(_, Some(buf))), _, _) =>
+        if (buf.length == rspSize) Future.Unit
+        else Future.exception(new IllegalArgumentException(s"received ${buf.length}B, expected ${rspSize}B"))
+
+      case rsp => Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+    }
+  }
+
+  def clientStreaming(reqSizes: Seq[Int]): Future[Unit] = {
+    val reqs = Stream.mk[pb.StreamingInputCallRequest]
+
+    def sendReqs(szs0: Seq[Int]): Future[Unit] = szs0 match {
+      case Nil => reqs.close()
+      case Seq(sz, szs1@_*) =>
+        val req = pb.StreamingInputCallRequest(payload = Some(mkPayload(sz)))
+        reqs.send(req).before(sendReqs(szs1))
+    }
+
+    sendReqs(reqSizes).join(svc.streamingInputCall(reqs)).flatMap {
+      case (_, pb.StreamingInputCallResponse(Some(sz))) =>
+        val sum = reqSizes.sum
+        if (sz == sum) Future.Unit
+        else Future.exception(new IllegalArgumentException(s"received ${sz}B, expected ${sum}B"))
+      case (_, rsp) =>
+        Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+    }
+  }
+
+  def serverStreaming(rspSizes0: Seq[Int]): Future[Unit] = {
+    val req = pb.StreamingOutputCallRequest(
+      responseParameters = rspSizes0.map { sz => pb.ResponseParameters(size = Some(sz)) }
+    )
+    val rsps = svc.streamingOutputCall(req)
+
+    def read(rspSizes: Seq[Int]): Future[Unit] = rspSizes match {
+      case Nil => Future.Unit
+      case Seq(expected, rest@_*) =>
+        rsps.recv().transform {
+          case Throw(GrpcStatus.Ok(_)) => Future.Unit
+          case Throw(e) => Future.exception(e)
+          case Return(Stream.Releasable(rsp, release)) => rsp match {
+            case pb.StreamingOutputCallResponse(Some(pb.Payload(_, Some(buf)))) =>
+              val sz = buf.length
+              release().before {
+                if (sz != expected) {
+                  Future.exception(new IllegalArgumentException(s"recieved ${sz}B, expected ${expected}B"))
+                } else read(rest)
+              }
+
+            case rsp =>
+              release().before(Future.exception(new IllegalArgumentException(s"invalid response: $rsp")))
+          }
+        }
+    }
+    read(rspSizes0)
+  }
+
+  def pingPong(sizes0: Seq[(Int, Int)]): Future[Unit] = {
+    val reqs = Stream.mk[pb.StreamingOutputCallRequest]
+    val rsps = svc.fullDuplexCall(reqs)
+
+    def sendRecv(sizes: Seq[(Int, Int)]): Future[Unit] = sizes match {
+      case Nil => Future.Unit
+
+      case Seq((reqSz, rspSz), rest@_*) =>
+        val req = pb.StreamingOutputCallRequest(
+          responseParameters = Seq(pb.ResponseParameters(size = Some(rspSz))),
+          payload = Some(mkPayload(reqSz))
+        )
+        reqs.send(req).before {
+          rsps.recv().flatMap { r =>
+            val Stream.Releasable(rsp, release) = r
+            rsp match {
+              case pb.StreamingOutputCallResponse(Some(pb.Payload(_, Some(buf)))) =>
+                val sz = buf.length
+                release().before {
+                  if (sz != rspSz) {
+                    Future.exception(new IllegalArgumentException(s"recieved ${sz}B, expected ${rspSz}B"))
+                  } else sendRecv(rest)
+                }
+
+              case rsp =>
+                release().before(Future.exception(new IllegalArgumentException(s"invalid response: $rsp")))
+            }
+          }.before(reqs.close())
+        }
+    }
+
+    sendRecv(sizes0)
+  }
+
+  def emptyStream(): Future[Unit] = {
+    val reqs = Stream.mk[pb.StreamingOutputCallRequest]
+    val rsps = svc.fullDuplexCall(reqs)
+    reqs.close().before {
+      rsps.recv().transform {
+        case Throw(GrpcStatus.Ok(msg)) => Future.Unit
+        case Throw(e) => Future.exception(e)
+        case Return(Stream.Releasable(rsp, release)) =>
+          Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+      }
+    }
+  }
+
+  def timeoutOnSleepingServer(): Future[Unit] = unimplementedTest("timeout_on_sleeping_server")
+
+  def cancelAfterBegin(): Future[Unit] = {
+    val reqs = Stream.mk[pb.StreamingInputCallRequest]
+    val rspF = svc.streamingInputCall(reqs)
+    rspF.raise(Failure(GrpcStatus.Canceled(), Failure.Interrupted))
+    rspF.transform {
+      case Throw(GrpcStatus.Canceled(_)) => Future.Unit
+      case Throw(Failure(Some(GrpcStatus.Canceled(_)))) => Future.Unit
+      case Throw(f@Failure(Some(e))) => Future.exception(e)
+      case Throw(e) => Future.exception(e)
+      case Return(rsp) =>
+        Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+    }
+  }
+
+  def cancelAfterFirstResponse(): Future[Unit] = {
+    val reqs = Stream.mk[pb.StreamingOutputCallRequest]
+    val rsps = svc.fullDuplexCall(reqs)
+    reqs.send(pb.StreamingOutputCallRequest(
+      responseParameters = Seq(pb.ResponseParameters(size = Some(2048))),
+      payload = Some(mkPayload(2048))
+    ))
+    rsps.recv().flatMap { r =>
+      val Stream.Releasable(_, release) = r
+      release().before {
+        reqs.reset(GrpcStatus.Canceled())
+        rsps.recv().transform {
+          case Throw(GrpcStatus.Canceled(_)) => Future.Unit
+          case Throw(e) => Future.exception(e)
+          case Return(rsp) =>
+            Future.exception(new IllegalArgumentException(s"unexpected response: $rsp"))
+        }
+      }
+    }
+  }
+
+  def statusCodeAndMessage(): Future[Unit] = {
+    val checkStatus: Try[_] => Future[Unit] = {
+      case Throw(GrpcStatus.Unknown("destroy fascism")) => Future.Unit
+      case result => Future.exception(new IllegalArgumentException(s"unexpected response: $result"))
+    }
+
+    // First a unary call, then a streaming call
+    val rspStatus = pb.EchoStatus(Some(2), Some("destroy fascism"))
+    val unaryReq = pb.SimpleRequest(responseStatus = Some(rspStatus))
+    val streamReq = pb.StreamingOutputCallRequest(responseStatus = Some(rspStatus))
+    svc.unaryCall(unaryReq).transform(checkStatus).before {
+      val rsps = svc.fullDuplexCall(Stream.value(streamReq))
+      rsps.recv().transform(checkStatus)
+    }
+  }
+}
