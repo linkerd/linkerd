8d4d433fbbd34846874c75dc183b75c44870a8cd
Risha Mars <rmars@users.noreply.github.com>
2017-01-30 15:38:58 -0800

    Add load balancer and retry stats bar charts to clients (#990)
    * Clean up passing of excessive elements
    
    * clean up expand/collapse toggles
    
    * Add barcharts for load balancer status and retry status (per client)
    
    * Get rid of the tri color bar chart
    
    * Add a RouterStats section for general router stats
    
    * Prevent a bar from overflowing
    
    * Add tests for pathQuery and new bar chart
    
    * Add template for over budget warning
    
    * Add retries to router summary stats. Since we need all these stats in here anyway, may as well add the bar chart here too
    
    Also remove router stats section, since retry bar chart can now be rendered in router summary
    
    * Try to make the expected values of displayData clearer
    
    * eslint
    
    * remove unneeded includes
    
    * Get configured retry budget from router config
    
    * Move bar chart helpers to their own module
    
    * Revert "Move bar chart helpers to their own module"
    
    This reverts commit 71d422c9f1ef532542158fd52f35157283c7d413.
    
    * explicitly scope bar chart helpers
    
    * Separate out bar chart tests
    
    * omg typos
    
    * Make clearer interface for bar chart wrapping
    
    * Display nulls as - and 0 as 0

--- admin/src/main/resources/io/buoyant/admin/js/src/router_controller.js
+++ admin/src/main/resources/io/buoyant/admin/js/src/router_controller.js
@@ -119,17 +119,19 @@ define([
       return routerContainers;
     }
 
-    return function(metricsCollector, selectedRouter, routers, $parentContainer) {
+    return function(metricsCollector, selectedRouter, routers, $parentContainer, routerConfig) {
       var routerContainerEls = initializeRouterContainers(selectedRouter, routers, $parentContainer);
 
       _.each(routerContainerEls, function(container, router) {
         var $summaryEl = $(container.find(".summary")[0]);
         var $serversEl = $(container.find(".servers")[0]);
         var $clientsEl = $(container.find(".clients")[0]);
+        var $combinedClientGraphEl = $(container.find(".router-graph")[0]);
+        var $routerStatsEl = $(container.find(".router-stats")[0]);
 
-        RouterSummary(metricsCollector, $summaryEl, router);
+        RouterSummary(metricsCollector, $summaryEl, $routerStatsEl, router, routerConfig);
         RouterServers(metricsCollector, routers, $serversEl, router);
-        RouterClients(metricsCollector, routers, $clientsEl, router, colorOrder);
+        RouterClients(metricsCollector, routers, $clientsEl, $combinedClientGraphEl, router, colorOrder);
       });
 
       return {};
