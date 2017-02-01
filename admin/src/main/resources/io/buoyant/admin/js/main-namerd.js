432061d484dcc2594252c32554dc01972c17dd78
Risha Mars <mars@buoyant.io>
2017-02-01 14:18:19 -0800

    remove duplicated configs

--- admin/src/main/resources/io/buoyant/admin/js/main-namerd.js
+++ admin/src/main/resources/io/buoyant/admin/js/main-namerd.js
@@ -1,27 +1,3 @@
-var requireConfig = {
-  paths: {
-    'jQuery': 'lib/jquery.min',
-    'lodash': 'lib/lodash.min',
-    'Handlebars': 'lib/handlebars-v4.0.5',
-    'bootstrap': 'lib/bootstrap.min',
-    'text': 'lib/text'
-  },
-  shim: {
-    'jQuery': {
-      exports: '$'
-    },
-    'lodash': {
-      exports: '_'
-    },
-    'bootstrap': {
-      deps : ['jQuery'],
-      exports: 'Bootstrap'
-    }
-  }
-}
-
-require.config(requireConfig);
-
 require([
   'jQuery',
   'bootstrap',
