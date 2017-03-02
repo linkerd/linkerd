c005226b6d4596cadc3a647abc0ca774a0b851ad
Oliver Gould <ver@buoyant.io>
2017-03-01 22:10:45 -0800

    Add weight metadata to io.l5d.thriftNameInterpreter
    Problem
    
    Finagle addresses may contain weight metadata. Namerd, however, does
    not currently propagate weights across the thrift interpreter
    interface, and so they do not factor into endpoint-level load
    balancing decisions.
    
    Solution
    
    Add the `endpoint_addr_weight` field to the `AddrMeta` thrift type. Servers may
    now encode this double value, if it is specified and valid; and clients will
    use this weight when making load balancing decisions.
    
    This is useful, for instance, when instrumenting "red line" tests.

--- namer/core/src/main/scala/io/buoyant/namer/Metadata.scala
+++ namer/core/src/main/scala/io/buoyant/namer/Metadata.scala
@@ -1,6 +1,9 @@
 package io.buoyant.namer
 
+import com.twitter.finagle.addr.WeightedAddress
+
 object Metadata {
   val authority = "authority" // HTTP/1.1 Host or HTTP/2.0 :authority
   val nodeName = "nodeName"
+  val endpointWeight = WeightedAddress.weightKey
 }
