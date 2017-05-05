a7939a65e1e771fd4e238c3893a2e2bebd17109a
Alex Leong <alex@buoyant.io>
2017-05-04 18:10:15 -0700

    Client stats should use service response classifier (#1266)
    If a response classifier is configured on a service, that response classifier
    is not used to report client level success and failure metrics.  This can lead
    to inconsistent metric counts.
    
    Modify the PerDstStatsFilter to use the response classifier from the service
    (stored in a local context).  Also add io.l5d.{http, h2}.allSuccessful response
    classifiers to aid with testing and for general usefulness.

--- router/core/src/main/scala/io/buoyant/router/PerDstPathStatsFilter.scala
+++ router/core/src/main/scala/io/buoyant/router/PerDstPathStatsFilter.scala
@@ -4,30 +4,31 @@ import com.twitter.finagle.{Filter, Path, Stack, Stackable, ServiceFactory, para
 import com.twitter.finagle.buoyant.Dst
 import com.twitter.finagle.service.StatsFilter
 import com.twitter.finagle.stats.{ExceptionStatsHandler, StatsReceiver}
+import io.buoyant.router.context.ResponseClassifierCtx
 
 object PerDstPathStatsFilter {
   val role = Stack.Role("PerDstPathStatsFilter")
 
   def module[Req, Rsp]: Stackable[ServiceFactory[Req, Rsp]] =
-    new Stack.Module4[param.Stats, param.ExceptionStatsHandler, param.ResponseClassifier, StatsFilter.Param, ServiceFactory[Req, Rsp]] {
+    new Stack.Module3[param.Stats, param.ExceptionStatsHandler, StatsFilter.Param, ServiceFactory[Req, Rsp]] {
       val role = PerDstPathStatsFilter.role
       val description = "Report request statistics for each logical destination"
 
       def make(
         _stats: param.Stats,
         _exceptions: param.ExceptionStatsHandler,
-        _classifier: param.ResponseClassifier,
         _unit: StatsFilter.Param,
         next: ServiceFactory[Req, Rsp]
       ): ServiceFactory[Req, Rsp] = _stats match {
         case param.Stats(statsReceiver) if !statsReceiver.isNull =>
-          val param.ResponseClassifier(classifier) = _classifier
           val param.ExceptionStatsHandler(handler) = _exceptions
           val StatsFilter.Param(unit) = _unit
 
           def mkScopedStatsFilter(path: Path): Filter[Req, Rsp, Req, Rsp] = {
             val name = path.show.stripPrefix("/")
             val sr = statsReceiver.scope("service", name)
+            val param.ResponseClassifier(classifier) =
+              ResponseClassifierCtx.current.getOrElse(param.ResponseClassifier.param.default)
             new StatsFilter(sr, classifier, handler, unit)
           }
 
