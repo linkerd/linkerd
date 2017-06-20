c8983585c8162195b008eb76cfa5f920b910b68b
Sarah Brown <sarah@buoyant.io>
2017-06-20 16:50:14 -0700

    Istio Identifier for H2
    Moves common precondition checking logic into the io.buoyant.k8s.istio package.
    Adds/updates documentation
    Fixes #1408

---
+++ k8s/src/main/scala/io/buoyant/k8s/istio/package.scala
@@ -0,0 +1,9 @@
+package io.buoyant.k8s
+
+package object istio {
+  val DefaultDiscoveryHost = "istio-manager"
+  val DefaultDiscoveryPort = 8080
+
+  val DefaultApiserverHost = "istio-manager"
+  val DefaultApiserverPort = 8081
+}
