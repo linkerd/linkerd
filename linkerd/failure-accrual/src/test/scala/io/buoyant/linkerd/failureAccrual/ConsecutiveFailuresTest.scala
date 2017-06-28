a4a297ad87e9eed49cacb1ca1b8983dc4e3fba42
Eliza Weisman <eliza@buoyant.io>
2017-06-28 14:40:20 -0500

    Add failing test for issue #1439
    This commit adds a test that reproduces #1439, by simulating 20
    consecutive failures and asserting that the stream of durations produced
    by the backoff policy are constant. This test currently fails. This
    commit also adds a test that asserts that the default backoff policy is
    used when no backoff policy is specified in YAML, to ensure fixes for
    this issue don't break that behaviour.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/failure-accrual/src/test/scala/io/buoyant/linkerd/failureAccrual/ConsecutiveFailuresTest.scala
+++ linkerd/failure-accrual/src/test/scala/io/buoyant/linkerd/failureAccrual/ConsecutiveFailuresTest.scala
@@ -2,10 +2,24 @@ package io.buoyant.linkerd.failureAccrual
 
 import com.twitter.finagle.liveness.FailureAccrualPolicy
 import com.twitter.finagle.util.LoadService
-import io.buoyant.linkerd.FailureAccrualInitializer
+import com.twitter.conversions.time._
+import io.buoyant.config.Parser
+import io.buoyant.linkerd.{FailureAccrualConfig, FailureAccrualInitializer}
 import io.buoyant.test.FunSuite
+import org.scalatest.Matchers
 
-class ConsecutiveFailuresTest extends FunSuite {
+class ConsecutiveFailuresTest extends FunSuite
+  with Matchers {
+  def parse(yaml: String): FailureAccrualConfig = {
+    val mapper = Parser.objectMapper(yaml, Seq(Seq(
+      new ConsecutiveFailuresInitializer,
+      new NoneInitializer,
+      new SuccessRateInitializer,
+      new SuccessRateWindowedInitializer
+    )))
+    mapper.readValue[FailureAccrualConfig](yaml)
+  }
+
   test("sanity") {
     val failures = 2
     val config = ConsecutiveFailuresConfig(failures)
@@ -15,5 +29,27 @@ class ConsecutiveFailuresTest extends FunSuite {
 
   test("service registration") {
     assert(LoadService[FailureAccrualInitializer].exists(_.isInstanceOf[ConsecutiveFailuresInitializer]))
+  }
+
+  test("constant backoff") {
+    val yaml = """|kind: io.l5d.consecutiveFailures
+                  |failures: 5
+                  |backoff:
+                  |  kind: constant
+                  |  ms: 10000""".stripMargin
+    val policy = parse(yaml).policy()
+    val probeDelays = Stream.continually(policy.markDeadOnFailure()).take(20)
+    probeDelays.take(4) should contain only (None)
+    probeDelays should contain inOrderOnly (None, Some(10000.millis))
+  }
+
+  test("default (exponential) backoff") {
+    val yaml =
+      """|kind: io.l5d.consecutiveFailures
+         |failures: 5""".stripMargin
+
+    val policy = parse(yaml).policy()
+    val probeDelays = Stream.continually(policy.markDeadOnFailure()).take(20)
+    probeDelays shouldBe sorted // todo: better assertion that the increase is exponential
   }
 }
