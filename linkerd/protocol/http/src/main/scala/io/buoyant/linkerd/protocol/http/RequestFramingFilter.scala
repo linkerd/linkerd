4e27645a66070cf5ed79bef4d26ca2e02752fb4b
Eliza Weisman <eliza@buoyant.io>
2017-07-07 11:24:40 -0700

    Add response to RequestFramingFilter
    Added a response to the filter in the case of duplicate Content-Length
    headers.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/http/RequestFramingFilter.scala
+++ linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/http/RequestFramingFilter.scala
@@ -2,22 +2,29 @@ package io.buoyant.linkerd.protocol.http
 
 import com.twitter.finagle._
 import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.buoyant.linkerd.Headers
 import com.twitter.logging.Logger
 import com.twitter.util.Future
 
 /**
-  * A filter that fails badly-framed requests.
-  */
+ * A filter that fails badly-framed requests.
+ */
 class RequestFramingFilter extends SimpleFilter[Request, Response] {
   private[this] lazy val log = Logger.get("RequestFramingFilter")
 
-  override def apply(request: Request,
-                     service: Service[Request, Response]): Future[Response] =
+  override def apply(
+    request: Request,
+    service: Service[Request, Response]
+  ): Future[Response] =
     if (request.headerMap.getAll("Content-Length").length > 1) {
       // if the length of the Content-Length key in the request's header map
       // is greater than 1, then there are duplicate values.
       log.error("request with duplicate Content-Length headers", request)
-      ???
+      val resp = Headers.Err.respond(
+        "Request contained duplicate `Content-Length` headers",
+        Status.BadRequest
+      )
+      Future(resp)
     } else {
       // otherwise, the request is fine!
       service(request)
