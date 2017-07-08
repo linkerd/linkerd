1d28f75ef5449e9f95952aa08be1a3303b9154a4
Eliza Weisman <eliza@buoyant.io>
2017-07-08 11:31:53 -0700

    Add a protocol name field to ProtocolException

--- linkerd/core/src/main/scala/io/buoyant/linkerd/ProtocolException.scala
+++ linkerd/core/src/main/scala/io/buoyant/linkerd/ProtocolException.scala
@@ -3,5 +3,9 @@ package io.buoyant.linkerd
 /**
  * An exception indicating an error in protocol compliance
  * @param reason a message describing the cause of the error
+ * @param protocol the name of the protocol in question
  */
-abstract class ProtocolException(reason: String) extends Exception(reason)
+// TODO: do we want to consider adding a proto version marker to
+//       this exception?
+abstract class ProtocolException(reason: String, protocol: String)
+  extends Exception(s"$protocol: $reason")
--- linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/FramingException.scala
+++ linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/FramingException.scala
@@ -1,10 +1,6 @@
 package io.buoyant.linkerd.protocol
 
-<<<<<<< HEAD
 import io.buoyant.linkerd.ProtocolException
 
 case class FramingException(reason: String)
-  extends ProtocolException(reason)
-=======
-case class FramingException(reason: String) extends Exception(reason)
->>>>>>> 81a624b0f88c20a86898acff7fe3209aaf0f50b0
+  extends ProtocolException(reason, "HTTP")
