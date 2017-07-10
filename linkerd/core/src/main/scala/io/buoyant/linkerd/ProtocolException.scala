cd02b669c1440e520d5e0b824af07c7d22797654
Eliza Weisman <eliza@buoyant.io>
2017-07-10 13:17:44 -0500

    Add NoStackTrace to ProtocolException and remove protocol field

--- linkerd/core/src/main/scala/io/buoyant/linkerd/ProtocolException.scala
+++ linkerd/core/src/main/scala/io/buoyant/linkerd/ProtocolException.scala
@@ -1,11 +1,12 @@
 package io.buoyant.linkerd
 
+import scala.util.control.NoStackTrace
+
 /**
  * An exception indicating an error in protocol compliance
+ *
  * @param reason a message describing the cause of the error
- * @param protocol the name of the protocol in question
  */
-// TODO: do we want to consider adding a proto version marker to
-//       this exception?
-abstract class ProtocolException(reason: String, protocol: String)
-  extends Exception(s"$protocol: $reason")
+abstract class ProtocolException(reason: String)
+  extends Exception(reason)
+  with NoStackTrace
