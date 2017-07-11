8b4a9e45c5d4522ef3a0dc7b6a932e0630479c97
Eliza Weisman <eliza@buoyant.io>
2017-07-11 10:06:09 -0500

    Simplify Server FramingFilter, add logging

--- linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/http/FramingFilter.scala
+++ linkerd/protocol/http/src/main/scala/io/buoyant/linkerd/protocol/http/FramingFilter.scala
@@ -4,9 +4,14 @@ import com.twitter.finagle._
 import com.twitter.finagle.buoyant.linkerd.Headers
 import com.twitter.finagle.http.{HeaderMap, Request, Response, Status}
 import com.twitter.util.Future
+import com.twitter.logging.Logger
 import io.buoyant.linkerd.ProtocolException
 
 object FramingFilter {
+  // since the client and server filters go in separate stacks,
+  // they can have the same role.
+  val role = Stack.Role("FramingFilter")
+
   // this is factored out so that the same logic can
   // be applied to requests from clients and responses from services.
   private[FramingFilter] def headerErrors(headers: HeaderMap): Option[FramingException] =
@@ -21,20 +26,24 @@ object FramingFilter {
    * A filter that fails badly-framed requests.
    */
   class ServerFilter extends SimpleFilter[Request, Response] {
+    // unlike the client-side filter, this needs its own logger, since
+    // it responds to bad requests directly, rather than bubbling up
+    // exceptions to an ErrorResponder
+    private[this] val log = Logger.get("FramingFilter.ServerFIlter")
 
     override def apply(
       request: Request,
       service: Service[Request, Response]
     ): Future[Response] =
         headerErrors(request.headerMap)
-          .map { err => Future.value(err.toResponse(Status.BadRequest)) }
+          .map { case FramingException(reason) =>
+            log.error(reason)
+            Future.value( Headers.Err.respond(reason, Status.BadRequest))
+          }
           .getOrElse(service(request))
 
   }
 
-  object ServerFilter {
-    val role = Stack.Role("ServerFramingFilter")
-  }
 
   /**
    * A filter that fails badly-framed responses
@@ -53,13 +62,9 @@ object FramingFilter {
 
   }
 
-  object ClientFilter {
-    val role = Stack.Role("ClientFramingFilter")
-  }
-
   val serverModule: Stackable[ServiceFactory[Request, Response]] =
     new Stack.Module0[ServiceFactory[Request, Response]] {
-      val role = ServerFilter.role
+      val role = FramingFilter.role
       val description = "Fails badly-framed HTTP requests"
       val filter = new ServerFilter
       def make(factory: ServiceFactory[Request, Response]) =
@@ -68,17 +73,12 @@ object FramingFilter {
 
   val clientModule: Stackable[ServiceFactory[Request, Response]] =
     new Stack.Module0[ServiceFactory[Request, Response]] {
-      val role = ClientFilter.role
+      val role = FramingFilter.role
       val description = "Fails badly-framed HTTP responses"
       val filter = new ClientFilter
       def make(factory: ServiceFactory[Request, Response]) =
         filter.andThen(factory)
     }
 
-  case class FramingException(reason: String) extends ProtocolException(reason) {
-    @inline def toResponse(status: Status): Response = {
-      val message = Option(this.getMessage).getOrElse(this.getClass.getName)
-      Headers.Err.respond(message, status)
-    }
-  }
+  case class FramingException(reason: String) extends ProtocolException(reason)
 }
