f7c23d8ebf5694ba590efb7ec3eed4b3eff12edb
Eliza Weisman <eliza@buoyant.io>
2017-07-16 17:06:50 -0500

    Add arbitrary Path generator

--- finagle/buoyant/src/test/scala/io/buoyant/finagle/test/DtabGenerators.scala
+++ finagle/buoyant/src/test/scala/io/buoyant/finagle/test/DtabGenerators.scala
@@ -1,15 +1,23 @@
 
 package io.buoyant.finagle.test
 
+import com.twitter.finagle.Path
 import com.twitter.io.Buf
 import com.twitter.io.Buf.ByteArray
 import org.scalacheck.{Arbitrary, Gen}
 
 trait DtabGenerators {
-  val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#._"
+  val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._"
   val labelChar = for { i <- Gen.choose(0, alphabet.length) } yield alphabet(i)
   val label: Gen[Buf] = Gen.sized { size =>
-    Gen.listOfN(size, labelChar).map(list => ByteArray(list.map(_.toByte):_*))
+    if (size == 1) Gen.oneOf("#", "*")
+    else Gen.listOfN(size, labelChar).map { _.mkString("") }
+  }.map { str =>
+    ByteArray(str.map(_.toByte):_*)
   }
 
+  implicit def arbPath: Arbitrary[Path] = Arbitrary {
+    def pathElems(size: Int) = Gen.listOfN(size, label)
+    Gen.sized { size => pathElems(size).map{ bufs => Path(bufs:_*) } }
+  }
 }
