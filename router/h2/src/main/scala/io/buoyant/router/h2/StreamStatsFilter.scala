11bd0323445df118384aaf317298d45c624399d6
Eliza Weisman <eliza@elizas.website>
2017-07-17 17:24:58 -0500

    Add basic observability for H2 (#1510)
    * Add additional stats to StreamStatsFilter
    
    * DRY up StreamStatsFilter
    
    * Replace default client and server stats filters with StreamStackFilter
    
    * Add test for StreamStatsFilter
    
    * Finish StreamStatsFilter implementation
    
    * Review feedback
    
    * varargs are magical

--- router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
@@ -3,7 +3,7 @@ package io.buoyant.router.h2
 import com.twitter.finagle.{Status => _, _}
 import com.twitter.finagle.buoyant.h2.{Request, Response, Stream}
 import com.twitter.finagle.stats.StatsReceiver
-import com.twitter.util.{Future, Return, Stopwatch, Throw}
+import com.twitter.util._
 
 object StreamStatsFilter {
   val role = Stack.Role("StreamStatsFilter")
@@ -21,40 +21,64 @@ object StreamStatsFilter {
 class StreamStatsFilter(statsReceiver: StatsReceiver)
   extends SimpleFilter[Request, Response] {
 
-  private[this] val reqStreamTimeMs = statsReceiver.stat("stream", "request", "duration_ms")
-  private[this] val rspStreamTimeMs = statsReceiver.stat("stream", "response", "duration_ms")
-  private[this] val streamTimeMs = statsReceiver.stat("stream", "duration_ms")
-  private[this] val reqStreamFailures = statsReceiver.counter("stream", "request", "failures")
-  private[this] val rspStreamFailures = statsReceiver.counter("stream", "response", "failures")
-  private[this] val streamFailures = statsReceiver.counter("stream", "failures")
+  class StreamStats(
+    stats: StatsReceiver,
+    durationName: Option[String] = None,
+    successName: Option[String] = None
+  ) {
 
-  private[this] val latencyMs = statsReceiver.stat("latency_ms")
+    private[this] val durationMs =
+      stats.stat(s"${durationName.getOrElse("stream_duration")}_ms")
+    private[this] val successes =
+      stats.counter(s"${successName.getOrElse("stream_")}successes")
+    private[this] val failures =
+      stats.counter(s"${successName.getOrElse("stream_")}failures")
+
+    @inline def apply(startT: Stopwatch.Elapsed)(result: Try[_]): Unit = {
+      durationMs.add(startT().inMillis)
+      result match {
+        case Return(_) => successes.incr()
+        case Throw(_) => failures.incr()
+      }
+    }
+  }
+
+  //   total number of requests received
+  private[this] val reqCount = statsReceiver.counter("requests")
+
+  private[this] val reqStreamStats =
+    new StreamStats(statsReceiver.scope("request", "stream"))
+  private[this] val rspStreamStats =
+    new StreamStats(statsReceiver.scope("response", "stream"))
+  private[this] val totalStreamStats =
+    new StreamStats(statsReceiver.scope("stream"),
+                    durationName = Some("total_latency"))
+  private[this] val rspFutureStats =
+    new StreamStats(statsReceiver,
+                    durationName = Some("request_latency"),
+                    successName = Some(""))
 
   override def apply(req: Request, service: Service[Request, Response]): Future[Response] = {
+    reqCount.incr()
     val reqT = Stopwatch.start()
-    req.stream.onEnd.respond {
-      case Return(_) => reqStreamTimeMs.add(reqT().inMillis)
-      case Throw(_) => reqStreamFailures.incr()
-    }
 
-    val rspF = service(req)
+    req.stream.onEnd.respond(reqStreamStats(reqT))
 
-    rspF.onSuccess { rsp =>
-      latencyMs.add(reqT().inMillis)
-
-      val rspT = Stopwatch.start()
-      rsp.stream.onEnd.respond {
-        case Return(_) => rspStreamTimeMs.add(rspT().inMillis)
-        case Throw(_) => rspStreamFailures.incr()
+    service(req)
+      .respond { result =>
+        rspFutureStats(reqT)(result)
+        val stream = result match {
+          case Return(rsp) => req.stream.onEnd.join(rsp.stream.onEnd)
+          case Throw(_) => req.stream.onEnd
+        }
+        val _ = stream.respond(totalStreamStats(reqT))
+      }
+      .onSuccess { rsp =>
+        val rspT = Stopwatch.start()
+        val _ = rsp.stream.onEnd.respond(rspStreamStats(rspT))
       }
 
-      val _ = req.stream.onEnd.join(rsp.stream.onEnd).respond {
-        case Return(_) => streamTimeMs.add(reqT().inMillis)
-        case Throw(_) => streamFailures.incr()
-      }
-    }
 
-    rspF
   }
 
 }
---
+++ router/h2/src/test/scala/io/buoyant/router/h2/StreamStatsFilterTest.scala
@@ -0,0 +1,157 @@
+package io.buoyant.router.h2
+
+import com.twitter.finagle.{Service, ServiceFactory, Stack, param}
+import com.twitter.finagle.buoyant.h2.{Method, Request, Response, Status, Stream}
+import com.twitter.finagle.stats.InMemoryStatsReceiver
+import com.twitter.util.Future
+import io.buoyant.test.{Awaits, FunSuite}
+
+class StreamStatsFilterTest extends FunSuite with Awaits {
+  val successCounters = Seq(
+    Seq("response", "stream", "stream_successes"),
+    Seq("request", "stream", "stream_successes"),
+    Seq("stream", "stream_successes"),
+    Seq("successes")
+  )
+  val failureCounters = Seq(
+    Seq("response", "stream", "stream_failures"),
+    Seq("request", "stream", "stream_failures"),
+    Seq("stream", "stream_failures"),
+    Seq("failures")
+  )
+  val requestCounter = Seq("requests")
+  val allCounters = successCounters ++ failureCounters :+ requestCounter
+  val latencyStats = Seq(
+    Seq("response", "stream", "stream_duration_ms"),
+    Seq("request", "stream", "stream_duration_ms"),
+    Seq("stream", "total_latency_ms"),
+    Seq("request_latency_ms")
+  )
+
+  private[this] def setup(response: Request => Future[Response]) = {
+
+    val stats = new InMemoryStatsReceiver()
+    val svc = Service.mk[Request, Response] { response(_) }
+    val stk = StreamStatsFilter.module.toStack(
+      Stack.Leaf(StreamStatsFilter.role, ServiceFactory.const(svc))
+    )
+    val service = await(stk.make(Stack.Params.empty + param.Stats(stats))())
+
+    (stats, service)
+  }
+
+  test("increments success counters on success") {
+    val (stats, service) = setup { _ =>
+      Future.value(Response(Status.Ok, Stream.empty()))
+    }
+
+    // all counters should be empty before firing request
+    withClue("before request") {
+      for {counter <- allCounters}
+        withClue(s"stat: $counter") { assert(!stats.counters.isDefinedAt(counter)) }
+    }
+
+    val req = Request("http", Method.Get, "hihost", "/", Stream.empty())
+    await(service(req))
+
+    withClue("after first response") {
+      for { counter <- successCounters :+ requestCounter }
+        withClue(s"stat: $counter") { assert(stats.counters(counter) == 1) }
+    }
+
+    await(service(req))
+    withClue("after second response") {
+      for { counter <- successCounters :+ requestCounter }
+        withClue(s"stat: $counter") { assert(stats.counters(counter) == 2) }
+    }
+  }
+
+  test("does not increment failure counters after successes") {
+    val (stats, service) = setup { _ =>
+      Future.value(Response(Status.Ok, Stream.empty()))
+    }
+
+    val req = Request("http", Method.Get, "hihost", "/", Stream.empty())
+    await(service(req))
+
+    withClue("after first response") {
+      for { counter <- failureCounters }
+        withClue(s"stat: $counter") { assert(!stats.counters.isDefinedAt(counter)) }
+    }
+
+    await(service(req))
+    withClue("after second response") {
+      for { counter <- failureCounters }
+        withClue(s"stat: $counter") { assert(!stats.counters.isDefinedAt(counter)) }
+    }
+  }
+
+  object Thrown extends Throwable
+
+  test("increments correct failure counters after failure") {
+    val (stats, service) = setup { _ =>
+      Future.exception(Thrown)
+    }
+    val req = Request("http", Method.Get, "hihost", "/", Stream.empty())
+    val expectedCounters = Seq(
+      requestCounter,
+      Seq("failures"),
+      Seq("request", "stream", "stream_successes"))
+
+    assertThrows[Throwable] { await(service(req)) }
+    withClue("after first failure") {
+      for { counter <- expectedCounters }
+        assert(stats.counters(counter) == 1)
+      // counters that should not be incremented on this failure
+      for { counter <- failureCounters if !expectedCounters.contains(counter) }
+        withClue(s"stat: $counter") { assert(!stats.counters.isDefinedAt(counter)) }
+
+    }
+
+    assertThrows[Throwable] { await(service(req)) }
+    withClue("after second failure") {
+      for { counter <- expectedCounters }
+        withClue(s"stat: $counter") { assert(stats.counters(counter) == 2) }
+
+      // counters that should not be incremented on this failure
+      for { counter <- failureCounters if !expectedCounters.contains(counter) }
+        withClue(s"stat: $counter") { assert(!stats.counters.isDefinedAt(counter)) }
+    }
+  }
+
+  test("stats are defined after success") {
+    val (stats, service) = setup { _ =>
+      Future.value(Response(Status.Ok, Stream.empty()))
+    }
+    withClue("before request") {
+      // stats undefined before request
+      for {stat <- latencyStats}
+        withClue(s"stat: $stat") { assert(!stats.stats.isDefinedAt(stat)) }
+    }
+
+    val req = Request("http", Method.Get, "hihost", "/", Stream.empty())
+    await(service(req))
+    withClue("after success") {
+      for {stat <- latencyStats}
+        withClue(s"stat: $stat") { assert(stats.stats.isDefinedAt(stat)) }
+    }
+  }
+
+  test("stats are defined after failure") {
+    val (stats, service) = setup { _ =>
+      Future.exception(Thrown)
+    }
+    withClue("before request") {
+      // stats undefined before request
+      for {stat <- latencyStats}
+        withClue(s"stat: $stat") {assert(!stats.stats.isDefinedAt(stat)) }
+    }
+
+    val req = Request("http", Method.Get, "hihost", "/", Stream.empty())
+    assertThrows[Throwable] { await(service(req)) }
+    withClue("after failure") {
+      for { stat <- latencyStats.filter(_ != Seq("response", "stream", "stream_duration_ms")) }
+        withClue(s"stat: $stat") { assert(stats.stats.isDefinedAt(stat)) }
+    }
+  }
+}
