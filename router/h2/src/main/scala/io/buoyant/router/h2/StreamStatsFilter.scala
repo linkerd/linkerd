48790e41a02b5285e6186828e9ac5c343fba332a
Eliza Weisman <eliza@buoyant.io>
2017-07-18 13:16:16 -0500

    Use H2ResponseClassifier for global success/failure counters

--- router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
@@ -1,8 +1,9 @@
 package io.buoyant.router.h2
 
 import com.twitter.finagle.{Status => _, _}
-import com.twitter.finagle.buoyant.h2.{Frame, H2ResponseClassifier, Request, Response, Stream, param => h2param}
+import com.twitter.finagle.buoyant.h2.{Frame, H2ReqRep, H2ResponseClassifier, Request, Response, Stream, param => h2param}
 import com.twitter.finagle.buoyant.h2.StreamProxy._
+import com.twitter.finagle.service.ResponseClass.Successful
 import com.twitter.finagle.stats.StatsReceiver
 import com.twitter.util._
 
@@ -30,15 +31,12 @@ class StreamStatsFilter(statsReceiver: StatsReceiver, classifier: H2ResponseClas
   class StreamStats(
     stats: StatsReceiver,
     durationName: Option[String] = None,
-    successName: Option[String] = None
   ) {
 
     private[this] val durationMs =
       stats.stat(s"${durationName.getOrElse("stream_duration")}_ms")
-    private[this] val successes =
-      stats.counter(s"${successName.getOrElse("stream_")}successes")
-    private[this] val failures =
-      stats.counter(s"${successName.getOrElse("stream_")}failures")
+    private[this] val successes = stats.counter(s"stream_successes")
+    private[this] val failures = stats.counter(s"stream_failures")
 
     @inline def apply(startT: Stopwatch.Elapsed)(result: Try[_]): Unit = {
       durationMs.add(startT().inMillis)
@@ -47,6 +45,7 @@ class StreamStatsFilter(statsReceiver: StatsReceiver, classifier: H2ResponseClas
         case Throw(_) => failures.incr()
       }
     }
+
   }
 
   //   total number of requests received
@@ -59,11 +58,12 @@ class StreamStatsFilter(statsReceiver: StatsReceiver, classifier: H2ResponseClas
   private[this] val totalStreamStats =
     new StreamStats(statsReceiver.scope("stream"),
                     durationName = Some("total_latency"))
-  private[this] val rspFutureStats =
-    new StreamStats(statsReceiver,
-                    durationName = Some("request_latency"),
-                    successName = Some(""))
   private[this] val frameSizes = statsReceiver.stat("stream", "data_frame", "bytes")
+  private[this] val reqLatency = statsReceiver.stat("request_latency")
+  // overall successes stat from response classifier
+  private[this] val successes = statsReceiver.counter("successes")
+  // overall failures stat from response classifier
+  private[this] val failures = statsReceiver.counter("failures")
 
   override def apply(req: Request, service: Service[Request, Response]): Future[Response] = {
     reqCount.incr()
@@ -75,17 +75,24 @@ class StreamStatsFilter(statsReceiver: StatsReceiver, classifier: H2ResponseClas
         case Return(rsp0) =>
           val stream = rsp0.stream.onFrame {
             case Return(frame) if frame.isEnd =>
-              ???
+              classifier(H2ReqRep(req, Return((rsp0, Return(frame))))) match {
+                case Successful(_) => successes.incr()
+                case _ => failures.incr()
+              }
             case Return(frame: Frame.Data) =>
               // if the frame is a data frame, update the data frame size stat
               frameSizes.add(frame.buf.length)
-            case Throw(e) =>
-              ???
+            case e @ Throw(_) =>
+              // TODO: make this less repetitive
+              classifier(H2ReqRep(req, Return((rsp0, e))))  match {
+                case Successful(_) => successes.incr()
+                case _ => failures.incr()
+              }
           }
           Future.value(Response(rsp0.headers, stream))
       }
       .respond { result =>
-        rspFutureStats(reqT)(result)
+        reqLatency.add(reqT().inMillis)
         val stream = result match {
           case Return(rsp) => req.stream.onEnd.join(rsp.stream.onEnd)
           case Throw(_) => req.stream.onEnd
