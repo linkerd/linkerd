eb42e4905d7e2cd7180e554d8f2dec65fd648430
Eliza Weisman <eliza@buoyant.io>
2017-07-19 17:11:39 -0500

    Make English code more idiomatic

--- finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/package.scala
+++ finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/package.scala
@@ -6,9 +6,10 @@ import com.twitter.util.Try
 package object h2 {
   case class H2ReqRep(request: Request, response: Try[(Response, Try[Frame])])
   /**
-   * A HTTP/2 response classifier is not unlike a standard
+   * A HTTP/2 response classifier is like a standard
    * [[com.twitter.finagle.service.ResponseClassifier ResponseClassifier]],
-   * except that it takes a [[Frame]] as well as a [[Response]] as inputs.
+   * except that it takes the final [[Frame]] in the [[Stream]] as well as
+   * a [[Response]] as inputs.
    */
   type H2ResponseClassifier = PartialFunction[H2ReqRep, ResponseClass]
 }
