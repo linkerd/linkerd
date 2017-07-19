bc87529d06c6246faaa6c8350b545e3fc588e31b
Eliza Weisman <eliza@buoyant.io>
2017-07-19 13:38:45 -0500

    Use NameTreeParsers to validate generated paths

--- finagle/buoyant/src/test/scala/io/buoyant/finagle/test/DtabGenerators.scala
+++ finagle/buoyant/src/test/scala/io/buoyant/finagle/test/DtabGenerators.scala
@@ -1,23 +1,22 @@
 
-package io.buoyant.finagle.test
+package com.twitter.finagle.buoyant.test
 
-import com.twitter.finagle.Path
+import com.twitter.finagle.{NameTreeParsers, Path}
 import com.twitter.io.Buf
 import com.twitter.io.Buf.ByteArray
 import org.scalacheck.{Arbitrary, Gen}
+import scala.util.Try
 
 trait DtabGenerators {
   val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._"
   val labelChar = for { i <- Gen.choose(0, alphabet.length) } yield alphabet(i)
-  val label: Gen[Buf] = Gen.sized { size =>
+  val label: Gen[String] = Gen.sized { size =>
     if (size == 1) Gen.oneOf("#", "*")
     else Gen.listOfN(size, labelChar).map { _.mkString("") }
-  }.map { str =>
-    ByteArray(str.map(_.toByte):_*)
-  }
+  } suchThat { str => Try(NameTreeParsers.parsePath(str)).isSuccess }
 
   implicit def arbPath: Arbitrary[Path] = Arbitrary {
     def pathElems(size: Int) = Gen.listOfN(size, label)
-    Gen.sized { size => pathElems(size).map{ bufs => Path(bufs:_*) } }
+    Gen.sized { size => pathElems(size) map { elems => NameTreeParsers.parsePath(elems.mkString("/"))} }
   }
 }
