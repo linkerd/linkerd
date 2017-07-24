4037f0ef89ae67493e93831843887b7f5bae755b
Eliza Weisman <eliza@buoyant.io>
2017-07-24 17:43:50 -0500

    Add JsonSubTypes to ResponseClassifierConfig

--- linkerd/protocol/h2/src/main/scala/io/buoyant/linkerd/protocol/h2/ResponseClassifierConfig.scala
+++ linkerd/protocol/h2/src/main/scala/io/buoyant/linkerd/protocol/h2/ResponseClassifierConfig.scala
@@ -1,10 +1,22 @@
 package io.buoyant.linkerd.protocol.h2
 
-import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.fasterxml.jackson.annotation.{JsonIgnore, JsonSubTypes}
 import com.twitter.finagle.buoyant.h2.service.ResponseClassifier
 import io.buoyant.config.{ConfigInitializer, PolymorphicConfig}
 
 abstract class ResponseClassifierInitializer extends ConfigInitializer
+
+@JsonSubTypes(Array(
+  new Type(value = classOf[RetryableIdempotent5XXConfig],
+    name = "io.l5d.h2.retryableIdempotent5XX"),
+  new Type(value = classOf[RetryableRead5XXConfig ],
+    name = "io.l5d.h2.retryableRead5XX"),
+  new Type(value = classOf[NonRetryable5XXConfig],
+    name = "io.l5d.h2.nonRetryable5XX"),
+  new Type(value = classOf[AllSuccessfulConfig],
+    name = "io.l5d.h2.allSuccessful")
+))
 abstract class ResponseClassifierConfig extends PolymorphicConfig {
   @JsonIgnore
   def mk: ResponseClassifier
