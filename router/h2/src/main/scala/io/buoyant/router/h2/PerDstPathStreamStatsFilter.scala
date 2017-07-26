b5ca203d0b2347b8894382a65f257395bbd678fb
Eliza Weisman <eliza@buoyant.io>
2017-07-26 13:52:16 -0500

    Separate PerDstPath and LocalClassifier StreamStats filters

--- router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
@@ -14,28 +14,28 @@ import io.buoyant.router.context.h2.StreamClassifierCtx
 object PerDstPathStreamStatsFilter {
 
   def module: Stackable[ServiceFactory[Request, Response]] =
-    new Stack.Module2[param.Stats, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
+    new Stack.Module3[param.Stats, StreamStatsFilter.Param, H2StreamClassifier, ServiceFactory[Request, Response]] {
       val role: Stack.Role = PerDstPathStatsFilter.role
       val description = "Report request statistics for each logical destination"
 
       override def make(
         statsP: param.Stats,
         statsFilterP: StreamStatsFilter.Param,
+        classifierP: H2StreamClassifier,
         next: ServiceFactory[Request, Response]
       ): ServiceFactory[Request, Response] =
         statsP match {
           case param.Stats(stats) if !stats.isNull =>
             val StreamStatsFilter.Param(timeUnit) = statsFilterP
+            val H2StreamClassifier(classifier) = classifierP
 
-            def mkScopedStatsFilter(path: Path): SimpleFilter[Request, Response] = {
+            def mkScopedStatsFilter(path: Path): Filter[Request, Response, Request, Response] = {
               val name = path.show.stripPrefix("/")
               val scopedStats = stats.scope("service", name)
-              val H2StreamClassifier(classifier) =
-                StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
               new StreamStatsFilter(scopedStats, classifier, timeUnit)
             }
 
-            val filter = new PerDstPathFilter(mkScopedStatsFilter)
+            val filter = new PerDstPathFilter(mkScopedStatsFilter _)
             filter.andThen(next)
 
           // can this actually be null? the HTTP1 `PerDstPathStatsFilter`
