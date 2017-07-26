b1fd7da724a01a7bcf585c5804986207c9028583
Eliza Weisman <eliza@buoyant.io>
2017-07-26 13:55:13 -0500

    Fix whitespace in LocalClassifierSSF (and remove bonus curly)

--- router/h2/src/main/scala/io/buoyant/router/h2/LocalClassifierStreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/LocalClassifierStreamStatsFilter.scala
@@ -1,50 +1,48 @@
+package io.buoyant.router.h2
 
-  package io.buoyant.router.h2
+import com.twitter.finagle.param
+import com.twitter.finagle._
+import com.twitter.finagle.buoyant.h2.{Request, Response}
+import com.twitter.finagle.buoyant.h2.param.H2StreamClassifier
+import io.buoyant.router.{PerDstPathFilter, PerDstPathStatsFilter}
+import io.buoyant.router.context.h2.StreamClassifierCtx
 
-  import com.twitter.finagle.param
-  import com.twitter.finagle._
-  import com.twitter.finagle.buoyant.h2.{Request, Response}
-  import com.twitter.finagle.buoyant.h2.param.H2StreamClassifier
-  import io.buoyant.router.{PerDstPathFilter, PerDstPathStatsFilter}
-  import io.buoyant.router.context.h2.StreamClassifierCtx
+/**
+  * Like [[io.buoyant.router.LocalClassifierStatsFilter]],
+  * but specialized for H2 streams.
+  */
+object LocalClassifierStreamStatsFilter {
 
-  /**
-    * Like [[io.buoyant.router.LocalClassifierStatsFilter]],
-    * but specialized for H2 streams.
-    */
-  object LocalClassifierStreamStatsFilter {
+  def module: Stackable[ServiceFactory[Request, Response]] =
+    new Stack.Module2[param.Stats, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
+      val role: Stack.Role = PerDstPathStatsFilter.role
+      val description = "Report request statistics for each logical destination"
 
-    def module: Stackable[ServiceFactory[Request, Response]] =
-      new Stack.Module2[param.Stats, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
-        val role: Stack.Role = PerDstPathStatsFilter.role
-        val description = "Report request statistics for each logical destination"
+      override def make(
+        statsP: param.Stats,
+        statsFilterP: StreamStatsFilter.Param,
+        next: ServiceFactory[Request, Response]
+      ): ServiceFactory[Request, Response] =
+        statsP match {
+          case param.Stats(stats) if !stats.isNull =>
+            val StreamStatsFilter.Param(timeUnit) = statsFilterP
 
-        override def make(
-          statsP: param.Stats,
-          statsFilterP: StreamStatsFilter.Param,
-          next: ServiceFactory[Request, Response]
-        ): ServiceFactory[Request, Response] =
-          statsP match {
-            case param.Stats(stats) if !stats.isNull =>
-              val StreamStatsFilter.Param(timeUnit) = statsFilterP
+            def mkScopedStatsFilter(path: Path): Filter[Request, Response, Request, Response] = {
+              val name = path.show.stripPrefix("/")
+              val scopedStats = stats.scope("service", name)
+              val H2StreamClassifier(classifier) =
+                StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
+              new StreamStatsFilter(scopedStats, classifier, timeUnit)
+            }
 
-              def mkScopedStatsFilter(path: Path): Filter[Request, Response, Request, Response] = {
-                val name = path.show.stripPrefix("/")
-                val scopedStats = stats.scope("service", name)
-                val H2StreamClassifier(classifier) =
-                  StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
-                new StreamStatsFilter(scopedStats, classifier, timeUnit)
-              }
+            val filter = new PerDstPathFilter(mkScopedStatsFilter _)
+            filter.andThen(next)
 
-              val filter = new PerDstPathFilter(mkScopedStatsFilter _)
-              filter.andThen(next)
+          // can this actually be null? the HTTP1 `PerDstPathStatsFilter`
+          // checks for this case, so i figured we ought to here as well...
+          case _ => next
 
-            // can this actually be null? the HTTP1 `PerDstPathStatsFilter`
-            // checks for this case, so i figured we ought to here as well...
-            case _ => next
-
-          }
-      }
-  }
+        }
+    }
 
 }
