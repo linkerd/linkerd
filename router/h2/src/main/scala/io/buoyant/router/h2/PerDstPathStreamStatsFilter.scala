49e9a96d931bc9acd7d19fd85f929f52681a3ab4
Eliza Weisman <eliza@buoyant.io>
2017-07-26 12:45:05 -0500

    First pass on PerDstPathStreamStatsFilter

--- router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
@@ -1,25 +1,47 @@
 package io.buoyant.router.h2
 
-import com.twitter.finagle.param.{ExceptionStatsHandler, Stats}
-import com.twitter.finagle.{ServiceFactory, Stack, Stackable, param}
-import com.twitter.finagle.service.StatsFilter
-import io.buoyant.router.PerDstPathStatsFilter
+import com.twitter.finagle.param
+import com.twitter.finagle._
+import com.twitter.finagle.buoyant.h2.{Request, Response}
+import com.twitter.finagle.buoyant.h2.param.H2StreamClassifier
+import io.buoyant.router.{PerDstPathFilter, PerDstPathStatsFilter}
+import io.buoyant.router.context.h2.StreamClassifierCtx
 
 /**
-  * Like [[io.buoyant.router.PerDstPathStatsFilter]],
-  * but specialized for H2 streams.
-  */
+ * Like [[io.buoyant.router.PerDstPathStatsFilter]],
+ * but specialized for H2 streams.
+ */
 object PerDstPathStreamStatsFilter {
 
-  def module[Req, Rsp]: Stackable[ServiceFactory[Req, Rsp]] =
-    new Stack.Module3[param.Stats, param.ExceptionStatsHandler, StreamStatsFilter.Param, ServiceFactory[Req, Rsp]] {
-      val role = PerDstPathStatsFilter.role
+  def module: Stackable[ServiceFactory[Request, Response]] =
+    new Stack.Module2[param.Stats, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
+      val role: Stack.Role = PerDstPathStatsFilter.role
       val description = "Report request statistics for each logical destination"
 
       override def make(
-        p1: Stats,
-        p2: ExceptionStatsHandler,
-        p3: StreamStatsFilter.Param,
-        next: ServiceFactory[Req, Rsp]): ServiceFactory[Req, Rsp] = ???
+        statsP: param.Stats,
+        statsFilterP: StreamStatsFilter.Param,
+        next: ServiceFactory[Request, Response]
+      ): ServiceFactory[Request, Response] =
+        statsP match {
+          case param.Stats(stats) if !stats.isNull =>
+            val StreamStatsFilter.Param(timeUnit) = statsFilterP
+
+            def mkScopedStatsFilter(path: Path): SimpleFilter[Request, Response] = {
+              val name = path.show.stripPrefix("/")
+              val scopedStats = stats.scope("service", name)
+              val H2StreamClassifier(classifier) =
+                StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
+              new StreamStatsFilter(scopedStats, classifier, timeUnit)
+            }
+
+            val filter = new PerDstPathFilter(mkScopedStatsFilter)
+            filter.andThen(next)
+
+          // can this actually be null? the HTTP1 `PerDstPathStatsFilter`
+          // checks for this case, so i figured we ought to here as well...
+          case _ => next
+
+        }
     }
 }
