1bd259b33c2129f5768d88ff3c0fd9979ea15474
Eliza Weisman <eliza@buoyant.io>
2017-07-26 12:42:57 -0500

    Initial pass on end-to-end test for stream classifiers

--- finagle/h2/src/test/scala/io/buoyant/test/h2/StreamTestUtils.scala
+++ finagle/h2/src/test/scala/io/buoyant/test/h2/StreamTestUtils.scala
@@ -5,16 +5,16 @@ import com.twitter.util.Future
 
 object StreamTestUtils {
   /**
-    * Read a [[Stream]] to the end, [[Frame.release release()]]ing each
-    * [[Frame]] before reading the next one.
-    *
-    * The value of each frame is discarded, but assertions can be made about
-    * their contents by attaching an [[Stream.onFrame onFrame()]] callback
-    * before calling `readAll()`.
-    *
-    * @param stream the [[Stream]] to read to the end
-    * @return a [[Future]] that will finish when the whole stream is read
-    */
+   * Read a [[Stream]] to the end, [[Frame.release release()]]ing each
+   * [[Frame]] before reading the next one.
+   *
+   * The value of each frame is discarded, but assertions can be made about
+   * their contents by attaching an [[Stream.onFrame onFrame()]] callback
+   * before calling `readAll()`.
+   *
+   * @param stream the [[Stream]] to read to the end
+   * @return a [[Future]] that will finish when the whole stream is read
+   */
   final def readToEnd(stream: Stream): Future[Unit] =
     stream.read().flatMap { frame =>
       val end = frame.isEnd
@@ -24,11 +24,11 @@ object StreamTestUtils {
     }
 
   /**
-    * Enhances a [[Stream]] by providing the [[readToEnd()]] function in the
-    * method position
- *
-    * @param stream the underlying [[Stream]]
-    */
+   * Enhances a [[Stream]] by providing the [[readToEnd()]] function in the
+   * method position
+   *
+   * @param stream the underlying [[Stream]]
+   */
   implicit class ReadAllStream(val stream: Stream) extends AnyVal {
     @inline def readToEnd: Future[Unit] = StreamTestUtils.readToEnd(stream)
   }
--- linkerd/protocol/h2/src/e2e/scala/io/buoyant/linkerd/protocol/h2/StreamClassificationEndToEndTest.scala
+++ linkerd/protocol/h2/src/e2e/scala/io/buoyant/linkerd/protocol/h2/StreamClassificationEndToEndTest.scala
@@ -32,13 +32,37 @@ class StreamClassificationEndToEndTest extends FunSuite with Awaits {
       .newClient(name, "upstream").toService
   }
 
+
+  private[this] val successReqCounters = Seq(
+    Seq("request", "stream", "stream_success"),
+    Seq("requests")
+  )
+
+  private[this] val successRspCounters = Seq(
+    Seq("response", "stream", "stream_success"),
+    Seq("stream", "stream_success"),
+    Seq("success")
+  )
+
+  private[this] val successCounters = successReqCounters ++ successRspCounters
+
+  def requestSuccessCounters(port: Int, svc: String): Seq[Seq[String]] =
+    (for { counter <- successReqCounters } yield { Seq("rt", "h2", "server", "127.0.0.1/0") ++ counter}) ++ Seq(
+      Seq("rt", "h2", "client", s"$$/inet/127.1/$port", "service", s"svc/$svc", "success")
+    )
+  def responseSuccessCounters(port: Int, svc: String): Seq[Seq[String]] =
+    requestSuccessCounters(port, svc) ++
+    (for { counter <- successRspCounters } yield { Seq("rt", "h2", "server", "127.0.0.1/0") ++ counter})
+
   test("success") {
     val downstream = Downstream("ds", Service.mk { req =>
       Future.value(Response(Status.Ok, Stream.const("aaaaaaa")))
     })
+
     val config =
       s"""|routers:
           |- protocol: h2
+          |  experimental: true
           |  dtab: /svc/* => /$$/inet/127.1/${downstream.port}
           |  service:
           |    responseClassifier:
@@ -54,13 +78,187 @@ class StreamClassificationEndToEndTest extends FunSuite with Awaits {
     val client = upstream(server)
 
     val req = Request("http", Method.Get, "foo", "/", Stream.empty())
-    await(client(req))
+    val rsp = await(client(req))
 
-    withClue(s"after request ($req):") {
-      assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "success")) == 1)
-      assert(stats.counters(Seq("rt", "h2", "service", "svc/foo", "success")) == 1)
-      assert(stats.counters(Seq("rt", "h2", "server", "127.0.0.1/0", "success")) == 1)
+    withClue("after request, before response:") {
+      for { counter <- requestSuccessCounters(downstream.port, "foo") }
+        assert(stats.counters(counter) == 1)
     }
 
+    await(rsp.stream.readToEnd)
+
+    withClue("after response:") {
+      for { counter <- responseSuccessCounters(downstream.port, "foo") }
+        assert(stats.counters(counter) == 1)
+    }
+
+  }
+  test("non-retryable failure") {
+    val downstream = Downstream("ds", Service.mk { req =>
+      Future.value(Response(Status.InternalServerError, Stream.empty()))
+    })
+
+    val config =
+      s"""|routers:
+          |- protocol: h2
+          |  experimental: true
+          |  dtab: /svc/* => /$$/inet/127.1/${downstream.port}
+          |  service:
+          |    responseClassifier:
+          |      kind: io.l5d.h2.retryableRead5XX
+          |  servers:
+          |  - port: 0
+          |""".stripMargin
+
+    val stats = new InMemoryStatsReceiver
+    val linker = Linker.load(config).configured(param.Stats(stats))
+    val router = linker.routers.head.initialize()
+    val server = router.servers.head.serve()
+    val client = upstream(server)
+
+    val req = Request("http", Method.Post, "foo", "/", Stream.empty())
+    val rsp = await(client(req))
+    await(rsp.stream.onEnd)
+    withClue("after response:") {
+      assert(stats
+        .counters(
+          Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "failures")) == 1)
+      assert(stats.counters(Seq("rt", "h2", "service", "svc/foo", "failures")) == 1)
+      assert(stats.counters(Seq("rt", "h2", "server", "127.0.0.1/0", "failures")) == 1)
+    }
+  }
+
+
+  test("retryable failure") {
+    @volatile var i = 0
+    val downstream = Downstream("ds", Service.mk { req =>
+      if (i == 0) {
+        i += 1
+        val rsp = Response(Status.InternalServerError, Stream.empty)
+        Future.value(rsp)
+      } else {
+        i += 1
+        Future.value(Response(Status.Ok, Stream.empty))
+      }
+    })
+    val config =
+      s"""|routers:
+          |- protocol: h2
+          |  experimental: true
+          |  dtab: /svc/* => /$$/inet/127.1/${downstream.port}
+          |  service:
+          |    responseClassifier:
+          |      kind: io.l5d.h2.retryableRead5XX
+          |  servers:
+          |  - port: 0
+          |""".stripMargin
+
+    val stats = new InMemoryStatsReceiver
+    val linker = Linker.load(config).configured(param.Stats(stats))
+    val router = linker.routers.head.initialize()
+    val server = router.servers.head.serve()
+    val client = upstream(server)
+
+    val req = Request("http", Method.Post, "foo", "/", Stream.empty())
+    await(client(req))
+
+    assert(stats.counters(Seq("rt", "h2", "server", "127.0.0.1/0", "success")) == 1)
+    assert(stats.counters.get(Seq("rt", "h2", "server", "127.0.0.1/0", "failures")) == None)
+    assert(stats.counters(Seq("rt", "h2", "service", "svc/foo", "success")) == 1)
+    assert(stats.counters.get(Seq("rt", "h2", "service", "svc/foo", "failures")) == None)
+    assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "success")) == 1)
+    assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "failures")) == 1)
+  }
+
+  test("per service classification") {
+    @volatile var i = 0
+    val downstream = Downstream("ds", Service.mk { req =>
+      if (i % 2 == 0) {
+        i += 1
+        val rsp = Response(Status.InternalServerError, Stream.empty)
+        Future.value(rsp)
+      } else {
+        i += 1
+        Future.value(Response(Status.Ok, Stream.empty))
+      }
+    })
+    val config =
+      s"""|routers:
+          |- protocol: h2
+          |  experimental: true
+          |  dtab: /svc/* => /$$/inet/127.1/${downstream.port}
+          |  service:
+          |    kind: io.l5d.static
+          |    configs:
+          |    - prefix: /svc/a
+          |      responseClassifier:
+          |        kind: io.l5d.h2.retryableRead5XX
+          |    - prefix: /svc/b
+          |      responseClassifier:
+          |        kind: io.l5d.h2.nonRetryable5XX
+          |  servers:
+          |  - port: 0
+          |""".stripMargin
+
+    val stats = new InMemoryStatsReceiver
+    val linker = Linker.load(config).configured(param.Stats(stats))
+    val router = linker.routers.head.initialize()
+    val server = router.servers.head.serve()
+    val client = upstream(server)
+
+    var req = Request("http", Method.Post, "a", "/", Stream.empty())
+    await(client(req))
+
+    assert(stats.counters.get(Seq("rt", "h2", "server", "127.0.0.1/0", "success")) == Some(1))
+    assert(stats.counters.get(Seq("rt", "h2", "server", "127.0.0.1/0", "failures")) == None)
+    assert(stats.counters(Seq("rt", "h2", "service", "svc/a", "success")) == 1)
+    assert(stats.counters.get(Seq("rt", "h2", "service", "svc/a", "failures")) == None)
+    assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/a", "success")) == 1)
+    assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/a", "failures")) == 1)
+
+    // Request to "b" is non-retryable
+    // fails and doesn't retry
+
+    req = Request("http", Method.Post, "b", "/", Stream.empty())
+    await(client(req))
+
+    assert(stats.counters(Seq("rt", "h2", "server", "127.0.0.1/0", "success")) == 1)
+    assert(stats.counters(Seq("rt", "h2", "server", "127.0.0.1/0", "failures")) == 1)
+    assert(stats.counters.get(Seq("rt", "h2", "service", "svc/b", "success")) == None)
+    assert(stats.counters(Seq("rt", "h2", "service", "svc/b", "failures")) == 1)
+    assert(stats.counters.get(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/b", "success")) == None)
+    assert(stats.counters(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/b", "failures")) == 1)
+  }
+
+  test("client stats use service response classifier") {
+    val downstream = Downstream("ds", Service.mk { req =>
+      val rsp = Response(Status.InternalServerError, Stream.empty)
+      Future.value(rsp)
+    })
+    val config =
+      s"""|routers:
+          |- protocol: h2
+          |  experimental: true
+          |  dtab: /svc/* => /$$/inet/127.1/${downstream.port}
+          |  service:
+          |    responseClassifier:
+          |      kind: io.l5d.h2.allSuccessful
+          |  servers:
+          |  - port: 0
+          |""".stripMargin
+
+    val stats = new InMemoryStatsReceiver
+    val linker = Linker.load(config).configured(param.Stats(stats))
+    val router = linker.routers.head.initialize()
+    val server = router.servers.head.serve()
+    val client = upstream(server)
+
+    val req = Request("http", Method.Post, "foo", "/", Stream.empty)
+    await(client(req))
+
+    assert(stats.counters.get(Seq("rt", "h2", "service", "svc/foo", "success")) == Some(1))
+    assert(stats.counters.get(Seq("rt", "h2", "service", "svc/foo", "failures")) == None)
+    assert(stats.counters.get(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "success")) == Some(1))
+    assert(stats.counters.get(Seq("rt", "h2", "client", s"$$/inet/127.1/${downstream.port}", "service", "svc/foo", "failures")) == None)
   }
 }
