d01b26b1f9d1e2c8fc472b0111ec4309287dd602
Eliza Weisman <eliza@buoyant.io>
2017-07-26 10:37:30 -0500

    Add StreamClassifierCtx

--- router/h2/src/main/scala/io/buoyant/router/context/h2/StreamClassifierCtx.scala
+++ router/h2/src/main/scala/io/buoyant/router/context/h2/StreamClassifierCtx.scala
@@ -4,10 +4,10 @@ import com.twitter.finagle.buoyant.h2.param
 import io.buoyant.router.context.LocalKey
 
 /**
- * We set the StreamClassifier from the path stack into a local context
- * so that when the request enters the client stack, it can use the path stack's
- * stream classifier for reporting stats.
- *
- * This is based directly on `ResponseClassifierCtx` in the H1 protocol
- */
+  * We set the StreamClassifier from the path stack into a local context
+  * so that when the request enters the client stack, it can use the path stack's
+  * stream classifier for reporting stats.
+  *
+  * This is based directly on `ResponseClassifierCtx` in the H1 protocol
+  */
 object StreamClassifierCtx extends LocalKey[param.H2StreamClassifier]("H2StreamClassifier")
