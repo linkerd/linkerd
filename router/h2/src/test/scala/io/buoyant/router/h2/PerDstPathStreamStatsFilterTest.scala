53a365752e76f9c509c0845c4d548b6996da260f
Eliza Weisman <eliza@buoyant.io>
2017-07-26 09:46:51 -0500

    Add test for PerDstPathStreamStatsFilter (based on the test for H1)

--- router/h2/src/test/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilterTest.scala
+++ router/h2/src/test/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilterTest.scala
@@ -5,89 +5,64 @@ import com.twitter.finagle.context.Contexts
 import com.twitter.finagle.stack.{Endpoint, nilStack}
 import com.twitter.finagle.stats.InMemoryStatsReceiver
 import com.twitter.finagle._
-import com.twitter.finagle.buoyant.h2.{Method, Request, Response, Status, Stream}
 import com.twitter.util.{Future, Local}
 import io.buoyant.router.context.DstPathCtx
 import io.buoyant.test.FunSuite
-import org.scalatest.Matchers
 
 class NotDog extends Exception
 
 class DangCat extends Exception("meow", new NotDog)
 
-class PerDstPathStreamStatsFilterTest extends FunSuite with Matchers {
+class PerDstPathStreamStatsFilterTest extends FunSuite {
 
-  def setContext(f: Request => Path) =
-    Filter.mk[Request, Response, Request, Response] { (req, service) =>
+  def setContext(f: String => Path) =
+    Filter.mk[String, Unit, String, Unit] { (req, service) =>
       val save = Local.save()
       try Contexts.local.let(DstPathCtx, Dst.Path(f(req))) { service(req) }
       finally Local.restore(save)
     }
 
-  val service = Service.mk[Request, Response] {
-    case r: Request if r.path == "cat" => Future.exception(new DangCat)
-    case _ => Future.value(Response(Status.Ok, Stream.empty()))
+  val service = Service.mk[String, Unit] {
+    case "cat" => Future.exception(new DangCat)
+    case _ => Future.Unit
   }
 
   val stack = {
     val sf = ServiceFactory(() => Future.value(service))
-    val stk = new StackBuilder[ServiceFactory[Request, Response]](nilStack)
-    stk.push(PerDstPathStreamStatsFilter.module)
+    val stk = new StackBuilder[ServiceFactory[String, Unit]](nilStack)
+    stk.push(PerDstPathStreamStatsFilter.module[String, Unit])
     stk.result ++ Stack.Leaf(Endpoint, sf)
   }
 
-  val dogReq = Request("http", Method.Get, "foo", "dog", Stream.empty())
-  val catReq = Request("http", Method.Get, "foo", "cat", Stream.empty())
   test("module installs a per-path StreamStatsFilter") {
     val stats = new InMemoryStatsReceiver
     val params = Stack.Params.empty + param.Stats(stats.scope("pfx"))
-    val ctxFilter = setContext({ r => Path.Utf8("req", r.path) })
+    val ctxFilter = setContext(Path.Utf8("req", _))
     val factory = ctxFilter.andThen(stack.make(params))
     val service = await(factory())
 
-    await(service(dogReq))
-    assert(await(service(catReq).liftToTry).isThrow)
-    await(service(dogReq))
+    await(service("dog"))
+    assert(await(service("cat").liftToTry).isThrow)
+    await(service("dog"))
 
     val pfx = Seq("pfx", "service")
-    val catPfx = pfx :+ "req/cat"
-    val dogPfx = pfx :+ "req/dog"
-    assert(
-      stats.counters.get(catPfx :+ "requests").contains(1),
-      s"actually got: ${stats.counters}"
-    )
-    assert(
-      stats.counters.get(catPfx :+ "failures").contains(1),
-      s"actually got: ${stats.counters}"
-    )
-    //    assert(
-    //      stats.counters.get(catPfx :+ "requests" :+ "io.buoyant.router.DangCat").contains(1),
-    //      s"actually got: ${stats.counters}"
-    //    )
-    //    assert(stats.counters.get(catPfx :+ "failures" :+ "io.buoyant.router.DangCat").contains(1))
-    //    assert(stats.counters.get(catPfx :+ "failures" :+ "io.buoyant.router.DangCat" :+ "io.buoyant.router.NotDog").contains(1))
-
-    assert(stats.counters.get(dogPfx :+ "requests").contains(2))
-    assert(stats.counters.get(dogPfx :+ "success").contains(2))
-
-    //    assert(stats.gauges.keys == Set(
-    //      (catPfx :+ "pending"),
-    //      (dogPfx :+ "pending")
-    //    ))
+    val catPfx = pfx :+ "req/cat/stream"
+    val dogPfx = pfx :+ "req/dog/stream"
+    assert(stats.counters == Map(
+      (catPfx :+ "requests") -> 1,
+      (catPfx :+ "failures") -> 1,
+      (catPfx :+ "failures" :+ "io.buoyant.router.DangCat") -> 1,
+      (catPfx :+ "failures" :+ "io.buoyant.router.DangCat" :+ "io.buoyant.router.NotDog") -> 1,
+      (dogPfx :+ "requests") -> 2,
+      (dogPfx :+ "success") -> 2
+    ))
+    assert(stats.gauges.keys == Set(
+      (catPfx :+ "pending"),
+      (dogPfx :+ "pending")
+    ))
     assert(stats.histogramDetails.keys == Set(
-      "pfx/service/req/cat/request_latency_ms",
-      "pfx/service/req/dog/request_latency_ms",
-
-      "pfx/service/req/cat/stream/total_latency_ms",
-      "pfx/service/req/dog/stream/total_latency_ms",
-
-      "pfx/service/req/cat/request/stream/stream_duration_ms",
-      "pfx/service/req/dog/request/stream/stream_duration_ms",
-      "pfx/service/req/dog/response/stream/stream_duration_ms",
-
-      "pfx/service/req/cat/request/stream/data_bytes",
-      "pfx/service/req/dog/request/stream/data_bytes",
-      "pfx/service/req/dog/response/stream/data_bytes"
+      "pfx/service/req/cat/stream/request_latency_ms",
+      "pfx/service/req/dog/stream/request_latency_ms"
     ))
   }
 
@@ -98,9 +73,9 @@ class PerDstPathStreamStatsFilterTest extends FunSuite with Matchers {
     val service = await(factory())
 
     Contexts.local.letClear(DstPathCtx) {
-      await(service(dogReq))
-      assert(await(service(catReq).liftToTry).isThrow)
-      await(service(dogReq))
+      await(service("dog"))
+      assert(await(service("cat").liftToTry).isThrow)
+      await(service("dog"))
     }
 
     assert(stats.counters.isEmpty)
@@ -116,9 +91,9 @@ class PerDstPathStreamStatsFilterTest extends FunSuite with Matchers {
     val service = await(factory())
 
     Contexts.local.letClear(DstPathCtx) {
-      await(service(dogReq))
-      assert(await(service(catReq).liftToTry).isThrow)
-      await(service(dogReq))
+      await(service("dog"))
+      assert(await(service("cat").liftToTry).isThrow)
+      await(service("dog"))
     }
 
     assert(stats.counters.isEmpty)
