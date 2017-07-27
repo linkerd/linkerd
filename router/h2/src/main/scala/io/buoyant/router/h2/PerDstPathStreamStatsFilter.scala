74220a6230c2239a47a8a229fa21006b592751c8
Eliza Weisman <eliza@buoyant.io>
2017-07-27 11:13:53 -0500

    Add ExceptionStatsHandler to LocalClassifier & PerDstPath stream stats

--- router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
@@ -13,13 +13,14 @@ import io.buoyant.router.context.h2.StreamClassifierCtx
  */
 object PerDstPathStreamStatsFilter {
 
-  def module: Stackable[ServiceFactory[Request, Response]] =
-    new Stack.Module2[param.Stats, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
-      val role: Stack.Role = PerDstPathStatsFilter.role
-      val description = "Report request statistics for each logical destination"
+  val module: Stackable[ServiceFactory[Request, Response]] =
+    new Stack.Module3[param.Stats, param.ExceptionStatsHandler, StreamStatsFilter.Param, ServiceFactory[Request, Response]] {
+      override val role: Stack.Role = PerDstPathStatsFilter.role
+      override val description = "Report request statistics for each logical destination"
 
       override def make(
         statsP: param.Stats,
+        exHandlerP: param.ExceptionStatsHandler,
         statsFilterP: StreamStatsFilter.Param,
         next: ServiceFactory[Request, Response]
       ): ServiceFactory[Request, Response] =
@@ -28,14 +29,15 @@ object PerDstPathStreamStatsFilter {
             val StreamStatsFilter.Param(timeUnit) = statsFilterP
             val H2StreamClassifier(classifier) =
               StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
+            val param.ExceptionStatsHandler(exHandler) = exHandlerP
 
-            def mkScopedStatsFilter(path: Path): Filter[Request, Response, Request, Response] = {
+            def mkScopedStatsFilter(path: Path): SimpleFilter[Request, Response] = {
               val name = path.show.stripPrefix("/")
               val scopedStats = stats.scope("service", name)
-              new StreamStatsFilter(scopedStats, classifier, timeUnit)
+              new StreamStatsFilter(scopedStats, classifier, exHandler, timeUnit)
             }
 
-            val filter = new PerDstPathFilter(mkScopedStatsFilter _)
+            val filter = new PerDstPathFilter(mkScopedStatsFilter)
             filter.andThen(next)
 
           // can this actually be null? the HTTP1 `PerDstPathStatsFilter`
