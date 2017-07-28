016bf9cd14797cab66ee79f35375cf862cc5a685
Eliza Weisman <eliza@elizas.website>
2017-07-28 14:42:18 -0500

    Add interpreter for reading from a k8s ConfigMap (#1532)
    This commit adds an interpreter for reading Dtabs from a Kubernetes ConfigMap – as described in issue #1506.
    
    I've built on @klingerf's work in 9a7c55f1449dc44fa95a31ebf74347c1fb9713ad and my changes to `Watchable` e8985e2f1a66ec6726216d5927510dc7b09e14e9 to refactor Kevin's old interpreter code. This interpreter uses the new `Watchable` API for Kubernetes object resources to establish a watch on the requested ConfigMap, access a key containing a dtab, and use that dtab to resolve routes. Updates to the dtab are reflected in the interpreter. The new interpreter is currently marked as experimental until it can be thoroughly tested in production.
    
    I've added unit tests for getting ConfigMaps to ApiTest and new unit tests for the `ConfigMapInterpreterInitializer` configuration. Additionally, I believe that Kevin has tested this a bit in a live Kubernetes cluster. I've also added documentation on using the new interpreter to `linkerd/docs/interpreters.md`.
    
    Closes #1506

---
+++ interpreter/k8s/src/main/scala/io/buoyant/interpreter/k8s/ConfigMapInterpreterInitializer.scala
@@ -0,0 +1,80 @@
+package io.buoyant.interpreter
+package k8s
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.twitter.finagle.Dtab
+import com.twitter.finagle.Stack.Params
+import com.twitter.finagle.naming.NameInterpreter
+import com.twitter.finagle.param.Label
+import com.twitter.logging.Logger
+import com.twitter.util._
+import io.buoyant.config.types.Port
+import io.buoyant.k8s.ClientConfig
+import io.buoyant.k8s.v1._
+import io.buoyant.namer.{ConfiguredDtabNamer, InterpreterConfig, InterpreterInitializer, Param}
+import java.util.concurrent.atomic.AtomicReference
+
+class ConfigMapInterpreterInitializer extends InterpreterInitializer {
+  val configClass = classOf[ConfigMapInterpreterConfig]
+  override def configId: String = "io.l5d.k8s.configMap"
+}
+
+object ConfigMapInterpreterInitializer extends ConfigMapInterpreterInitializer
+
+case class ConfigMapInterpreterConfig(
+  host: Option[String],
+  port: Option[Port],
+  namespace: Option[String],
+  name: String,
+  filename: String
+) extends InterpreterConfig with ClientConfig {
+  require(name != null, "ConfigMap name is required!")
+  require(filename != null, "ConfigMap dtab filename is required!")
+
+  @JsonIgnore
+  override def experimentalRequired = true
+
+  @JsonIgnore
+  def portNum = port.map(_.port)
+
+  @JsonIgnore
+  private[this] val log = Logger()
+
+  @JsonIgnore
+  val api = {
+    val client = mkClient(Params.empty).configured(Label("configMapInterpreter"))
+    val nsOrDefault = namespace.getOrElse(DefaultNamespace)
+    Api(client.newService(dst)).withNamespace(nsOrDefault)
+  }
+
+  @JsonIgnore
+  val act = api.configMap(name)
+    .activity(getDtab) {
+      (dtab, event) =>
+        event match {
+          case ConfigMapAdded(a) => getDtab(a)
+          case ConfigMapModified(m) => getDtab(m)
+          case ConfigMapDeleted(_) =>
+            log.warning(s"k8s ConfigMap $name was deleted!")
+            Dtab.empty
+          case ConfigMapError(e) =>
+            log.error("k8s watch error: %s", e)
+            dtab
+        }
+    }
+
+  @JsonIgnore
+  def getDtab(configMap: ConfigMap): Dtab =
+    configMap.data.get(filename) match {
+      case None =>
+        log.warning(s"dtab at $filename in k8s ConfigMap $name did not exist!")
+        Dtab.empty
+      case Some(data) => Dtab.read(data)
+    }
+
+  @JsonIgnore
+  override def newInterpreter(params: Params): NameInterpreter = {
+    val Param.Namers(namers) = params[Param.Namers]
+    ConfiguredDtabNamer(act, namers)
+  }
+}
