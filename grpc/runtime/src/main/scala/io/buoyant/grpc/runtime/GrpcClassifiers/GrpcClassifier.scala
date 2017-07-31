8958fdbbc3007cb09fc95e49a89d83b5ae977ad5
Eliza Weisman <eliza@buoyant.io>
2017-07-31 15:57:55 -0500

    DRY up GrpcClassifier

--- grpc/runtime/src/main/scala/io/buoyant/grpc/runtime/GrpcClassifiers/GrpcClassifier.scala
+++ grpc/runtime/src/main/scala/io/buoyant/grpc/runtime/GrpcClassifiers/GrpcClassifier.scala
@@ -5,17 +5,29 @@ import com.twitter.finagle.service.ResponseClass
 import com.twitter.util.Return
 import io.buoyant.grpc.runtime.GrpcStatus.{Internal, Ok, Unavailable}
 
+
+trait GrpcClassifier extends H2Classifier {
+  /**
+    * @inheritdoc
+    * Since GRPC sends status codes in the
+    * [[com.twitter.finagle.buoyant.h2.Frame.Trailers Trailers]] frame of an H2
+    * stream, we can never attempt early classification
+    */
+  override val responseClassifier: PartialFunction[H2ReqRep, ResponseClass] =
+    PartialFunction.empty
+}
+
+
 /**
   * [[H2Classifier]]s for gRPC
   */
-object GrpcClassifiers {
-
+object GrpcClassifier {
 
   /**
     * [[H2Classifier]] that classifies all error status
     * codes as [[ResponseClass.RetryableFailure]]
     */
-  object AlwaysRetryable extends H2Classifier {
+  object AlwaysRetryable extends GrpcClassifier {
 
     override val streamClassifier: PartialFunction[H2ReqRepFrame, ResponseClass] = {
       case H2ReqRepFrame(_, Return((_, Some(Return(GrpcStatus(Ok(_))))))) =>
@@ -24,21 +36,13 @@ object GrpcClassifiers {
         ResponseClass.RetryableFailure
     }
 
-    /**
-      * @inheritdoc
-      * Since GRPC sends status codes in the
-      * [[com.twitter.finagle.buoyant.h2.Frame.Trailers Trailers]] frame of an H2
-      * stream, we can never attempt early classification
-      */
-    override val responseClassifier: PartialFunction[H2ReqRep, ResponseClass] =
-      PartialFunction.empty
   }
 
   /**
     * [[H2Classifier]] that classifies all error status
     * codes as [[ResponseClass.NonRetryableFailure]]
     */
-  object NeverRetryable extends H2Classifier {
+  object NeverRetryable extends GrpcClassifier {
 
     override val streamClassifier: PartialFunction[H2ReqRepFrame, ResponseClass] = {
       case H2ReqRepFrame(_, Return((_, Some(Return(GrpcStatus(Ok(_))))))) =>
@@ -47,14 +51,6 @@ object GrpcClassifiers {
         ResponseClass.NonRetryableFailure
     }
 
-    /**
-      * @inheritdoc
-      * Since GRPC sends status codes in the
-      * [[com.twitter.finagle.buoyant.h2.Frame.Trailers Trailers]] frame of an H2
-      * stream, we can never attempt early classification
-      */
-    override val responseClassifier: PartialFunction[H2ReqRep, ResponseClass] =
-      PartialFunction.empty
   }
 
 
