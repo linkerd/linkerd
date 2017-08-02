0024ba4fa54e1a7f7c1c2235bef1630784524ab7
Eliza Weisman <eliza@buoyant.io>
2017-08-02 11:32:10 -0500

    wip

--- finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/StreamProxy.scala
+++ finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/StreamProxy.scala
@@ -1,23 +1,53 @@
 package com.twitter.finagle.buoyant.h2
 import com.twitter.util.{Future, Try}
+import scala.collection.mutable
+
+abstract class StreamProxy(underlying: Stream) extends Stream {
+  override def isEmpty: Boolean = underlying.isEmpty
+  /**
+    * Satisfied when an end-of-stream frame has been read from this
+    * stream.
+    *
+    * If the stream is reset prematurely, onEnd fails with a [[Reset]].
+    */
+  override def onEnd: Future[Unit] = underlying.onEnd
+}
 
 /**
- * Wraps an underlying [[Stream]] with an [[StreamProxy.onFrame onFrame]] function.
+ * Wraps an underlying [[Stream]] with an [[StreamOnFrame.onFrame onFrame]] function.
  * The `onFrame` function is called for each frame in the stream.
+ *
  * @param underlying the [[Stream]] wrapped by this proxy
  * @param onFrame function called for each [[Frame]] in the underlying [[Stream]]
  */
-class StreamProxy(underlying: Stream, onFrame: Try[Frame] => Unit) extends Stream {
-  override def isEmpty: Boolean = underlying.isEmpty
-
+// TODO: consider renaming `onFrame` to `foreach`?
+class StreamOnFrame(underlying: Stream, onFrame: Try[Frame] => Unit) extends StreamProxy(underlying) {
   override def read(): Future[Frame] = underlying.read().respond(onFrame)
-
-  /**
-   * Satisfied when an end-of-stream frame has been read from this
-   * stream.
-   *
-   * If the stream is reset prematurely, onEnd fails with a [[Reset]].
-   */
-  override def onEnd: Future[Unit] = underlying.onEnd
   override def toString: String = s"StreamProxy($underlying, onFrame=$onFrame)"
 }
+
+class StreamFlatMap(underlying: Stream, f: Frame => Seq[Frame]) extends StreamProxy(underlying) {
+  private[this] val q = new mutable.Queue[Frame]()
+
+  override def read(): Future[Frame] = {
+    if (q.nonEmpty) Future.value(q.dequeue())
+    else underlying.read().map(f).map { fs =>
+      q.enqueue(fs:_*)
+      q.dequeue()
+    }
+  }
+
+  override def toString: String = s"StreamProxy($underlying, flatMap=$f)"
+}
+
+//class StreamFold[T](underlying: Stream, f: (T, Try[Frame]) => T) extends StreamProxy(underlying) {
+//  override def read(): Future[Frame] = {
+//    if (q.nonEmpty) Future.value(q.dequeue())
+//    else underlying.read().map(f).map { fs =>
+//      q.enqueue(fs:_*)
+//      q.dequeue()
+//    }
+//  }
+//
+//  override def toString: String = s"StreamProxy($underlying, fold=$f)"
+//}
