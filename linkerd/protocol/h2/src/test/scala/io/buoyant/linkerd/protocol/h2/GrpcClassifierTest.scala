8b0bd9da8c40a152ece6a5f38d9eb0295091d590
Eliza Weisman <eliza@buoyant.io>
2017-08-02 17:24:13 -0500

    Add tests for parsing classifiers from YAML

--- linkerd/protocol/h2/src/test/scala/io/buoyant/linkerd/protocol/h2/GrpcClassifierTest.scala
+++ linkerd/protocol/h2/src/test/scala/io/buoyant/linkerd/protocol/h2/GrpcClassifierTest.scala
@@ -1,11 +1,16 @@
-package io.buoyant.linkerd.protocol.h2
+package io.buoyant.linkerd.protocol.h2.grpc
 
 import com.twitter.finagle.buoyant.h2.service.H2ReqRepFrame
 import com.twitter.finagle.buoyant.h2.{Headers, Request, Response, Stream => FStream}
 import com.twitter.finagle.service.ResponseClass
+import com.twitter.finagle.util.LoadService
 import com.twitter.util.Return
+import io.buoyant.config.Parser
 import io.buoyant.linkerd.protocol.h2.grpc.GrpcClassifiers.{AlwaysRetryable, NeverRetryable}
 import io.buoyant.grpc.runtime.GrpcStatus
+import io.buoyant.linkerd.RouterConfig
+import io.buoyant.linkerd.protocol.h2.H2ClassifierInitializer
+import io.buoyant.linkerd.protocol.{H2DefaultSvc, H2Initializer}
 import org.scalacheck.Arbitrary
 import org.scalatest.FunSuite
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
@@ -34,6 +39,7 @@ class GrpcClassifierTest extends FunSuite with GeneratorDrivenPropertyChecks {
       }
     }
   }
+
   test("NeverRetryable classifies no errors as retryable") {
     forAll("status") { status: GrpcStatus =>
       val trailers = status.toTrailers
@@ -50,6 +56,60 @@ class GrpcClassifierTest extends FunSuite with GeneratorDrivenPropertyChecks {
       } else {
         assert(NeverRetryable.streamClassifier(reqrep) == ResponseClass.Success)
       }
+    }
+  }
+
+
+  for { init <- Seq(
+          AlwaysRetryableInitializer,
+          NeverRetryableInitializer,
+          DefaultInitializer)
+        kind = init.configId }  {
+
+    test(s"loads $kind") {
+      assert(LoadService[H2ClassifierInitializer]().exists(_.configId == kind))
+    }
+
+    test(s"parse router with $kind") {
+      val yaml =
+        s"""|protocol: h2
+            |service:
+            |  responseClassifier:
+            |    kind: $kind
+            |servers:
+            |- port: 0
+            |""".stripMargin
+      val mapper = Parser.objectMapper(yaml, Iterable(Seq(H2Initializer), Seq(init)))
+      val router = mapper.readValue[RouterConfig](yaml)
+      assert(router.service.get.asInstanceOf[H2DefaultSvc]._h2Classifier.isDefined)
+      assertThrows[UnsupportedOperationException] {
+        router.service.get.asInstanceOf[H2DefaultSvc].responseClassifierConfig
+      }
+    }
+  }
+
+  test(s"loads io.l5d.h2.grpc.retryableStatusCodes") {
+    assert(LoadService[H2ClassifierInitializer]().exists(_.configId == "io.l5d.h2.grpc.retryableStatusCodes"))
+  }
+
+  test(s"parse router with io.l5d.h2.grpc.retryableStatusCodes") {
+    val yaml =
+      s"""|protocol: h2
+          |service:
+          |  responseClassifier:
+          |    kind: io.l5d.h2.grpc.retryableStatusCodes
+          |    retryableStatusCodes:
+          |    - 1
+          |    - 2
+          |    - 3
+          |servers:
+          |- port: 0
+          |""".stripMargin
+    val mapper = Parser.objectMapper(yaml, Iterable(Seq(H2Initializer), Seq(RetryableStatusCodesInitializer)))
+    val router = mapper.readValue[RouterConfig](yaml)
+    assert(router.service.get.asInstanceOf[H2DefaultSvc]._h2Classifier.isDefined)
+    assertThrows[UnsupportedOperationException] {
+      router.service.get.asInstanceOf[H2DefaultSvc].responseClassifierConfig
     }
   }
 }
