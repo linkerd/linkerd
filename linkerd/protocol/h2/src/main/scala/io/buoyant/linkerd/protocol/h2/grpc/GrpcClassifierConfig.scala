5550e6d68501e79abc10dbd95d72d794701dded9
Eliza Weisman <eliza@buoyant.io>
2017-08-03 15:55:45 -0500

    More review feedback

--- linkerd/protocol/h2/src/main/scala/io/buoyant/linkerd/protocol/h2/grpc/GrpcClassifierConfig.scala
+++ linkerd/protocol/h2/src/main/scala/io/buoyant/linkerd/protocol/h2/grpc/GrpcClassifierConfig.scala
@@ -2,32 +2,11 @@ package io.buoyant.linkerd.protocol.h2.grpc
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.twitter.finagle.buoyant.h2.service.H2Classifier
 import io.buoyant.linkerd.protocol.h2._
 
-@JsonSubTypes(Array(
-  new Type(
-    value = classOf[NeverRetryableConfig],
-    name = "io.l5d.h2.grpc.neverRetryable"
-  ),
-  new Type(
-    value = classOf[AlwaysRetryableConfig],
-    name = "io.l5d.h2.grpc.alwaysRetryable"
-  ),
-  new Type(
-    value = classOf[DefaultConfig],
-    name = "io.l5d.h2.grpc.default"
-  ),
-  new Type(
-    value = classOf[RetryableStatusCodesConfig],
-    name = "io.l5d.h2.grpc.retryableStatusCodes"
-  )
-))
-abstract class GrpcClassifierConfig extends H2ClassifierConfig {
-  override def mk: GrpcClassifier
-}
-
-class NeverRetryableConfig extends GrpcClassifierConfig {
-  override def mk: GrpcClassifier = GrpcClassifiers.NeverRetryable
+class NeverRetryableConfig extends H2ClassifierConfig {
+  override def mk: H2Classifier = GrpcClassifiers.NeverRetryable
 }
 
 class NeverRetryableInitializer extends H2ClassifierInitializer {
@@ -37,8 +16,8 @@ class NeverRetryableInitializer extends H2ClassifierInitializer {
 
 object NeverRetryableInitializer extends NeverRetryableInitializer
 
-class AlwaysRetryableConfig extends GrpcClassifierConfig {
-  override def mk: GrpcClassifier = GrpcClassifiers.NeverRetryable
+class AlwaysRetryableConfig extends H2ClassifierConfig {
+  override def mk: H2Classifier = GrpcClassifiers.NeverRetryable
 }
 
 class AlwaysRetryableInitializer extends H2ClassifierInitializer {
@@ -48,8 +27,8 @@ class AlwaysRetryableInitializer extends H2ClassifierInitializer {
 
 object AlwaysRetryableInitializer extends AlwaysRetryableInitializer
 
-class DefaultConfig extends GrpcClassifierConfig {
-  override def mk: GrpcClassifier = GrpcClassifiers.Default
+class DefaultConfig extends H2ClassifierConfig {
+  override def mk: H2Classifier = GrpcClassifiers.Default
 }
 
 class DefaultInitializer extends H2ClassifierInitializer {
@@ -60,8 +39,8 @@ class DefaultInitializer extends H2ClassifierInitializer {
 object DefaultInitializer extends DefaultInitializer
 
 // TODO: support parsing the status codes by name rather than by number?
-class RetryableStatusCodesConfig(val retryableStatusCodes: Set[Int]) extends GrpcClassifierConfig {
-  override def mk: GrpcClassifier = new GrpcClassifiers.RetryableStatusCodes(retryableStatusCodes)
+class RetryableStatusCodesConfig(val retryableStatusCodes: Set[Int]) extends H2ClassifierConfig {
+  override def mk: H2Classifier = new GrpcClassifiers.RetryableStatusCodes(retryableStatusCodes)
 }
 
 class RetryableStatusCodesInitializer extends H2ClassifierInitializer {
