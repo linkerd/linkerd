fb60a9ef7cc0566a088c6fb74e959e5cf38f96ba
Eliza Weisman <eliza@buoyant.io>
2017-08-17 12:05:32 -0500

    Skip logging on empty iterables

--- k8s/src/main/scala/io/buoyant/k8s/EventLogger.scala
+++ k8s/src/main/scala/io/buoyant/k8s/EventLogger.scala
@@ -2,32 +2,36 @@ package io.buoyant.k8s
 
 private[k8s] case class EventLogger(nsName: String, serviceName: String) {
   import EventLogger._
-  def addition[A: Loggable](additions: Iterable[A]): Unit = {
-    log.debug(
-      "k8s ns %s service %s added %ss",
-      nsName, serviceName,
-      implicitly[Loggable[A]].descriptor
-    )
-    additions.foreach(implicitly[Loggable[A]].logAddition(nsName, serviceName))
-  }
+  def addition[A: Loggable](additions: Iterable[A]): Unit =
+    if (additions.nonEmpty) {
+      log.debug(
+        "k8s ns %s service %s added %ss",
+        nsName, serviceName,
+        implicitly[Loggable[A]].descriptor
+      )
+      additions.foreach(implicitly[Loggable[A]].logAddition(nsName, serviceName))
+    }
 
-  def deletion[A: Loggable](deletions: Iterable[A]): Unit = {
-    log.debug(
-      "k8s ns %s service %s deleted %ss",
-      nsName, serviceName,
-      implicitly[Loggable[A]].descriptor
-    )
-    deletions.foreach(implicitly[Loggable[A]].logDeletion(nsName, serviceName))
-  }
+  def deletion[A: Loggable](deletions: Iterable[A]): Unit =
+    if (deletions.nonEmpty) {
+      log.debug(
+        "k8s ns %s service %s deleted %ss",
+        nsName, serviceName,
+        implicitly[Loggable[A]].descriptor
+      )
+      deletions.foreach(implicitly[Loggable[A]].logDeletion(nsName, serviceName))
+    }
 
-  def modification[A: Loggable](mods: Iterable[(A, A)]): Unit = {
-    log.debug(
-      "k8s ns %s service %s modified %ss",
-      nsName, serviceName,
-      implicitly[Loggable[A]].descriptor
-    )
-    mods.foreach(implicitly[Loggable[A]].logModification(nsName, serviceName).tupled)
-  }
+  def modification[A: Loggable](mods: Iterable[(A, A)]): Unit =
+    if (mods.nonEmpty) {
+      log.debug(
+        "k8s ns %s service %s modified %ss",
+        nsName, serviceName,
+        implicitly[Loggable[A]].descriptor
+      )
+      mods.foreach(implicitly[Loggable[A]].logModification(nsName, serviceName).tupled)
+    }
+
 }
 
 private[k8s] object EventLogger {
