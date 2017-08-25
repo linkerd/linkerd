9688dc4250880a0cd7bce3aa1160d4f75282d3f3
Eliza Weisman <eliza@buoyant.io>
2017-08-25 10:22:53 -0500

    Minor ServiceNamer improvements
    + Add comments
    + Remove dead code
    + Small code style tweaks

--- k8s/src/main/scala/io/buoyant/k8s/ServiceNamer.scala
+++ k8s/src/main/scala/io/buoyant/k8s/ServiceNamer.scala
@@ -24,25 +24,49 @@ class ServiceNamer(
   backoff: Stream[Duration] = Backoff.exponentialJittered(10.milliseconds, 10.seconds)
 )(implicit timer: Timer = DefaultTimer) extends Namer {
 
+  /**
+   * Internal representation of a Kubernetes service as a map of port names
+   * to `Address`es and a map of port numbers to port names.
+   * @param ports a map of `String`s representing port names to `Address`es.
+   * @param portMappings a map of port numbers to port names.
+   */
   private[this] case class Svc(
     ports: Map[String, Address],
     portMappings: Map[Int, String]
   ) {
+
+    /**
+     * Look up the port named `portName` and return the corresponding
+     * `Address`, if it exists.
+     * @param portName the port name to look up.
+     * @return `None` if no port named `portName` exists, `Some(Address)`
+     *        a port was found.
+     */
     def lookup(portName: String): Option[Address] =
       Try(portName.toInt).toOption match {
+        // if the port name could be parsed as an integer, look up a
+        // numbered port.
         case Some(portNumber) => lookupNumberedPort(portNumber)
+        // otherwise, look up a named port.
         case None => lookupNamedPort(portName)
       }
 
-    def lookupNamedPort(portName: String): Option[Address] =
+    private[this] def lookupNamedPort(portName: String): Option[Address] =
       ports.get(portName)
 
-    def lookupNumberedPort(portNumber: Int): Option[Address] =
+    private[this] def lookupNumberedPort(portNumber: Int): Option[Address] =
       for {
         portName <- portMappings.get(portNumber)
         address <- ports.get(portName)
       } yield address
 
+    /**
+     * Update this `Svc` with a [[v1.ServiceWatch]] watch event
+     * @param logEvent an event logger with which to log changes to this
+     *                 service.
+     * @param event the [[v1.ServiceWatch]] watch event that occurred.
+     * @return an updated `Svc` representing the watched service.
+     */
     def update(logEvent: EventLogging, event: v1.ServiceWatch): Svc =
       event match {
         case v1.ServiceAdded(service) =>
@@ -101,6 +125,15 @@ class ServiceNamer(
       }
       Svc(ports.toMap, portMap.toMap)
     }
+
+    /**
+     * Creates a new [[Svc]] from a services API response.
+     * @param response an `Option` containing either a [[v1.Service]] API
+     *                 response, or `None` if the service does not exist.
+     * @return either a [[Svc]] populated by the service API response, if
+     *         the service exists, or a [[Svc]] with empty ports and port
+     *         mappings maps if the service does not exist.
+     */
     def fromResponse(response: Option[v1.Service]): Svc =
       response.map(Svc(_)).getOrElse(Svc(Map.empty, Map.empty))
   }
@@ -108,6 +141,9 @@ class ServiceNamer(
   private[this] val PrefixLen = 3
   private[this] val variablePrefixLength = PrefixLen + labelName.size
 
+  // retrieves a memoized activity representing a watch for a
+  // (namespace name, service name, label selector), or establishes
+  // a new watch activity if one does not yet exist.
   private[this] val service
     : (String, String, Option[String]) => Activity[Svc] =
     untupled(Memoize[(String, String, Option[String]), Activity[Svc]] {
@@ -121,39 +157,37 @@ class ServiceNamer(
           ) { case (svc, event) => svc.update(eventLogger, event) }
     })
 
-  @inline private[this] def mkNameTree(
-    id: Path,
-    residual: Path
-  )(lookup: Option[Var[Set[Address]]]): NameTree[Name] = lookup match {
-    case Some(addresses) =>
-      val addrs = addresses.map {
-        Addr.Bound(_)
-      }
-      NameTree.Leaf(Name.Bound(addrs, idPrefix ++ id, residual))
-    case None => NameTree.Neg
-  }
-
   def lookup(path: Path): Activity[NameTree[Name]] =
     (path.take(variablePrefixLength), labelName) match {
       case (id@Path.Utf8(nsName, portName, serviceName), None) =>
+        // "unstable" activity - the activity will update when the existence of
+        // the address changes, *or* when the value of the address changes.
         val unstable = service(nsName.toLowerCase, serviceName.toLowerCase, None)
           .map { _.lookup(portName.toLowerCase) }
-        stabilize(unstable).map(addr => toNameTree(path, addr))
+        // stabilize the activity by converting it into an
+        // `Activity[Option[Var[Address]]]`, where the outer `Activity` will
+        // update if the `Option` changes, and the inner `Var` will update on
+        // changes to the value of the `Address`.
+        stabilize(unstable)
+        // convert the contents of the stable activity to a `NameTree`.
+          .map(toNameTree(path, _))
       case (id@Path.Utf8(nsName, portName, serviceName, labelValue), Some(label)) =>
         val labelSelector = Some(s"$label=$labelValue")
+        // as above, create an unstable activity, stabilize it, and then
+        // convert to a `NameTree`.
         val unstable = service(
           nsName.toLowerCase,
           serviceName.toLowerCase,
           labelSelector
         ).map { _.lookup(portName.toLowerCase) }
-        stabilize(unstable).map(addr => toNameTree(path, addr))
+        stabilize(unstable).map(toNameTree(path, _))
       case _ =>
         Activity.value(NameTree.Neg)
     }
 
   private[this] def toNameTree(
-    path: Path, svcAddress:
-    Option[Var[Address]]
+    path: Path,
+    svcAddress: Option[Var[Address]]
   ): NameTree[Name.Bound] = svcAddress match {
     case Some(address) =>
       val residual = path.drop(variablePrefixLength)
@@ -163,12 +197,6 @@ class ServiceNamer(
     case None =>
       NameTree.Neg
   }
-
-  private[this] def getPort(service: Service, portName: String): Option[Int] =
-    for {
-      spec <- service.spec
-      port <- spec.ports.find(_.name == portName)
-    } yield port.port
 
   private[ServiceNamer] case class EventLogger(ns: String, srv: String)
   extends EventLogging {
