e610319005c524883b7ec1e45118ab3e39ccf4dd
Eliza Weisman <eliza@buoyant.io>
2017-10-06 11:23:00 -0500

    Update head version

--- project/Base.scala
+++ project/Base.scala
@@ -32,7 +32,7 @@ object Base {
 class Base extends Build {
   import Base._
 
-  val headVersion = "1.2.1"
+  val headVersion = "1.3.0"
 
   object Git {
     def git(arg: String, args: String*) = Process("git" +: arg +: args)
