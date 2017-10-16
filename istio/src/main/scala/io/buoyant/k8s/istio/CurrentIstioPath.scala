07b5b9bd889740d17fe57b9f67fecf4bdb470d96
Eliza Weisman <eliza@buoyant.io>
2017-10-16 16:57:31 -0700

    Revert accidental changes to CurrentIstioPath

--- istio/src/main/scala/io/buoyant/k8s/istio/CurrentIstioPath.scala
+++ istio/src/main/scala/io/buoyant/k8s/istio/CurrentIstioPath.scala
@@ -20,19 +20,19 @@ object CurrentIstioPath {
   }
 
   /**
-   * Returns the target service requested based on the supplied [[Path]].
-   *
-   * The service is expected to be specified in the third last element in the path. For example,
-   * in the path:
-   *
-   * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
-   *
-   * The service is `reviews.default.svc.cluster.local`.
-   *
-   *
-   * @param istioPath
-   * @return
-   */
+    * Returns the target service requested based on the supplied [[Path]].
+    *
+    * The service is expected to be specified in the third last element in the path. For example,
+    * in the path:
+    *
+    * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
+    *
+    * The service is `reviews.default.svc.cluster.local`.
+    *
+    *
+    * @param istioPath
+    * @return
+    */
   def targetServiceIn(istioPath: Path): Option[String] = {
     val pathServiceIndex = istioPath.elems.size - 3
     if (pathServiceIndex > 0) {
@@ -48,19 +48,19 @@ object CurrentIstioPath {
   }
 
   /**
-   *
-   * Returns the target version of the app requested based on the supplied [[Path]].
-   *
-   * The version of the app is expected to be specified in the second last element in the path. For
-   * example, in the path:
-   *
-   * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
-   *
-   * The version is contained in the segment `az:us-west::env:prod::version:v1`, and its value is `v1`.
-   *
-   * @param istioPath
-   * @return
-   */
+    *
+    * Returns the target version of the app requested based on the supplied [[Path]].
+    *
+    * The version of the app is expected to be specified in the second last element in the path. For
+    * example, in the path:
+    *
+    * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
+    *
+    * The version is contained in the segment `az:us-west::env:prod::version:v1`, and its value is `v1`.
+    *
+    * @param istioPath
+    * @return
+    */
   def targetVersionIn(istioPath: Path): Option[String] = {
     val pathLabelsIndex = istioPath.elems.size - 2
     if (pathLabelsIndex >= 0) {
@@ -79,28 +79,28 @@ object CurrentIstioPath {
   }
 
   /**
-   * Returns the app requested based on the supplied [[Path]].
-   *
-   * The app is expected to be the first segment of the service as returned by [[targetServiceIn()]].
-   * For example, in the path:
-   *
-   * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
-   *
-   * The service is `reviews.default.svc.cluster.local`, and the app is `reviews`.
-   *
-   * Note that this should really come from the "app" label of the target pod. in most cases this
-   * will be the same value, but eventually the correct solution should be to get this directly from
-   * the label or the uid.
-   *
-   * @param istioPath
-   * @return
-   */
+    * Returns the app requested based on the supplied [[Path]].
+    *
+    * The app is expected to be the first segment of the service as returned by [[targetServiceIn()]].
+    * For example, in the path:
+    *
+    * `%,io.l5d.k8s.daemonset,default,incoming,l5d,#,io.l5d.k8s.istio,reviews.default.svc.cluster.local,az:us-west::env:prod::version:v1,http`
+    *
+    * The service is `reviews.default.svc.cluster.local`, and the app is `reviews`.
+    *
+    * Note that this should really come from the "app" label of the target pod. in most cases this
+    * will be the same value, but eventually the correct solution should be to get this directly from
+    * the label or the uid.
+    *
+    * @param istioPath
+    * @return
+    */
   def targetAppIn(istioPath: Path): Option[String] = targetServiceIn(istioPath)
     .flatMap(_.split('.').headOption)
 
   /**
-   * Resolves a valid Istio [[Path]], if any exists for the current [[DstBoundCtx]]
-   */
+    * Resolves a valid Istio [[Path]], if any exists for the current [[DstBoundCtx]]
+    */
   def apply(): Option[Path] = {
     Option(DstBoundCtx) match {
       case Some(p) => apply(p.current)
