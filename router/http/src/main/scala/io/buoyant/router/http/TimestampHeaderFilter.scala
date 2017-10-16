d8f0f0815536f360c1b561c6a9d6b4c5b0ec2531
Eliza Weisman <eliza@buoyant.io>
2017-10-16 13:10:11 -0700

    More work on TimestampHeaderFilter

--- router/http/src/main/scala/io/buoyant/router/http/TimestampHeaderFilter.scala
+++ router/http/src/main/scala/io/buoyant/router/http/TimestampHeaderFilter.scala
@@ -1,6 +1,6 @@
 package io.buoyant.router.http
 
-import com.twitter.finagle.{Filter, Service}
+import com.twitter.finagle._
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Time
 
@@ -11,6 +11,31 @@ import com.twitter.util.Time
  * `x-request-start` header, but might be useful for other purposes?
  */
 object TimestampHeaderFilter {
+
+  case class Header(header: String) {
+    def mk(): (Header, Stack.Param[Header]) = (this, Header.param)
+  }
+
+  object Header {
+    implicit val param = Stack.Param(Header("X-Request-Start"))
+  }
+
+  val role = Stack.Role("TimestampHeaderFilter")
+
+  object module extends Stack.Module1[
+    TimestampHeaderFilter.Header,
+    ServiceFactory[Request, Response]
+  ] {
+    override val role: Stack.Role = TimestampHeaderFilter.role
+    override val description = "Adds a timestamp header to requests"
+    override def make(
+      param: TimestampHeaderFilter.Header,
+      next: ServiceFactory[Request, Response]
+    ): ServiceFactory[Request, Response] = {
+      val Header(header) = param
+      filter(header).andThen(next)
+    }
+  }
 
  def filter(header: String): Filter[Request, Response, Request, Response] =
    (req: Request, svc: Service[Request, Response]) => {
