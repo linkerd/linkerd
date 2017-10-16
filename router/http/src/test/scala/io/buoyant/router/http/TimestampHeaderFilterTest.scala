06ee38aefc4a60c3cc3960bcf7849c5ecfe42ff0
Eliza Weisman <eliza@buoyant.io>
2017-10-16 13:09:48 -0700

    Add quick tests for timestamp header filter

---
+++ router/http/src/test/scala/io/buoyant/router/http/TimestampHeaderFilterTest.scala
@@ -0,0 +1,45 @@
+package io.buoyant.router.http
+
+import com.twitter.finagle.{Service, Stack}
+import com.twitter.finagle.http.{Method, Request, Response}
+import com.twitter.util.{Future, Time}
+import io.buoyant.test.FunSuite
+import org.scalatest.OptionValues
+
+class TimestampHeaderFilterTest extends FunSuite with OptionValues {
+
+  val Ok = Service.mk[Request, Response] { req =>
+    Future.value(Response())
+  }
+
+  val DefaultHeader = "X-Request-Start"
+
+  test("adds timestamp header") {
+    val svc = TimestampHeaderFilter.filter(DefaultHeader) andThen Ok
+
+    val req = Request()
+    req.method = Method.Head
+    req.uri = ""
+
+    val rsp = await(svc(req))
+    assert(rsp.headerMap.contains(DefaultHeader))
+
+  }
+
+  test("timestamp header contains request time") {
+    val svc = TimestampHeaderFilter.filter(DefaultHeader) andThen Ok
+
+    val req = Request()
+    req.method = Method.Head
+    req.uri = ""
+
+    val t_0 = Time.now
+    val rspF = Time.withTimeAt(t_0) { _ =>
+      svc(req)
+    }
+    val rsp = await(rspF)
+    assert(Time.now != t_0)
+    assert(rsp.headerMap.get(DefaultHeader).value == t_0.inMillis.toString)
+  }
+
+}
