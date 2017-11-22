43577cc2cd5d75b42bd281a8b92a78dca2f7f475
Eliza Weisman <eliza@buoyant.io>
2017-11-22 11:52:05 -0800

    only generate StreamClosed objects on close if they're needed

--- finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4DispatcherBase.scala
+++ finagle/h2/src/main/scala/com/twitter/finagle/buoyant/h2/netty4/Netty4DispatcherBase.scala
@@ -33,15 +33,14 @@ trait Netty4DispatcherBase[SendMsg <: Message, RecvMsg <: Message] {
     def reset(err: Reset): Unit = {}
   }
   private[this] case class StreamOpen(stream: Netty4StreamTransport[SendMsg, RecvMsg]) extends StreamTransport {
-    def toClosed: StreamClosed = StreamClosed(stream.streamId, this.stream.resetClient)
+    def toClosed: Option[StreamClosed] =
+      this.stream.resetClient.map { resetP => StreamClosed(stream.streamId, resetP ) }
     override def reset(err: Reset): Unit = stream.remoteReset(err)
   }
-  private[this] case class StreamClosed(id: Int, onReset: Option[Promise[Unit]]) extends StreamTransport {
+  private[this] case class StreamClosed(id: Int, onReset: Promise[Unit]) extends StreamTransport {
     override def reset(err: Reset): Unit = synchronized {
-      onReset.foreach { resetP =>
-        log.debug("[%s S:%d] propagating %s to client half of closed stream", prefix, id, err)
-        resetP.setException(err)
-      }
+      log.debug("[%s S:%d] propagating %s to client half of closed stream", prefix, id, err)
+      onReset.setException(err)
       streams.remove(id); ()
     }
   }
@@ -79,10 +78,19 @@ trait Netty4DispatcherBase[SendMsg <: Message, RecvMsg <: Message] {
       case Return(_) =>
         // Free and clear.
         addClosedId(id)
-        if (streams.replace(id, open, open.toClosed)) {
-          log.debug("[%s S:%d] open stream closed", prefix, id)
-        } else {
-          streams.remove(id)
+        open.toClosed match {
+          case Some(closed) if streams.replace(id, open, closed) =>
+            // the open stream is a server dispatcher stream whose corresponding
+            // client stream may not be closed yet. we need to continue tracking
+            // it in the event of a reset.
+            log.debug("[%s S:%d] open stream replaced by StreamClosed", prefix, id)
+          case _ =>
+            // either the stream is a client dispatcher stream, or the stream
+            // transitioned to closed from a state other than open. just
+            // remove it.
+            log.debug("[%s S:%d] open stream removed", prefix, id)
+            streams.remove(id)
+
         }
         log.debug("[%s S:%d] stream closed", prefix, id)
 
