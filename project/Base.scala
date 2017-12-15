b49a8441edc45c18c5988b9eb5792a429ceff605
Eliza Weisman <eliza@buoyant.io>
2017-12-15 10:01:30 -0800

    Advance head version
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- project/Base.scala
+++ project/Base.scala
@@ -32,7 +32,7 @@ object Base {
 class Base extends Build {
   import Base._
 
-  val headVersion = "1.3.3"
+  val headVersion = "1.3.4"
 
   object Git {
     def git(arg: String, args: String*) = Process("git" +: arg +: args)
