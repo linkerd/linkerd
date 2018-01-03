8e5ecc191e671f3f5499ff26e94011dbda6c658b
Alex Leong <alex@buoyant.io>
2018-01-03 15:59:45 -0800

    Clean up output format
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- linkerd/admin/src/main/scala/com/twitter/finagle/client/buoyant/ClientStateHandler.scala
+++ linkerd/admin/src/main/scala/com/twitter/finagle/client/buoyant/ClientStateHandler.scala
@@ -1,41 +1,41 @@
 package com.twitter.finagle.client.buoyant
 
-import com.twitter.finagle.{Addr, Address, Service}
+import com.twitter.finagle.{Addr, Address, Path, Service}
 import com.twitter.finagle.client.ClientRegistry
 import com.twitter.finagle.http.{MediaType, Request, Response}
 import com.twitter.finagle.loadbalancer.LoadBalancerFactory
-import com.twitter.util.Future
+import com.twitter.util.{Future, Try}
 import io.buoyant.config.Parser
 
 class ClientStateHandler extends Service[Request, Response] {
 
+  private case class ClientState(state: String, addresses: Seq[String] = Nil)
+
   private[this] val mapper = Parser.jsonObjectMapper(Nil)
 
   override def apply(request: Request): Future[Response] = {
-    val entries = ClientRegistry.registrants.map { entry =>
-      val LoadBalancerFactory.Dest(va) = entry.params[LoadBalancerFactory.Dest]
-      va.changes.toFuture().map(entry.addr -> _)
-    }
-    Future.collect(entries.toSeq).map { entries =>
-      val clientState = entries.map {
-        case (client, addr) =>
-          val state = addr match {
-            case Addr.Bound(addresses, meta) =>
-              addresses.map {
-                case Address.Inet(isa, _) => isa.getHostString
-                case a => a.toString
-              }.toSeq
-            case Addr.Failed(why) => s"Failed: ${why.getMessage}"
-            case a@Addr.Pending => a.toString
-            case a@Addr.Neg => a.toString
-          }
-          client -> state
+    val entries = ClientRegistry.registrants
+      .filter { entry =>
+        // only display clients with a Path name
+        Try(Path.read(entry.addr)).isReturn
+      }.map { entry =>
+        val LoadBalancerFactory.Dest(va) = entry.params[LoadBalancerFactory.Dest]
+        val clientState = va.sample() match {
+          case Addr.Bound(addresses, _) =>
+            ClientState("bound", addresses.map {
+              case Address.Inet(isa, _) => s"${isa.getHostString}:${isa.getPort}"
+              case a => a.toString
+            }.toSeq)
+          case Addr.Failed(why) => ClientState(s"Failed: ${why.getMessage}")
+          case a@Addr.Pending => ClientState(a.toString)
+          case a@Addr.Neg => ClientState(a.toString)
+        }
+        entry.addr -> clientState
       }.toMap
 
-      val response = Response()
-      response.contentType = MediaType.Json + ";charset=UTF-8"
-      response.contentString = mapper.writeValueAsString(clientState)
-      response
-    }
+    val response = Response()
+    response.contentType = MediaType.Json + ";charset=UTF-8"
+    response.contentString = mapper.writeValueAsString(entries)
+    Future.value(response)
   }
 }
