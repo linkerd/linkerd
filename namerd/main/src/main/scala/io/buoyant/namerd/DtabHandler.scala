41efb96ae541e311b730592e69a6e3688525d2c5
Alex Leong <alex@buoyant.io>
2018-01-11 11:09:24 -0800

    Render an error instead of a blank page for failures on namerd dtab page (#1770)
    * Render an error instead of a blank page for failures on namerd dtab page
    
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- namerd/main/src/main/scala/io/buoyant/namerd/DtabHandler.scala
+++ namerd/main/src/main/scala/io/buoyant/namerd/DtabHandler.scala
@@ -2,7 +2,7 @@ package io.buoyant.namerd
 
 import com.twitter.finagle.http.{MediaType, Request, Response, Status}
 import com.twitter.finagle.{Dtab, Service}
-import com.twitter.util.Future
+import com.twitter.util.{Future, Return, Throw}
 import io.buoyant.admin.names.DelegateApiHandler
 import io.buoyant.namer.RichActivity
 
@@ -18,20 +18,55 @@ class DtabHandler(
 
   override def apply(req: Request): Future[Response] = req.path match {
     case rexp(namespace) =>
-      getDtab(namespace).map {
-        case Some(dtab) =>
-          val response = Response()
-          response.contentType = MediaType.Html + ";charset=UTF-8"
-          response.contentString = render(namespace, dtab.dtab)
-          response
-        case None => Response(Status.NotFound)
+      getDtab(namespace).transform {
+        case Return(None) => Future.value(Response(Status.NotFound))
+        case Return(Some(dtab)) =>
+          renderDtab(namespace, dtab.dtab)
+        case Throw(e) =>
+          renderError(e)
       }
     case _ =>
       Future.value(Response(Status.NotFound))
   }
 
-  def render(name: String, dtab: Dtab) =
-    s"""
+  def renderDtab(name: String, dtab: Dtab): Future[Response] = {
+    val body = s"""
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-6">
+                <h2 class="router-label-title">Namespace "$name"</h2>
+              </div>
+            </div>
+            <div class="delegator">
+            </div>
+          </div>
+
+          <script id="data" type="application/json">{"namespace": "$name", "dtab": ${
+      DelegateApiHandler
+        .Codec.writeStr(dtab)
+    }}</script>
+          <script data-main="../files/js/main-namerd" src="../files/js/lib/require.js"></script>
+      """
+    render(body)
+  }
+
+  def renderError(e: Throwable): Future[Response] = {
+    val body = s"""
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-6">
+                <h2 class="router-label-title">Dtab Error</h2>
+                <p>An error was encountered when fetching or parsing the dtab:</p>
+                <pre>$e</pre>
+              </div>
+            </div>
+          </div>
+    """
+    render(body)
+  }
+
+  def render(body: String): Future[Response] = {
+    val content = s"""
       <!doctype html>
       <html>
         <head>
@@ -43,21 +78,15 @@ class DtabHandler(
           <link type="text/css" href="../files/css/delegator.css" rel="stylesheet"/>
         </head>
         <body>
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-lg-6">
-                <h2 class="router-label-title">Namespace "$name"</h2>
-              </div>
-            </div>
-            <div class="delegator">
-            </div>
-          </div>
-
-          <script id="data" type="application/json">{"namespace": "$name", "dtab": ${DelegateApiHandler.Codec.writeStr(dtab)}}</script>
-          <script data-main="../files/js/main-namerd" src="../files/js/lib/require.js"></script>
+        $body
         </body>
       </html>
     """
+    val response = Response()
+    response.contentType = MediaType.Html + ";charset=UTF-8"
+    response.contentString = content
+    Future.value(response)
+  }
 }
 
 object DtabHandler {
