e131b9c2326edebb6c81580ebab68d96de0e7c38
Nic Cope <negz@planet.com>
2018-01-30 03:06:26 -0800

    Clarify that ingress cache logs are scoped per ingress resource
    The previous rules looked a lot like they were saying "no rule matches
    host+path anywhere", when in fact they were saying "no rule matches
    host+path in the ingress resource I am currently processing". With
    multiple ingresses this leads to a lot of logs like:
    
    no rule found matching blah /foo
    no rule found matching blah /foo
    k8s found rule matching blah /foo
    no rule found maching blah /foo
    
    This change attempts to make it obvious that the log line is scoped to
    a single resource, and sets the "no rule found" log line to debug, since
    it should be pretty common to not find matching rules in a cluster with
    a lot of ingress resources.
    
    Signed-off-by: Nic Cope <negz@planet.com>

--- k8s/src/main/scala/io/buoyant/k8s/IngressCache.scala
+++ k8s/src/main/scala/io/buoyant/k8s/IngressCache.scala
@@ -15,8 +15,8 @@ case class IngressSpec(
   def getMatchingPath(hostHeader: Option[String], requestPath: String): Option[IngressPath] = {
     val matchingPath = rules.find(_.matches(hostHeader, requestPath))
     matchingPath match {
-      case Some(path) => log.info("k8s found rule matching %s %s: %s", hostHeader.getOrElse(""), requestPath, path)
-      case None => log.info("no ingress rule found for request %s %s", hostHeader.getOrElse(""), requestPath)
+      case Some(path) => log.info("ingress %s.%s: found rule matching %s %s: %s", name.getOrElse("unknown"), namespace.getOrElse("default"), hostHeader.getOrElse(""), requestPath, path)
+      case None => log.debug("ingress %s.%s: no rules found matching %s %s", name.getOrElse("unknown"), namespace.getOrElse("default"), hostHeader.getOrElse(""), requestPath)
     }
     matchingPath
   }
