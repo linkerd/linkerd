linkerd/linkerd

commit 188ec602726c72aa7d665816394b96510a5937f1

Handle K8s Delete Events Correctly (#1622)
When Linkerd receives a DELETE event from the Kubernetes API, it treats the
contents of the event as the new state.  However, the semantics of the API are
that the contents of the event are the previous state and the resource should
be deleted entirely.  This causes Linkerd to get stuck in a bad state when
watching a resource as it is being deleted.

Treat DELETE events as total deletions and ignore the contents of the event.

Manually tested on Minikube.

k8s/src/main/scala/io/buoyant/k8s/IngressCache.scala
@@ -90,10 +90,8 @@ class IngressCache(namespace: Option[String], apiClient: Service[Request, Respon
             mkIngress(m)
               .map { item => ingresses.filterNot(isNameEqual(_, item)) :+ item }
               .getOrElse(ingresses)
-          case v1beta1.IngressDeleted(d) =>
-            mkIngress(d)
-              .map { item => ingresses.filterNot(isNameEqual(_, item)) }
-              .getOrElse(ingresses)
+          case v1beta1.IngressDeleted(_) =>
+            Seq.empty
           case v1beta1.IngressError(e) =>
             log.error("k8s watch error: %s", e)
             ingresses

k8s/src/main/scala/io/buoyant/k8s/EndpointsNamer.scala
@@ -158,10 +158,9 @@ abstract class EndpointsNamer(
                 logEvent.deletion(oldMap -- newMap.keys)
                 logEvent.modification(oldMap, newMap)
                 oldMap ++ newMap
-              case (oldMap, v1.ServiceDeleted(service)) =>
-                val newMap = service.portMappings
-                logEvent.deletion(oldMap -- newMap.keys)
-                newMap
+              case (oldMap, v1.ServiceDeleted(_)) =>
+                logEvent.deletion(oldMap)
+                Map.empty
               case (oldMap, v1.ServiceError(error)) =>
                 log.warning(
                   "k8s ns %s service %s watch error %s",
@@ -306,16 +305,12 @@ object EndpointsNamer {
           logEvent.modification(ports, newPorts)
           this.copy(endpoints = newEndpoints, ports = newPorts)
 
-        case v1.EndpointsDeleted(update) =>
-          val (newEndpoints, newPorts: Map[String, Int]) =
-            update.subsets.toEndpointsAndPorts
-          val deletedEndpoints = endpoints.diff(newEndpoints)
-          val deletedPorts = ports.filterKeys(!newPorts.contains(_))
-          logEvent.deletion(deletedEndpoints)
-          logEvent.deletion(deletedPorts)
+        case v1.EndpointsDeleted(_) =>
+          logEvent.deletion(endpoints)
+          logEvent.deletion(ports)
           this.copy(
-            endpoints = endpoints -- deletedEndpoints,
-            ports = ports -- deletedPorts.keys
+            endpoints = Set.empty,
+            ports = Map.empty
           )
         case v1.EndpointsError(error) =>
           log.warning(

k8s/src/main/scala/io/buoyant/k8s/ServiceNamer.scala
@@ -83,13 +83,12 @@ class ServiceNamer(
           logEvent.modification(portMappings, svc.portMappings)
           logEvent.modification(ports, svc.ports)
           svc
-        case v1.ServiceDeleted(deleted) =>
-          val Svc(deletedPorts, deletedMappings) = Svc(deleted)
-          logEvent.deletion(deletedPorts)
-          logEvent.deletion(deletedMappings)
+        case v1.ServiceDeleted(_) =>
+          logEvent.deletion(ports)
+          logEvent.deletion(portMappings)
           this.copy(
-            ports = ports -- deletedPorts.keys,
-            portMappings = portMappings -- deletedMappings.keys
+            ports = Map.empty,
+            portMappings = Map.empty
           )
         case v1.ServiceError(error) =>
           log.warning(

commit e164e03192c71f94a6e46ecc48cdcd3b036c6b6a

Don't reset the ingress cache when ingresses are deleted (#1817)
We've observed on our Kubernetes clusters that linkerd is unable to route ingress traffic after any one ingress resource is deleted, even when there are plenty of valid ingress resources remaining. I'm pretty sure this bug was introduced in https://github.com/linkerd/linkerd/pull/1622/files#diff-7e056dcc7844eb0f58a507bf1b29cff2L93

When we see an ingress being deleted we want to delete that ingress from our ingress cache, not set the ingress cache to an empty sequence. 9c3c619 adds a test intended to demonstrate the bug, which is fixed in bfed573.

Signed-off-by: Nic Cope <negz@planet.com>

k8s/src/main/scala/io/buoyant/k8s/IngressCache.scala
@@ -96,8 +96,10 @@ class IngressCache(
             mkIngress(m)
               .map { item => ingresses.filterNot(isNameEqual(_, item)) :+ item }
               .getOrElse(ingresses)
-          case v1beta1.IngressDeleted(_) =>
-            Seq.empty
+          case v1beta1.IngressDeleted(d) =>
+            mkIngress(d)
+              .map { item => ingresses.filterNot(isNameEqual(_, item)) }
+              .getOrElse(ingresses)
           case v1beta1.IngressError(e) =>
             log.error("k8s watch error: %s", e)
             ingresses

k8s/src/test/scala/io/buoyant/k8s/IngressCacheTest.scala
@@ -3,11 +3,13 @@ package io.buoyant.k8s
 import com.twitter.finagle.Service
 import com.twitter.io.Buf
 import com.twitter.util.Future
+import com.twitter.util.Promise
 import io.buoyant.test.Awaits
 import org.scalatest.FunSuite
+import org.scalatest.OptionValues
 import com.twitter.finagle.http.{Request, Response}
 
-class IngressCacheTest extends FunSuite with Awaits {
+class IngressCacheTest extends FunSuite with Awaits with OptionValues {
   val host = Some("myhost")
   val ns = Some("ns")
 
@@ -243,6 +245,55 @@ class IngressCacheTest extends FunSuite with Awaits {
   ]
 }"""
 
+  val ingressDeletedFromMoreThanOneLinkerdIngresses =
+    """
+{
+  "type": "DELETED",
+  "object": {
+    "apiVersion": "extensions/v1beta1",
+    "kind": "Ingress",
+    "metadata": {
+      "name": "linkerd-ingress",
+      "namespace": "default",
+      "selfLink": "/apis/extensions/v1beta1/namespaces/default/ingresses/istio-ingress",
+      "uid": "07900e6b-813d-11e7-b89b-080027a996b8",
+      "resourceVersion": "58098",
+      "generation": 1,
+      "creationTimestamp": "2017-08-14T22:07:57Z",
+      "annotations": {
+        "kubernetes.io/ingress.class": "linkerd"
+      }
+    },
+    "spec": {
+      "rules": [
+        {
+          "http": {
+            "paths": [
+              {
+                "path": "/linkerd-1",
+                "backend": {
+                  "serviceName": "echo1",
+                  "servicePort": 6060
+                }
+              },
+              {
+                "path": "/shared",
+                "backend": {
+                  "serviceName": "shared1",
+                  "servicePort": 6061
+                }
+              }
+            ]
+          }
+        }
+      ]
+    },
+    "status": {
+      "loadBalancer": {}
+    }
+  }
+}"""
+
   val ingressResourceListWithOneIngress =
     """
 {
@@ -352,12 +403,19 @@ class IngressCacheTest extends FunSuite with Awaits {
   val annotationClass = "linkerd"
 
   def mkIngressApiServiceReturning(response: String) = Service.mk[Request, Response] {
+    mkWatchedIngressApiServiceReturning(response, "")
+  }
+
+  def mkWatchedIngressApiServiceReturning(response: String, watchResponse: String) = Service.mk[Request, Response] {
     case req if req.uri.contains("/apis/extensions/v1beta1/ingresses") =>
       val rsp = Response()
       rsp.content = Buf.Utf8(response)
       Future.value(rsp)
     case req if req.uri.contains("/apis/extensions/v1beta1/watch/ingresses") =>
-      Future.value(Response())
+      val rsp = Response()
+      rsp.setChunked(true)
+      rsp.writer.write(Buf.Utf8(watchResponse))
+      Future.value(rsp)
     case req =>
       fail(s"unexpected request for [${req.uri}]: $req")
   }
@@ -368,42 +426,55 @@ class IngressCacheTest extends FunSuite with Awaits {
     assert(await(cache.matchPath(host, "/istio-only")).isEmpty)
     assert(await(cache.matchPath(host, "/nginx-only")).isEmpty)
     assert(await(cache.matchPath(host, "/non-existing-path")).isEmpty)
-    assert(await(cache.matchPath(host, "/linkerd-only")).get.svc == "echo")
+    assert(await(cache.matchPath(host, "/linkerd-only")).value.svc == "echo")
   }
 
   test("when multiple ingress resources with desired annotation class, matches based on path of either") {
     val service = mkIngressApiServiceReturning(ingressResourceListWithMoreThanOneLinkerdIngresses)
     val cache = new IngressCache(None, service, annotationClass)
-    assert(await(cache.matchPath(host, "/linkerd-1")).get.svc == "echo1")
-    assert(await(cache.matchPath(host, "/linkerd-2")).get.svc == "echo2")
-    assert(Set("shared1", "shared2").contains(await(cache.matchPath(host, "/shared")).get.svc))
+    assert(await(cache.matchPath(host, "/linkerd-1")).value.svc == "echo1")
+    assert(await(cache.matchPath(host, "/linkerd-2")).value.svc == "echo2")
+    assert(Set("shared1", "shared2").contains(await(cache.matchPath(host, "/shared")).value.svc))
   }
 
   test("when only one ingress configured, adds it irrespective of annotations") {
     val service = mkIngressApiServiceReturning(ingressResourceListWithOneIngress)
     val cache = new IngressCache(None, service, annotationClass)
-    assert(await(cache.matchPath(host, "/some-path")).get.svc == "echo")
+    assert(await(cache.matchPath(host, "/some-path")).value.svc == "echo")
     assert(await(cache.matchPath(host, "/non-existing-path")).isEmpty)
   }
 
   test("ignores port in a request's hostname") {
     val service = mkIngressApiServiceReturning(ingressResourceListWithOneIngress)
     val cache = new IngressCache(None, service, annotationClass)
-    assert(await(cache.matchPath(Some("myhost:80"), "/some-path")).get.svc == "echo")
+    assert(await(cache.matchPath(Some("myhost:80"), "/some-path")).value.svc == "echo")
   }
 
   test("ingress caches honor default backends") {
     val service = mkIngressApiServiceReturning(ingressResourceListWithOneIngressWithFallback)
     val cache = new IngressCache(None, service, annotationClass)
-    assert(await(cache.matchPath(host, "/some-path")).get.svc == "echo")
-    assert(await(cache.matchPath(host, "/unknown-path")).get.svc == "fallback")
+    assert(await(cache.matchPath(host, "/some-path")).value.svc == "echo")
+    assert(await(cache.matchPath(host, "/unknown-path")).value.svc == "fallback")
   }
 
   test("ignoreDefaultBackends ingress caches ignore default backends") {
     val service = mkIngressApiServiceReturning(ingressResourceListWithOneIngressWithFallback)
     val cache = new IngressCache(None, service, annotationClass, true)
-    assert(await(cache.matchPath(host, "/some-path")).get.svc == "echo")
+    assert(await(cache.matchPath(host, "/some-path")).value.svc == "echo")
     assert(await(cache.matchPath(host, "/unknown-path")) == None)
+  }
+
+  test("deleting an ingress does not reset the ingress cache") {
+    val service = mkWatchedIngressApiServiceReturning(
+      ingressResourceListWithMoreThanOneLinkerdIngresses,
+      ingressDeletedFromMoreThanOneLinkerdIngresses
+    )
+    val cache = new IngressCache(None, service, annotationClass, true)
+    // The ingress that was deleted is gone.
+    assert(await(cache.matchPath(host, "/linkerd-1")) == None)
+
+    // Untouched ingresses remain.
+    assert(await(cache.matchPath(host, "/linkerd-2")).value.svc == "echo2")
   }
 
   test("on multiple path matches, return first match") {
@@ -413,14 +484,14 @@ class IngressCacheTest extends FunSuite with Awaits {
     )
     val spec = IngressSpec(Some("my-ingress"), ns, None, paths)
     val matchingPath = IngressCache.getMatchingPath(host, "/path", Seq(spec))
-    assert(matchingPath.get.svc == "primary-svc")
+    assert(matchingPath.value.svc == "primary-svc")
   }
 
   test("on multiple host matches, return first match") {
     val resource1 = IngressSpec(Some("polar-bear1"), ns, None, Seq(IngressPath(host, None, ns.get, "svc1", "80")))
     val resource2 = IngressSpec(Some("polar-bear2"), ns, None, Seq(IngressPath(host, None, ns.get, "svc2", "80")))
     val matchingPath = IngressCache.getMatchingPath(host, "/path", Seq(resource1, resource2))
-    assert(matchingPath.get.svc == "svc1")
+    assert(matchingPath.value.svc == "svc1")
   }
 
   test("don't return default backend before checking all resources for matches") {
@@ -432,7 +503,7 @@ class IngressCacheTest extends FunSuite with Awaits {
     )
     val resource2 = IngressSpec(Some("polar-bear2"), ns, None, Seq(IngressPath(host, None, ns.get, "svc2", "80")))
     val matchingPath = IngressCache.getMatchingPath(host, "/path", Seq(resource1, resource2))
-    assert(matchingPath.get.svc == "svc2")
+    assert(matchingPath.value.svc == "svc2")
   }
 
   test("on no host/path matches, return first default backend") {
@@ -444,7 +515,7 @@ class IngressCacheTest extends FunSuite with Awaits {
       Seq(IngressPath(host, None, ns.get, "svc2", "80"))
     )
     val matchingPath = IngressCache.getMatchingPath(Some("unknown host"), "/path", Seq(resource1, resource2))
-    assert(matchingPath.get.svc == "fallback")
+    assert(matchingPath.value.svc == "fallback")
   }
 
   test("no matches") {