c747101aee652bd55c86dd5975a97a046ac1ef4d
fantayeneh <fantayeneh@gmail.com>
2018-03-03 10:44:19 +0000

    Use system default timezone when logging
    Signed-off-by: fantayeneh <fantayeneh@gmail.com>

--- linkerd/protocol/http/src/test/scala/io/buoyant/linkerd/protocol/http/AccessLoggerTest.scala
+++ linkerd/protocol/http/src/test/scala/io/buoyant/linkerd/protocol/http/AccessLoggerTest.scala
@@ -1,15 +1,15 @@
 package io.buoyant.linkerd.protocol.http
 
+import java.util.{TimeZone, logging => javalog}
+
 import com.twitter.finagle.Service
 import com.twitter.finagle.http._
-import com.twitter.util.{Future, Promise, Time}
 import com.twitter.logging._
+import com.twitter.util.{Future, Promise, Time}
 import io.buoyant.test.Awaits
-import java.util.{logging => javalog}
 import org.scalatest.FunSuite
 
 class AccessLoggerTest extends FunSuite with Awaits {
-
   object StringLogger extends Logger("string", javalog.Logger.getAnonymousLogger()) {
     val handler = new StringHandler(new Formatter {
       override def format(record: javalog.LogRecord): String = formatText(record)
@@ -41,8 +41,20 @@ class AccessLoggerTest extends FunSuite with Awaits {
       req.contentType = "text/plain"
 
       val f = service(req)
-      assert(StringLogger.getLoggedLines() ==
-        """monkeys 0.0.0.0 - - [06/01/2016:21:21:26 +0000] "HEAD /foo?bar=bah HTTP/1.1" 402 304374 "-" "-"""")
+      assert(
+        StringLogger.getLoggedLines() ==
+          """monkeys 0.0.0.0 - - [06/01/2016:21:21:26 GMT] "HEAD /foo?bar=bah HTTP/1.1" 402 304374 "-" "-""""
+      )
+    }
+  }
+
+  override protected def withFixture(test: NoArgTest) = {
+    val currentTimezone = TimeZone.getDefault
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
+    try {
+      super.withFixture(test)
+    } finally {
+      TimeZone.setDefault(currentTimezone)
     }
   }
 }
