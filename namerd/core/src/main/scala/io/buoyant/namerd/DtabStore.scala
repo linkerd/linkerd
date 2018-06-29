f9003a8d041237fb95dd9033473a7bd4a589ab00
Dennis Adjei-Baah <dennis@buoyant.io>
2018-06-29 16:22:27 -0700

    check Dtab for null entries and fail Http POST request to update Dtab
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- namerd/core/src/main/scala/io/buoyant/namerd/DtabStore.scala
+++ namerd/core/src/main/scala/io/buoyant/namerd/DtabStore.scala
@@ -1,6 +1,6 @@
 package io.buoyant.namerd
 
-import com.twitter.finagle.Dtab
+import com.twitter.finagle.{Dentry, Dtab}
 import com.twitter.io.Buf
 import com.twitter.util.{Activity, Future}
 
@@ -54,6 +54,8 @@ object DtabStore {
 
   class DtabNamespaceInvalidException(ns: Ns)
     extends Exception(s"invalid dtab namespace $ns: namespace must contain only letter, number or '-' characters")
+  class DtabContainsInvalidDentriesException(dentries: IndexedSeq[Dentry])
+    extends Exception(s"The dtab contains invalid dentries: $dentries")
 
   object Forbidden extends Exception("You do not have sufficient permissions")
 
--- namerd/iface/control-http/src/main/scala/io/buoyant/namerd/iface/Json.scala
+++ namerd/iface/control-http/src/main/scala/io/buoyant/namerd/iface/Json.scala
@@ -7,6 +7,7 @@ import com.twitter.finagle.http._
 import com.twitter.finagle.{Status => _, _}
 import com.twitter.io.Buf
 import com.twitter.util._
+import io.buoyant.namerd.DtabStore.DtabContainsInvalidDentriesException
 import io.buoyant.namerd.{DtabCodec => DtabModule}
 
 object Json {
@@ -35,8 +36,13 @@ object DtabCodec {
   object JsonCodec extends DtabCodec {
     val mediaTypes = Set(MediaType.Json)
     def write(dtab: Dtab) = Json.write(dtab)
-    def read(buf: Buf) =
-      Json.read[IndexedSeq[Dentry]](buf).map(Dtab(_))
+    def read(buf: Buf) = {
+      Json.read[IndexedSeq[Dentry]](buf).map { dentries =>
+        if (dentries.contains(null)) {
+          throw new DtabContainsInvalidDentriesException(dentries)
+        } else Dtab(dentries)
+      }
+    }
   }
 
   object TextCodec extends DtabCodec {
