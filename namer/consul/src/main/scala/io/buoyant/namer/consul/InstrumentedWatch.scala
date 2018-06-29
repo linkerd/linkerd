37d69d5d3e26bdf07e9fe6ac56602672f4926e6c
Ivano Pagano <ivano.pagano@scalac.io>
2018-06-29 23:18:06 +0200

    Use the error message to serialize the PollState failure
    Signed-off-by: Ivano Pagano <ivano.pagano@scalac.io>

--- namer/consul/src/main/scala/io/buoyant/namer/consul/InstrumentedWatch.scala
+++ namer/consul/src/main/scala/io/buoyant/namer/consul/InstrumentedWatch.scala
@@ -26,7 +26,7 @@ class PollState[Req, Rep] {
         response = Some(r)
         error = None
       case Throw(e) =>
-        error = Some(e)
+        error = Some(e.getMessage)
         response = None
     }
   }
@@ -36,7 +36,7 @@ class PollState[Req, Rep] {
   protected var lastRequestAt: Option[String] = None
   protected var response: Option[Rep] = None
   protected var lastResponseAt: Option[String] = None
-  protected var error: Option[Throwable] = None
+  protected var error: Option[String] = None
 }
 
 private[consul] object InstrumentedApiCall {
