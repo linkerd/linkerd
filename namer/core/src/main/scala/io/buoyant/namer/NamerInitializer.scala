2168569077e1f6734b0253f43f41a83eb2dac23b
Alex Leong <alex@buoyant.io>
2018-07-03 15:47:22 -0700

    Remove import
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- namer/core/src/main/scala/io/buoyant/namer/NamerInitializer.scala
+++ namer/core/src/main/scala/io/buoyant/namer/NamerInitializer.scala
@@ -2,7 +2,6 @@ package io.buoyant.namer
 
 import com.fasterxml.jackson.annotation.{JsonIgnore, JsonProperty}
 import com.twitter.finagle._
-import io.buoyant.admin.Admin
 import io.buoyant.config.{ConfigInitializer, PolymorphicConfig}
 
 /**
