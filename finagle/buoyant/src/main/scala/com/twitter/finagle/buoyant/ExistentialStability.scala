8b3b272395f29b79118d295bb0377db17fa3929f
Alex Leong <alex@buoyant.io>
2018-11-05 17:27:04 -0800

    Review feedback and add test
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- finagle/buoyant/src/main/scala/com/twitter/finagle/buoyant/ExistentialStability.scala
+++ finagle/buoyant/src/main/scala/com/twitter/finagle/buoyant/ExistentialStability.scala
@@ -31,34 +31,30 @@ object ExistentialStability {
         }
       }
 
-      // Initial value for outer Var.
-      val init = unstable.sample().map { t =>
-        if (inner == null) {
-          inner = makeInner(t)
-        }
-        inner
-      }
-
-      @volatile var exists = init.isDefined
+      var exists = false
       val mu = new {}
 
-      Var.async[Option[Var[T]]](init) { update =>
+      Var.async[Option[Var[T]]](None) { update =>
         unstable.changes.respond { state =>
           mu.synchronized {
             state match {
-              case Some(_) if exists =>
-              // Do nothing.
-              case Some(t) if !exists =>
+              case Some(t) if inner == null =>
+                // T created.
+                inner = makeInner(t)
                 exists = true
-                if (inner == null) {
-                  inner = makeInner(t)
-                }
                 update() = Some(inner)
-              case None if !exists =>
-              // Do nothing.
+              case Some(_) if !exists =>
+                // T re-created.
+                update() = Some(inner)
+              case Some(_) if exists =>
+              // T modified.
+              // Existence has not changed so the outer Var should not be updated.
               case None if exists =>
+                // T deleted.
                 exists = false
                 update() = None
+              case None if !exists =>
+              // Spurious update in None state.  We can just ignore it.
             }
           }
         }
@@ -73,6 +69,13 @@ object ExistentialStability {
       // The inner Var is null until the first time unstable becomes a Some.
       @volatile var inner: Var[T] = null
 
+      def makeInner(init: T) = Var.async(init) { update =>
+        unstable.states.respond {
+          case Activity.Ok(Some(tt)) => update() = tt
+          case _ =>
+        }
+      }
+
       // This keeps track of the state of the outer Var:
       // None means the state was not Activity.Ok
       // Some(false) means the state was Activity.Ok(Some)
@@ -80,35 +83,37 @@ object ExistentialStability {
       //
       // We need to track these three states because we only want to update the outer Var when we
       // transition between states.
-      @volatile var exists: Option[Boolean] = None
+      var exists: Option[Boolean] = None
       val mu = new {}
 
       val outer = Var.async[Activity.State[Option[Var[T]]]](Activity.Pending) { update =>
         unstable.states.respond { state =>
           mu.synchronized {
             state match {
-              case Activity.Ok(Some(_)) if exists == Some(true) =>
-              // Do nothing.
-              case Activity.Ok(Some(t)) if exists != Some(true) =>
+              case Activity.Ok(Some(t)) if inner == null =>
+                // T created.
+                inner = makeInner(t)
                 exists = Some(true)
-                if (inner == null) {
-                  inner = Var.async(t) { update =>
-                    unstable.states.respond {
-                      case Activity.Ok(Some(tt)) => update() = tt
-                      case _ =>
-                    }
-                  }
-                }
                 update() = Activity.Ok(Some(inner))
-              case Activity.Ok(None) if exists == Some(false) =>
-              // Do nothing.
+              case Activity.Ok(Some(_)) if exists != Some(true) =>
+                // T re-created.
+                exists = Some(true)
+                update() = Activity.Ok(Some(inner))
+              case Activity.Ok(Some(_)) if exists == Some(true) =>
+              // T modified.
+              // Existence has not changed so the outer Var should not be updated.
               case Activity.Ok(None) if exists != Some(false) =>
+                // T deleted.
                 exists = Some(false)
                 update() = Activity.Ok(None)
+              case Activity.Ok(None) if exists == Some(false) =>
+              // Spurious update in None state.  We can just ignore it.
               case Activity.Pending =>
+                // Pending.
                 exists = None
                 update() = Activity.Pending
               case Activity.Failed(e) =>
+                // Failed.
                 exists = None
                 update() = Activity.Failed(e)
             }
