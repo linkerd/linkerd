linkerd/linkerd

commit ad5943322990d0cb14ab582a33b6738fcf0b1130

Add Prometheus exception counters (#2163)
### Problem
Prometheus metric names for exception counters have the exception in the name (e.g., rt:server:exn:io_netty_handler_codec_DecoderException). This makes it difficult to aggregate or display by exception because it requires you to know in advance all of the possible exceptions.

### Solution
Consolidate all exceptions into a single counter and put the exception name in a label (e.g. rt:server:exn{exception="io_netty_handler_codec_DecoderException"}). That way, the exception names could be grouped or regex matched and you can select all of them using the same time series.

### Validation
Two tests were added for client and server side.

Fixes #2118 

Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

telemetry/prometheus/src/main/scala/io/buoyant/telemetry/prometheus/PrometheusTelemeter.scala
@@ -38,9 +38,6 @@ class PrometheusTelemeter(metrics: MetricsTree, private[prometheus] val handlerP
   private[this] val metricNameDisallowedChars = "[^a-zA-Z0-9:]".r
   private[this] def escapeKey(key: String) = metricNameDisallowedChars.replaceAllIn(key, "_")
 
-  private[this] val labelKeyDisallowedChars = "[^a-zA-Z0-9_]".r
-  private[this] def escapeLabelKey(key: String) = labelKeyDisallowedChars.replaceAllIn(key, "_")
-
   // https://prometheus.io/docs/instrumenting/exposition_formats/#text-format-details
   private[this] val labelValDisallowedChars = """(\\|\"|\n)""".r
   private[this] def escapeLabelVal(key: String) = labelValDisallowedChars.replaceAllIn(key, """\\\\""")
@@ -64,6 +61,16 @@ class PrometheusTelemeter(metrics: MetricsTree, private[prometheus] val handlerP
   private[this] def labelExists(labels: Seq[(String, String)], name: String) =
     labels.toIterator.map(first).contains(name)
 
+  private[this] def addException(labels: Seq[(String, String)], name: String) = {
+    val index = labels.map(first).indexOf("exception")
+    if (index == -1) {
+      labels :+ ("exception" -> escapeLabelVal(name))
+    } else {
+      val nested = labels(index)._2
+      labels.updated(index, "exception" -> (nested ++ ":" ++ escapeLabelVal(name)))
+    }
+  }
+
   private[this] def writeMetrics(
     tree: MetricsTree,
     sb: StringBuilder,
@@ -75,14 +82,21 @@ class PrometheusTelemeter(metrics: MetricsTree, private[prometheus] val handlerP
     val (prefix1, labels1) = prefix0 match {
       case Seq("rt", router) if !labelExists(labels0, "rt") =>
         (Seq("rt"), labels0 :+ ("rt" -> escapeLabelVal(router)))
-      case Seq("rt", "service", path) if !labelExists(labels0, "service") =>
-        (Seq("rt", "service"), labels0 :+ ("service" -> escapeLabelVal(path)))
-      case Seq("rt", "client", id) if !labelExists(labels0, "client") =>
-        (Seq("rt", "client"), labels0 :+ ("client" -> escapeLabelVal(id)))
+
+      // Add label for stack { "service", "client", "server" }
+      case Seq("rt", stack, identifier) if !labelExists(labels0, stack) =>
+        (Seq("rt", stack), labels0 :+ (stack -> escapeLabelVal(identifier)))
+
+      // Handle client service case
       case Seq("rt", "client", "service", path) if !labelExists(labels0, "service") =>
         (Seq("rt", "client", "service"), labels0 :+ ("service" -> escapeLabelVal(path)))
-      case Seq("rt", "server", srv) if !labelExists(labels0, "server") =>
-        (Seq("rt", "server"), labels0 :+ ("server" -> escapeLabelVal(srv)))
+
+      // Add label for exception { "failures", "exn" }
+      case Seq("rt", stack, "failures", exception) =>
+        (Seq("rt", stack, "failures"), addException(labels0, exception))
+      case Seq("rt", stack, "exn", exception) =>
+        (Seq("rt", stack, "exceptions"), addException(labels0, exception))
+
       case _ => (prefix0, labels0)
     }
 

telemetry/prometheus/src/test/scala/io/buoyant/telemetry/prometheus/PrometheusTelemeterTest.scala
@@ -144,4 +144,68 @@ class PrometheusTelemeterTest extends FunSuite {
     val rsp = await(handler(Request(prometheusPath))).contentString
     assert(rsp == "linkerd_rt:server:requests{rt=\"incoming\", server=\"127.0.0.1/4141\"} 1\n")
   }
+
+  test("exception stats are labelled for service") {
+    val (stats, handler) = statsAndHandler
+    // Replicate stack trace for some failure bar:baz
+    stats.scope("rt", "incoming", "service", "/#/foo", "failures").counter("bar").incr()
+    stats.scope("rt", "incoming", "service", "/#/foo", "failures").counter("bar:baz").incr()
+
+    // Replicate stack trace for some failure bar:qux
+    stats.scope("rt", "incoming", "service", "/#/foo", "failures").counter("bar").incr()
+    stats.scope("rt", "incoming", "service", "/#/foo", "failures").counter("bar", "qux").incr()
+
+    // Replicate stack trace for some some exception thud
+    stats.scope("rt", "incoming", "service", "/#/foo", "exn").counter("thud").incr()
+
+    val rsp = await(handler(Request(prometheusPath))).contentString
+    assert(rsp == """linkerd_rt:service:failures{rt="incoming", service="/#/foo", exception="bar"} 2
+                    |linkerd_rt:service:failures{rt="incoming", service="/#/foo", exception="bar:qux"} 1
+                    |linkerd_rt:service:failures{rt="incoming", service="/#/foo", exception="bar:baz"} 1
+                    |linkerd_rt:service:exceptions{rt="incoming", service="/#/foo", exception="thud"} 1
+                    |""".stripMargin)
+  }
+
+  test("exception stats are labelled for client") {
+    val (stats, handler) = statsAndHandler
+    // Replicate stack trace for some failure bar:baz
+    stats.scope("rt", "incoming", "client", "/#/foo", "failures").counter("bar").incr()
+    stats.scope("rt", "incoming", "client", "/#/foo", "failures").counter("bar:baz").incr()
+
+    // Replicate stack trace for some failure bar:qux
+    stats.scope("rt", "incoming", "client", "/#/foo", "failures").counter("bar").incr()
+    stats.scope("rt", "incoming", "client", "/#/foo", "failures").counter("bar", "qux").incr()
+
+    // Replicate stack trace for some some exception thud
+    stats.scope("rt", "incoming", "client", "/#/foo", "exn").counter("thud").incr()
+
+    val rsp = await(handler(Request(prometheusPath))).contentString
+    assert(rsp == """linkerd_rt:client:failures{rt="incoming", client="/#/foo", exception="bar"} 2
+                    |linkerd_rt:client:failures{rt="incoming", client="/#/foo", exception="bar:qux"} 1
+                    |linkerd_rt:client:failures{rt="incoming", client="/#/foo", exception="bar:baz"} 1
+                    |linkerd_rt:client:exceptions{rt="incoming", client="/#/foo", exception="thud"} 1
+                    |""".stripMargin)
+  }
+
+  test("exception stats are labelled for server") {
+    val (stats, handler) = statsAndHandler
+    // Replicate stack trace for some failure foo:bar
+    stats.scope("rt", "incoming", "server", "127.0.0.1/4141", "failures").counter("foo").incr()
+    stats.scope("rt", "incoming", "server", "127.0.0.1/4141", "failures").counter("foo:bar").incr()
+
+    // Replicate stack trace for some failure foo:baz
+    stats.scope("rt", "incoming", "server", "127.0.0.1/4141", "failures").counter("foo").incr()
+    stats.scope("rt", "incoming", "server", "127.0.0.1/4141", "failures").counter("foo", "baz").incr()
+
+    // Replicate stack trace for some some exception qux
+    stats.scope("rt", "incoming", "server", "127.0.0.1/4141", "exn").counter("qux").incr()
+
+
+    val rsp = await(handler(Request(prometheusPath))).contentString
+    assert(rsp == """linkerd_rt:server:failures{rt="incoming", server="127.0.0.1/4141", exception="foo:bar"} 1
+                    |linkerd_rt:server:failures{rt="incoming", server="127.0.0.1/4141", exception="foo"} 2
+                    |linkerd_rt:server:failures{rt="incoming", server="127.0.0.1/4141", exception="foo:baz"} 1
+                    |linkerd_rt:server:exceptions{rt="incoming", server="127.0.0.1/4141", exception="qux"} 1
+                    |""".stripMargin)
+  }
 }

commit 828cf167c0d3ca33e0f727ab7724591624b6315c

Fix incorrect match introduced by #2163
Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

telemetry/prometheus/src/main/scala/io/buoyant/telemetry/prometheus/PrometheusTelemeter.scala
@@ -84,7 +84,8 @@ class PrometheusTelemeter(metrics: MetricsTree, private[prometheus] val handlerP
         (Seq("rt"), labels0 :+ ("rt" -> escapeLabelVal(router)))
 
       // Add label for stack { "service", "client", "server" }
-      case Seq("rt", stack, identifier) if !labelExists(labels0, stack) =>
+      case Seq("rt", stack, identifier) if Seq("service", "client", "server").contains(stack)
+        && !labelExists(labels0, stack) =>
         (Seq("rt", stack), labels0 :+ (stack -> escapeLabelVal(identifier)))
 
       // Handle client service case