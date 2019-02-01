79fa5554251aaddb5f7aec17f5f75c192f7b5926
Dennis Adjei-Baah <dennis@buoyant.io>
2019-01-31 17:33:33 -0800

    fix incorrect rendering of IP addresses in watch endpoints
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- interpreter/mesh/src/main/scala/io/buoyant/interpreter/mesh/BufSerializers.scala
+++ interpreter/mesh/src/main/scala/io/buoyant/interpreter/mesh/BufSerializers.scala
@@ -9,8 +9,9 @@ object BufSerializers {
   def path(path: Seq[Buf]): String = path.map(utf8).mkString("/", "/", "")
 
   def ipv4(addr: Buf): String = {
-    val bytes = Buf.ByteArray.coerce(addr)
-    s"${bytes.get(0)}.${bytes.get(1)}.${bytes.get(2)}.${bytes.get(3)}"
+    val bytes = Buf.ByteArray.Owned.extract(addr).map(_ & (-1 >>> 24))
+
+    s"${bytes(0)}.${bytes(1)}.${bytes(2)}.${bytes(3)}"
   }
 
 }
