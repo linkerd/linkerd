98f8dc5de5f284d21740de016f0091be93e9cf13
Dennis Adjei-Baah <dennis@buoyant.io>
2019-02-18 14:43:39 -0800

    update dependencies to finagle 19.1.0
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,14 +8,14 @@ object Deps {
 
   // process lifecycle
   val twitterServer =
-    ("com.twitter" %% "twitter-server" % "18.12.0")
+    ("com.twitter" %% "twitter-server" % "19.1.0")
       .exclude("com.twitter", "finagle-zipkin_2.12")
 
   def twitterUtil(mod: String) =
-    "com.twitter" %% s"util-$mod" % "18.12.0"
+    "com.twitter" %% s"util-$mod" % "19.1.0"
   // networking
   def finagle(mod: String) =
-    "com.twitter" %% s"finagle-$mod" % "18.12.0"
+    "com.twitter" %% s"finagle-$mod" % "19.1.0"
 
   def netty4(mod: String) =
     "io.netty" % s"netty-$mod" % "4.1.31.Final"
