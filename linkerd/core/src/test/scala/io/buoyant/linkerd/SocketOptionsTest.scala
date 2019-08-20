b52ba047d491cff2e38119f5c0897528b0b77f85
fantayeneh <fantayeneh@gmail.com>
2019-08-20 08:07:09 +0100

    Apply default socketoptions values when partial config is supplied.
    Signed-off-by: fantayeneh <fantayeneh@gmail.com>

--- linkerd/core/src/test/scala/io/buoyant/linkerd/SocketOptionsTest.scala
+++ linkerd/core/src/test/scala/io/buoyant/linkerd/SocketOptionsTest.scala
@@ -22,7 +22,29 @@ class SocketOptionsTest extends FunSuite {
       """.stripMargin
     val linker = Linker.load(config, Initializers(protocol = Seq(TestProtocol.Plain)))
     val params = linker.routers.head.params[PerClientParams].paramsFor(Path.read("/foo"))
-    val options = SocketOptionsConfig(reusePort = true).params
+    val options = SocketOptionsConfig(reusePort = Some(true)).params
+
+    assert(params == options)
+  }
+
+  test("socket options should keep default values if not specified") {
+    val config =
+      """
+        |routers:
+        |- protocol: plain
+        |  client:
+        |    socketOptions:
+        |      backlog: 128
+        |  servers:
+        |  - {}
+      """.stripMargin
+    val linker = Linker.load(config, Initializers(protocol = Seq(TestProtocol.Plain)))
+    val params = linker.routers.head.params[PerClientParams].paramsFor(Path.read("/foo"))
+    val options = SocketOptionsConfig(
+      noDelay = Some(true),
+      reuseAddr = Some(true),
+      reusePort = Some(false),
+      backlog = Some(128)).params
 
     assert(params == options)
   }
