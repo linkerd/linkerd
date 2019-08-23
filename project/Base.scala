5329014e4a96f8002eb3b0039cfc8dd009c508a9
Dennis Adjei-Baah <dennis@buoyant.io>
2019-08-23 09:56:19 -0700

    merge release branch cpretzer/1.6.4 (#2317)
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- project/Base.scala
+++ project/Base.scala
@@ -32,7 +32,7 @@ object Base {
 class Base extends Build {
   import Base._
 
-  val headVersion = "1.6.3"
+  val headVersion = "1.6.4"
   val openJdkVersion = "8u212"
   val openJ9Version = "jdk8u212-b04_openj9-0.14.2"
 
