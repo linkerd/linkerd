df4951ce760a0ade3ed740fb50c766a993c0fa8c
Dmytro Kostiuchenko <edio@archlinux.us>
2019-09-04 12:51:30 -0400

    Extract static parts of gRPC headers to variables
    Signed-off-by: Dmytro Kostiuchenko <edio@archlinux.us>

--- grpc/runtime/src/main/scala/io/buoyant/grpc/runtime/H2Headers.scala
+++ grpc/runtime/src/main/scala/io/buoyant/grpc/runtime/H2Headers.scala
@@ -3,19 +3,21 @@ package io.buoyant.grpc.runtime
 import com.twitter.finagle.buoyant.h2
 
 private[runtime] object H2Headers {
-  private[this] val ContentType = "content-type"
-  private[this] val GrpcProtoContentType = "application/grpc+proto"
+  private[this] val AuthorityHeader = h2.Headers.Authority -> ""
+  private[this] val ContentTypeHeader = "content-type" -> "application/grpc+proto"
+  private[this] val MethodHeader = h2.Headers.Method -> h2.Method.Post.toString
+  private[this] val SchemeHeader = h2.Headers.Scheme -> "http"
 
   def responseHeaders(http2Status: h2.Status = h2.Status.Ok): h2.Headers = h2.Headers(
     h2.Headers.Status -> http2Status.toString,
-    ContentType -> GrpcProtoContentType
+    ContentTypeHeader
   )
 
   def requestHeaders(path: String): h2.Headers = h2.Headers(
-    h2.Headers.Scheme -> "http",
-    h2.Headers.Method -> h2.Method.Post.toString,
-    h2.Headers.Authority -> "",
     h2.Headers.Path -> path,
-    ContentType -> GrpcProtoContentType
+    AuthorityHeader,
+    ContentTypeHeader,
+    MethodHeader,
+    SchemeHeader
   )
 }
