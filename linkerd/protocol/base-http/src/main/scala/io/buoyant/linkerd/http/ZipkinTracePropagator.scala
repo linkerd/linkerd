45d5c023746bbd6bdc56b4116c34e64cdd23bf25
dst4096 <dst4096@gmail.com>
2019-10-07 19:51:52 +0300

    Moved http and h2 ZipkinTracePropagator to common http code. X-b3 multi headers are now removed from the request after the traceId call. Sampler is now computed using the traceid from thread local context.
    Signed-off-by: dst4096 <dst4096@gmail.com>

---
+++ linkerd/protocol/base-http/src/main/scala/io/buoyant/linkerd/http/ZipkinTracePropagator.scala
@@ -0,0 +1,103 @@
+package io.buoyant.linkerd.protocol.http
+
+import com.twitter.finagle.{SimpleFilter, Stack}
+import com.twitter.finagle.buoyant.Sampler
+import com.twitter.finagle.http.util.StringUtil
+import com.twitter.finagle.tracing.{Flags, SpanId, Trace, TraceId, Tracer}
+import com.twitter.util.Try
+import io.buoyant.linkerd.{TracePropagator, TracePropagatorInitializer}
+import io.buoyant.router.http.{HeadersLike, RequestLike}
+
+class ZipkinTracePropagator[Req, H: HeadersLike](implicit requestLike: RequestLike[Req, H]) extends TracePropagator[Req] {
+  /**
+   * Read the trace id from the request, if it has one.
+   */
+  override def traceId(req: Req): Option[TraceId] = {
+    var traceId = ZipkinTrace.get(requestLike.headers(req))
+    ZipkinTrace.clear(requestLike.headers(req))
+    traceId
+  }
+
+  /**
+   * Return a sampler which decides if the given request should be sampled, based on properties
+   * of the request (zipkin or linkerd if zipkin not present).  If None is returned, the decision of whether to sample the request is deferred
+   * to the tracer.
+   */
+  override def sampler(req: Req): Option[Sampler] = {
+    if (Trace.hasId) {
+      Trace.id.sampled.map {
+        case true => Sampler(1.0f)
+        case false => Sampler(0.0f)
+      }
+    } else {
+      None
+    }
+  }
+
+  /**
+   * Write the trace id onto a request.
+   */
+  override def setContext(
+    req: Req,
+    traceId: TraceId
+  ): Unit = {
+    ZipkinTrace.set(requestLike.headers(req), traceId)
+  }
+}
+
+object ZipkinTrace {
+
+  val ZipkinSpanHeader = "x-b3-spanid"
+  val ZipkinParentHeader = "x-b3-parentspanid"
+  val ZipkinTraceHeader = "x-b3-traceid"
+  val ZipkinSampleHeader = "x-b3-sampled"
+  val ZipkinFlagsHeader = "x-b3-flags"
+
+  def get[H: HeadersLike](headers: H): Option[TraceId] = {
+    val trace = caseInsensitiveGet(headers, ZipkinTraceHeader).flatMap(SpanId.fromString)
+    val parent = caseInsensitiveGet(headers, ZipkinParentHeader).flatMap(SpanId.fromString)
+    val span = caseInsensitiveGet(headers, ZipkinSpanHeader).flatMap(SpanId.fromString)
+    val sample = caseInsensitiveGet(headers, ZipkinSampleHeader).map(StringUtil.toBoolean)
+    val flags = caseInsensitiveGet(headers, ZipkinFlagsHeader).map(StringUtil.toSomeLong) match {
+      case Some(f) => Flags(f)
+      case None => Flags()
+    }
+    span.map { s =>
+      TraceId(trace, parent, s, sample, flags)
+    }
+  }
+
+  def clear[H: HeadersLike](headers: H): Unit = {
+    val headersLike = implicitly[HeadersLike[H]]
+
+    headersLike.remove(headers, ZipkinSpanHeader)
+    headersLike.remove(headers, ZipkinTraceHeader)
+    headersLike.remove(headers, ZipkinParentHeader)
+    headersLike.remove(headers, ZipkinSampleHeader)
+    headersLike.remove(headers, ZipkinFlagsHeader)
+
+    headersLike.remove(headers, ZipkinSpanHeader.toUpperCase())
+    headersLike.remove(headers, ZipkinTraceHeader.toUpperCase())
+    headersLike.remove(headers, ZipkinParentHeader.toUpperCase())
+    headersLike.remove(headers, ZipkinSampleHeader.toUpperCase())
+    headersLike.remove(headers, ZipkinFlagsHeader.toUpperCase())
+
+    ()
+  }
+
+  def set[H: HeadersLike](headers: H, id: TraceId): Unit = {
+    val headersLike = implicitly[HeadersLike[H]]
+
+    headersLike.set(headers, ZipkinSpanHeader, id.spanId.toString)
+    headersLike.set(headers, ZipkinTraceHeader, id.traceId.toString)
+    headersLike.set(headers, ZipkinParentHeader, id.parentId.toString)
+    headersLike.set(headers, ZipkinSampleHeader, (if ((id.sampled exists { _ == true })) 1 else 0).toString)
+    headersLike.set(headers, ZipkinFlagsHeader, id.flags.toLong.toString)
+    ()
+  }
+
+  private def caseInsensitiveGet[H: HeadersLike](headers: H, key: String): Option[String] = {
+    val headersLike = implicitly[HeadersLike[H]]
+    headersLike.toSeq(headers).iterator.collectFirst { case (k, v) if key.equalsIgnoreCase(k) => v }
+  }
+}
---
+++ linkerd/protocol/h2/src/test/scala/io/buoyant/linkerd/protocol/h2/ZipkinTracePropagatorTest.scala
@@ -0,0 +1,225 @@
+package io.buoyant.linkerd.protocol.h2
+
+import com.twitter.finagle.buoyant.Sampler
+import com.twitter.util.{Await, Future}
+import org.scalatest.FunSuite
+import com.twitter.conversions.DurationOps._
+import com.twitter.finagle.Service
+import com.twitter.finagle.tracing.Trace
+import com.twitter.finagle.buoyant.h2.{Headers, Request, Status}
+import com.twitter.finagle.buoyant.h2._
+import io.buoyant.router.RoutingFactory._
+import io.buoyant.linkerd.protocol.http._
+import io.buoyant.router.H2Instances._
+
+class ZipkinTracePropagatorTest extends FunSuite {
+
+  test("get traceid from multi x-b3 headers, 64bit trace id, sampled, UPPER CASE header doesn't matter") {
+    val ztp = new ZipkinTracePropagator
+
+    //x-b3 multi headers with lower case
+    val req1 = Request("http", Method.Get, "auf", "/", Stream.empty())
+    req1.headers.add("x-b3-traceid", "80f198ee56343ba8")
+    req1.headers.add("x-b3-spanid", "05e3ac9a4f6e3b90")
+    req1.headers.add("x-b3-sampled", "1")
+    req1.headers.add("x-b3-parentspanid", "e457b5a2e4d86bd1")
+    val trace1 = ztp.traceId(req1)
+    // test multi x-b3 headers have been removed after traceId call
+    assert(req1.headers.toSeq == Seq((":scheme","http"), (":method","GET"), (":authority","auf"), (":path","/")))
+
+    //X-B3 multi heders with upper case
+    val req2 = Request("http", Method.Get, "auf", "/", Stream.empty())
+    req2.headers.add("X-B3-TRACEID", "80f198ee56343ba8")
+    req2.headers.add("X-B3-SPANID", "05e3ac9a4f6e3b90")
+    req2.headers.add("X-B3-SAMPLED", "1")
+    req2.headers.add("X-B3-PARENTSPANID", "e457b5a2e4d86bd1")
+    val trace2 = ztp.traceId(req2)
+    // test multi x-b3 headers have been removed after traceId call
+    assert(req2.headers.toSeq == Seq((":scheme","http"), (":method","GET"), (":authority","auf"), (":path","/")))
+
+    // traceid is the same, lower/upper case headers don't matter
+    assert(trace1 == trace2)
+
+    assert(trace1.isDefined) //expect trace exists and has the expected values
+    trace1.foreach { tid => {
+      assert(tid.traceId.toString().equals("80f198ee56343ba8"))
+      assert(tid.spanId.toString().equals("05e3ac9a4f6e3b90"))
+      assert(tid.parentId.toString().equals("e457b5a2e4d86bd1"))
+      assert(tid.sampled.contains(true))
+    }
+    }
+
+    val svc = new Service[Request, Response] {
+      def apply(request: Request): Future[Response] = {
+        // expect traceid to have been set on thread local context
+        assert(Trace.idOption == trace1)
+
+        // expect to get the right sampled value which is 1.0
+        val sampler = ztp.sampler(req1)
+        assert(sampler.contains(Sampler(1.0f)))
+
+        Future.value(Response(Status.Ok, Stream()))
+      }
+    }
+
+    val res = Trace.letIdOption(trace1) {
+      svc(req1)
+    }
+
+    assert(Status.Ok == Await.result(res, 5.seconds).status)
+
+    ztp.setContext(req1, trace1.get)
+    // check "x-b3-traceid" and "x-b3-spanid" and "x-b3-sampled" and "x-b3-parentspanid" have been added to request
+    assert(req1.headers.contains("x-b3-traceid"))
+    assert(req1.headers.contains("x-b3-spanid"))
+    assert(req1.headers.contains("x-b3-parentspanid"))
+    assert(req1.headers.contains("x-b3-sampled"))
+
+  }
+
+  test("get traceid from multi x-b3 headers, 64bit trace id, do not sample decision set") {
+    val ztp = new ZipkinTracePropagator
+
+    //x-b3 multi headers with lower case
+    val req1 = Request("http", Method.Get, "auf", "/", Stream.empty())
+    req1.headers.add("x-b3-traceid", "80f198ee56343ba8")
+    req1.headers.add("x-b3-spanid", "05e3ac9a4f6e3b90")
+    req1.headers.add("x-b3-sampled", "0")
+    req1.headers.add("x-b3-parentspanid", "e457b5a2e4d86bd1")
+    val trace1 = ztp.traceId(req1)
+    // test multi x-b3 headers have been removed after traceId call
+    assert(req1.headers.toSeq == Seq((":scheme","http"), (":method","GET"), (":authority","auf"), (":path","/")))
+
+    assert(trace1.isDefined) //expect trace exists and has the expected values
+    trace1.foreach { tid => {
+      assert(tid.traceId.toString().equals("80f198ee56343ba8"))
+      assert(tid.spanId.toString().equals("05e3ac9a4f6e3b90"))
+      assert(tid.parentId.toString().equals("e457b5a2e4d86bd1"))
+      assert(tid.sampled.contains(false))
+    }
+    }
+
+    val svc = new Service[Request, Response] {
+      def apply(request: Request): Future[Response] = {
+        // expect traceid to have been set on thread local context
+        assert(Trace.idOption == trace1)
+
+        // expect to get the right sampled value which is 1.0
+        val sampler = ztp.sampler(req1)
+        assert(sampler.contains(Sampler(0.0f)))
+
+        Future.value(Response(Status.Ok, Stream()))
+      }
+    }
+
+    val res = Trace.letIdOption(trace1) {
+      svc(req1)
+    }
+
+    assert(Status.Ok == Await.result(res, 5.seconds).status)
+
+    ztp.setContext(req1, trace1.get)
+    // check "x-b3-traceid" and "x-b3-spanid" and "x-b3-sampled" and "x-b3-parentspanid" have been added to request
+    assert(req1.headers.contains("x-b3-traceid"))
+    assert(req1.headers.contains("x-b3-spanid"))
+    assert(req1.headers.contains("x-b3-parentspanid"))
+    assert(req1.headers.contains("x-b3-sampled"))
+  }
+
+  test("get traceid from multi x-b3 headers, 64bit trace id, no sampling decision present") {
+    val ztp = new ZipkinTracePropagator
+
+    //x-b3 multi headers with lower case
+    val req1 = Request("http", Method.Get, "auf", "/", Stream.empty())
+    req1.headers.add("x-b3-traceid", "80f198ee56343ba8")
+    req1.headers.add("x-b3-spanid", "05e3ac9a4f6e3b90")
+    req1.headers.add("x-b3-parentspanid", "e457b5a2e4d86bd1")
+    val trace1 = ztp.traceId(req1)
+    // test multi x-b3 headers have been removed after traceId call
+    assert(req1.headers.toSeq == Seq((":scheme","http"), (":method","GET"), (":authority","auf"), (":path","/")))
+
+    assert(trace1.isDefined) //expect trace exists and has the expected values
+    trace1.foreach { tid => {
+      assert(tid.traceId.toString().equals("80f198ee56343ba8"))
+      assert(tid.spanId.toString().equals("05e3ac9a4f6e3b90"))
+      assert(tid.parentId.toString().equals("e457b5a2e4d86bd1"))
+      assert(tid.sampled.isEmpty)
+    }
+    }
+
+    val svc = new Service[Request, Response] {
+      def apply(request: Request): Future[Response] = {
+        // expect traceid to have been set on thread local context
+        assert(Trace.idOption == trace1)
+
+        // expect to get the right sampled value which is 1.0
+        val sampler = ztp.sampler(req1)
+        assert(sampler.isEmpty)
+
+        Future.value(Response(Status.Ok, Stream()))
+      }
+    }
+
+    val res = Trace.letIdOption(trace1) {
+      svc(req1)
+    }
+
+    assert(Status.Ok == Await.result(res, 5.seconds).status)
+
+    ztp.setContext(req1, trace1.get)
+    // check "x-b3-traceid" and "x-b3-spanid" and "x-b3-parentspanid" have been added to request
+    assert(req1.headers.contains("x-b3-traceid"))
+    assert(req1.headers.contains("x-b3-spanid"))
+    assert(req1.headers.contains("x-b3-parentspanid"))
+  }
+
+  test("get traceid from multi x-b3 headers, 64bit trace id, debug flags set") {
+    val ztp = new ZipkinTracePropagator
+
+    //x-b3 multi headers with lower case
+    val req1 = Request("http", Method.Get, "auf", "/", Stream.empty())
+    req1.headers.add("x-b3-traceid", "80f198ee56343ba8")
+    req1.headers.add("x-b3-spanid", "05e3ac9a4f6e3b90")
+    req1.headers.add("x-b3-flags", "1")
+    req1.headers.add("x-b3-parentspanid", "e457b5a2e4d86bd1")
+    val trace1 = ztp.traceId(req1)
+    // test multi x-b3 headers have been removed after traceId call
+    assert(req1.headers.toSeq == Seq((":scheme","http"), (":method","GET"), (":authority","auf"), (":path","/")))
+
+    assert(trace1.isDefined) //expect trace exists and has the expected values
+    trace1.foreach { tid => {
+      assert(tid.traceId.toString().equals("80f198ee56343ba8"))
+      assert(tid.spanId.toString().equals("05e3ac9a4f6e3b90"))
+      assert(tid.parentId.toString().equals("e457b5a2e4d86bd1"))
+      assert(tid.sampled.contains(true))
+    }
+    }
+
+    val svc = new Service[Request, Response] {
+      def apply(request: Request): Future[Response] = {
+        // expect traceid to have been set on thread local context
+        assert(Trace.idOption == trace1)
+
+        // expect to get the right sampled value which is 1.0
+        val sampler = ztp.sampler(req1)
+        assert(sampler.contains(Sampler(1.0f)))
+
+        Future.value(Response(Status.Ok, Stream()))
+      }
+    }
+
+    val res = Trace.letIdOption(trace1) {
+      svc(req1)
+    }
+
+    assert(Status.Ok == Await.result(res, 5.seconds).status)
+
+    ztp.setContext(req1, trace1.get)
+    // check "x-b3-traceid" and "x-b3-spanid" and "x-b3-flags" and "x-b3-parentspanid" have been added to request
+    assert(req1.headers.contains("x-b3-traceid"))
+    assert(req1.headers.contains("x-b3-spanid"))
+    assert(req1.headers.contains("x-b3-parentspanid"))
+    assert(req1.headers.contains("x-b3-flags"))
+  }
+
+}
