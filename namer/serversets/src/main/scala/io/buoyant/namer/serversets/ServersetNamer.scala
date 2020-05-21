5a7a334f975451eea229ab9f6309152530bdf066
Justin Venus <justin.venus@gmail.com>
2020-05-21 15:52:16 -0500

    minor robustness improvement for ServersetNamer
    Signed-off-by: Justin Venus <justin.venus@gmail.com>

--- namer/serversets/src/main/scala/io/buoyant/namer/serversets/ServersetNamer.scala
+++ namer/serversets/src/main/scala/io/buoyant/namer/serversets/ServersetNamer.scala
@@ -2,7 +2,7 @@
 package io.buoyant.namer.serversets
 
 import com.twitter.finagle._
-import com.twitter.util.{Activity, Var}
+import com.twitter.util.{Activity, Var, Try, Return, Throw}
 import com.twitter.finagle.serverset2.{BouyantZkResolver, BouyantZkResolverImpl}
 
 /**
@@ -30,25 +30,29 @@ class ServersetNamer(zkHost: Path, idPrefix: Path, zk2: BouyantZkResolver) exten
   def this(zkHost: String, idPrefix: Path) = this(Path.Utf8(zkHost), idPrefix, new BouyantZkResolverImpl)
 
   /** Bind a name. */
-  protected[this] def bind(path: Path, residual: Path = Path.empty): Activity[NameTree[Name]] = {
-    val addr = zk2.resolve(zkHost ++ path)
-    Activity(addr.map(Activity.Ok(_))).flatMap {
-      case Addr.Neg if !path.isEmpty =>
-        val n = path.size
-        bind(path.take(n - 1), path.drop(n - 1) ++ residual)
-      case Addr.Neg =>
-        Activity.value(NameTree.Neg)
-      case Addr.Bound(_, _) =>
-        // Clients may depend on Name.Bound ids being Paths which resolve
-        // back to the same Name.Bound
-        val id = idPrefix ++ path
-        Activity.value(NameTree.Leaf(Name.Bound(addr, id, residual)))
-      case Addr.Pending =>
-        Activity.pending
-      case Addr.Failed(exc) =>
+  protected[this] def bind(path: Path, residual: Path = Path.empty): Activity[NameTree[Name]] =
+    Try(zk2.resolve(zkHost ++ path)) match {
+      case Return(addr) =>
+        val act = Activity(addr.map(Activity.Ok(_)))
+        act.flatMap {
+          case Addr.Neg if !path.isEmpty =>
+            val n = path.size
+            bind(path.take(n - 1), path.drop(n - 1) ++ residual)
+          case Addr.Neg =>
+            Activity.value(NameTree.Neg)
+          case Addr.Bound(_, _) =>
+            // Clients may depend on Name.Bound ids being Paths which resolve
+            // back to the same Name.Bound
+            val id = idPrefix ++ path
+            Activity.value(NameTree.Leaf(Name.Bound(addr, id, residual)))
+          case Addr.Pending =>
+            Activity.pending
+          case Addr.Failed(exc) =>
+            Activity.exception(exc)
+        }
+      case Throw(exc) =>
         Activity.exception(exc)
     }
-  }
 
   // We have to involve a serverset roundtrip here to return a tree. We run the
   // risk of invalidating an otherwise valid tree when there is a bad serverset
