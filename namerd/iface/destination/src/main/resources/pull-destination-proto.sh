fb196041b262483c8b61c9ea63e6d4fd8ce0009d
Charles Pretzer <charles@buoyant.io>
2020-07-14 12:54:04 -0700

    fixups for shellcheck
    Signed-off-by: Charles Pretzer <charles@buoyant.io>

--- namerd/iface/destination/src/main/resources/pull-destination-proto.sh
+++ namerd/iface/destination/src/main/resources/pull-destination-proto.sh
@@ -7,6 +7,6 @@ PROTOFILES="destination.proto net.proto"
 
 for proto_file in $PROTOFILES; do
   echo "Copying $proto_file from linkerd2 repo"
-  printf "//This file is added using pull-destination-proto.sh. DO NOT EDIT!\\n%s" "$(curl $BASE_PROTO_URL/$proto_file)" > $proto_file
+  printf "//This file is added using pull-destination-proto.sh. DO NOT EDIT!\\n%s" "$(curl $BASE_PROTO_URL/"$proto_file")" > "$proto_file"
 done
 
